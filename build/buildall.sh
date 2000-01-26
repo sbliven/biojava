6d97bc571d3e73a9d48567c95b9d92ddf08fb33f
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-01-26 16:05:37 +0000

    Fixed to make class directory.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@397 7c6358e6-4a41-0410-a743-a5b2a554c398

--- build/buildall.sh
+++ build/buildall.sh
@@ -1,4 +1,5 @@
-#! /usr/sh
+#! /bin/sh
+mkdir class
 find src -name "*.java" | xargs javac -classpath xml.jar:class -sourcepath src -d class
 jar -cf biojava.jar -C class .
 jar -uf biojava.jar -C resources .
