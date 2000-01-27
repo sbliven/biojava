e8e303ad505b00993cc03894277f22dbfb1ee44c
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-01-27 17:10:22 +0000

    Added a couple of missing files from my internal SVM development tree.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@402 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/stats/svm/CachingKernel.java
@@ -0,0 +1,58 @@
+/*
+ * @(#)CachingKernel.java      0.1 00/01/20
+ *
+ * By Thomas Down <td2@sanger.ac.uk>
+ */
+
+package org.biojava.stats.svm;
+
+import java.util.*;
+
+public class CachingKernel implements SVMKernel {
+    private SVMKernel parent;
+    private Map cache;
+
+    public CachingKernel() {
+	cache = new HashMap();
+    }
+
+    public void setNestedKernel(SVMKernel k) {
+	parent = k;
+    }
+
+    public SVMKernel getNestedKernel() {
+	return parent;
+    }
+
+    public double evaluate(Object x, Object y) {
+	ObjectPair op = new ObjectPair(x, y);
+	Double d = (Double) cache.get(op);
+	if (d == null) {
+	    d = new Double(parent.evaluate(x, y));
+	    cache.put(op, d);
+	}
+	return d.doubleValue();
+    }
+
+    private static class ObjectPair {
+	Object a;
+	Object b;
+
+	public ObjectPair(Object a, Object b) {
+	    this.a = a;
+	    this.b = b;
+	}
+
+	public boolean equals(Object x) {
+	    if (! (x instanceof ObjectPair))
+		return false;
+	    ObjectPair op = (ObjectPair) x;
+	    return ((op.a == a && op.b == b) || 
+		    (op.a == b && op.b == a));
+	}
+
+	public int hashCode() {
+	    return a.hashCode() + b.hashCode();
+	}
+    }
+}
---
+++ src/org/biojava/stats/svm/NormalizingKernel.java
@@ -0,0 +1,26 @@
+/*
+ * @(#)NormalizingKernel.java      0.1 00/01/20
+ *
+ * By Thomas Down <td2@sanger.ac.uk>
+ */
+
+package org.biojava.stats.svm;
+
+public class NormalizingKernel implements SVMKernel {
+    private SVMKernel k;
+
+    public void setNestedKernel(SVMKernel k) {
+	this.k = k;
+    }
+
+    public SVMKernel getNestedKernel() {
+	return k;
+    }
+
+    public double evaluate(Object a, Object b) {
+	double kAA = k.evaluate(a, a);
+	double kBB = k.evaluate(b, b);
+	double kAB = k.evaluate(a, b);
+	return kAB / Math.sqrt(kAA * kBB);
+    }
+}
