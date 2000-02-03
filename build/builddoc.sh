4647c45954ced7919c4f1a770d63ade866c7c3c2
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-02-03 14:37:36 +0000

    Added grouping on documentation index.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@425 7c6358e6-4a41-0410-a743-a5b2a554c398

--- build/builddoc.sh
+++ build/builddoc.sh
@@ -13,6 +13,10 @@ org.acedb.staticobj \
 org.acedb.socket \
 org.biojava.utils.xml)
 
+set PACKAGES_CORE=org.biojava.bio.seq:org.biojava.bio.seq.io:org.biojava.bio.seq.tools:org.biojava.bio.alignment:org.biojava.bio.gui
+
+set PACKAGES_ACEDB=org.acedb:org.acedb.seq:org.acedb.staticobj:org.acedb.socket
+
 echo $PACKAGES
 mkdir docs
 javadoc \
@@ -20,4 +24,7 @@ javadoc \
  -classpath xml.jar:class \
  -d docs \
  -private -use -version -author \
- $PACKAGES
+ -windowtitle "Biojava API documentation" \
+ -group "Core Packages" ${PACKAGES_CORE} \
+ -group "ACeDB Connection Packages" ${PACKAGES_ACEDB} \
+ ${PACKAGES}
