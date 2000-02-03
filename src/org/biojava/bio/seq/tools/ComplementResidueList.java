bc2be519a681bf18bcd50993e46b5059c2ff73f1
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-02-03 15:20:25 +0000

    Added AbstractResidueList, and converted ComplementResidueList to use this.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@429 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/seq/tools/ComplementResidueList.java
+++ src/org/biojava/bio/seq/tools/ComplementResidueList.java
@@ -34,7 +34,7 @@ import org.biojava.bio.seq.*;
  * @author Thomas Down
  */
 
-public class ComplementResidueList implements ResidueList {
+public class ComplementResidueList extends AbstractResidueList {
     private ResidueList parent;
 
     public ComplementResidueList(ResidueList p) throws IllegalAlphabetException {
@@ -48,10 +48,6 @@ public class ComplementResidueList implements ResidueList {
 	return parent.alphabet();
     }
 
-    public Iterator iterator() {
-	return new ResidueIterator(1, length());
-    }
-
     public int length() {
 	return parent.length();
     }
@@ -61,88 +57,6 @@ public class ComplementResidueList implements ResidueList {
 	    return DNATools.complement(parent.residueAt(parent.length() - pos + 1));
 	} catch (IllegalResidueException ex) {
 	    throw new BioError(ex);
-	}
-    }
-
-    public ResidueList subList(int start, int end) {
-	return new SubList(start, end);
-    }
-
-    public List toList() {
-	return new ListView(this);
-    }
-
-    class ResidueIterator implements Iterator {
-	private int min, max;
-        private int pos;
-	    
-	public ResidueIterator(int min, int max) {
-	    this.min = min;
-	    this.max = max;
-	    pos = min;
-	}
-
-	public boolean hasNext() {
-	    return (pos <= max);
-	}
-
-	public Object next() {
-	    if (pos > max)
-		throw new NoSuchElementException();
-	    return residueAt(pos++);
-	}
-
-	public void remove() {
-	    throw new UnsupportedOperationException();
-	}
-    }
-
-    class SubList implements ResidueList {
-	private int start, end;
-
-	public SubList(int start, int end) {
-	    this.start = start;
-	    this.end = end;
-	}
-
-	public Alphabet alphabet() {
-	    return ComplementResidueList.this.alphabet();
-	}
-
-	public Iterator iterator() {
-	    return new ResidueIterator(start, end);
-	}
-
-	public int length() {
-	    return end - start + 1;
-	}
-
-	public Residue residueAt(int pos) {
-	    return ComplementResidueList.this.residueAt(pos + start - 1);
-	}
-
-	public ResidueList subList(int sstart, int send) {
-	    return new SubList(sstart + start - 1, sstart + end - 1);
-	}
-
-	public List toList() {
-	    return new ListView(this);
-	}
-    }
-
-    private static class ListView extends AbstractList {
-	private ResidueList rl;
-
-	ListView(ResidueList rl) {
-	    this.rl = rl;
-	}
-
-	public int size() {
-	    return rl.length();
-	}
-	
-	public Object get(int pos) {
-	    return rl.residueAt(pos + 1);
 	}
     }
 }
