f1624c10c2e981c85aab400918b54cf2f1efd61a
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-02-12 14:11:19 +0000

    Corba server & client that serve & process a database. Cool.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@474 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ demos/CorbaDBServer.java
@@ -0,0 +1,61 @@
+/**
+ * A simple corba server that serves up a simple database with randomly
+ * positioned features.
+ */
+
+import java.io.*;
+import org.omg.CORBA.*;
+ 
+import org.biojava.bio.seq.*;
+import org.biojava.bio.seq.io.*;
+import org.biojava.bio.seq.tools.*;
+import org.biojava.bridge.Biocorba.Seqcore.*;
+import org.Biocorba.Seqcore.*;
+
+public class CorbaDBServer {
+  public static final void main(String [] args) throws Exception {
+    if(args.length != 2) {
+      throw new Exception("Use: CorbaDBServer sequences.fa type");
+    }
+    
+    // work out what to load
+    File seqDBFile = new File(args[0]);
+    Alphabet alpha = AlphabetManager.instance().alphabetForName(args[1]);
+    if(alpha == null) {
+      throw new NullPointerException("Alphabet " + args[1] + " not known");
+    }    
+    ResidueParser rParser = alpha.getParser("symbol");
+    SequenceFactory sFact = new SimpleSequenceFactory();
+    
+    // load it
+    System.out.println("Loading fasta database: " + seqDBFile);
+    HashSequenceDB seqDB = new HashSequenceDB(HashSequenceDB.byURN);
+    SequenceFormat sFormat = new FastaFormat();
+    InputStream seqDBI = new FileInputStream(seqDBFile);
+    SequenceIterator seqI = new StreamReader(seqDBI,
+                                             sFormat, rParser, sFact);
+    while(seqI.hasNext()) {
+      try {
+        seqDB.addSequence(seqI.nextSequence());
+      } catch (SeqException se) {
+        se.printStackTrace();
+      }
+    }
+    
+    // serve it as a corba server
+    SeqDBImpl seqDBImpl = new SeqDBImpl(seqDB, seqDBFile.toString());
+    _SeqDB_Tie seqDBTie = new _SeqDB_Tie(seqDBImpl);
+    ORB orb = ORB.init(new String[0], null);
+    orb.connect(seqDBTie);
+    
+    // print out the ior
+    String ior = orb.object_to_string(seqDBTie);
+    System.out.println(ior);
+    
+    // hang this thread so that the process doesn't exit
+    java.lang.Object sync = new java.lang.Object();
+    synchronized(sync) {
+      sync.wait();
+    }
+  }
+}
