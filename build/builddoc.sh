92962d2e8b5ddd2fab0383e80da8f73747711b7b
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-02-12 15:26:39 +0000

    Updated with groups for the corba stuff.
    I think this is the pattern of javadoc script that we should broadly follow
    for now.
    
    
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@477 7c6358e6-4a41-0410-a743-a5b2a554c398

--- build/builddoc.sh
+++ build/builddoc.sh
@@ -1,23 +1,11 @@
 #! /bin/csh -f
-set PACKAGES=(org.biojava.bio.gui \
-org.biojava.bio.seq \
-org.biojava.bio.seq.io \
-org.biojava.bio.seq.tools \
-org.biojava.bio.alignment \
-org.biojava.bio.program \
-org.biojava.stats.svm \
-org.biojava.stats.svm.tools \
-org.acedb \
-org.acedb.seq \
-org.acedb.staticobj \
-org.acedb.socket \
-org.biojava.utils.xml)
-
 set PACKAGES_CORE=org.biojava.bio.seq:org.biojava.bio.seq.io:org.biojava.bio.seq.tools:org.biojava.bio.alignment:org.biojava.bio.gui
-
+set PACKAGES_DEVELOPMENT=org.biojava.stats.svm:org.biojava.stats.svm.tools:org.biojava.utils.xml
+set PACKAGES_CORBA=GNOME:org.Biocorba.Seqcore:org.biojava.bridge.GNOME:org.biojava.bridge.Biocorba.Seqcore
 set PACKAGES_ACEDB=org.acedb:org.acedb.seq:org.acedb.staticobj:org.acedb.socket
 
-echo $PACKAGES
+set PACKAGES=`echo ${PACKAGES_CORE} ${PACKAGES_DEVELOPMENT} ${PACKAGES_CORBA} ${PACKAGES_ACEDB} | tr ':' ' '`
+
 mkdir docs
 javadoc \
  -sourcepath src \
@@ -26,5 +14,7 @@ javadoc \
  -private -use -version -author \
  -windowtitle "Biojava API documentation" \
  -group "Core Packages" ${PACKAGES_CORE} \
+ -group "Development Packages" ${PACKAGES_DEVELOPMENT} \
+ -group "Corba Packages" ${PACKAGES_CORBA} \
  -group "ACeDB Connection Packages" ${PACKAGES_ACEDB} \
  ${PACKAGES}
