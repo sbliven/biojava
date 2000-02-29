4e7277c6676af25b5c75ba5b6b53027ef16d28f4
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-02-29 10:49:23 +0000

    Alignments with special labels for the sequence, states & other info.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@534 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/alignment/SimpleStatePath.java
@@ -0,0 +1,41 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+
+package org.biojava.bio.alignment;
+
+import java.util.Map;
+
+/**
+ * A no-frills implementation of StatePath.
+ */
+public class SimpleStatePath extends SimpleAlignment implements StatePath {
+  private double score;
+  
+  public double getScore() {
+    return score;
+  }
+  
+  public SimpleStatePath(double score, Map labelToResLists) {
+    super(labelToResLists);
+    this.score = score;
+  }
+}
---
+++ src/org/biojava/bio/alignment/StatePath.java
@@ -0,0 +1,54 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+
+package org.biojava.bio.alignment;
+
+import java.util.Map;
+
+/**
+ * Extends the Alignment interface so that it is explicitly used to represent
+ * a state path through an HMM, and the associated emitted sequence and
+ * likelyhoods.
+ */
+public interface StatePath extends Alignment {
+  /**
+   * Alignment label for the emitted sequence.
+   */
+  public static final Object SEQUENCE = "SEQUENCE";
+
+  /**
+   * Alignment label for the state path.
+   */
+  public static final Object STATES   = "STATES";
+
+  /**
+   * Alignment label for the likelyhood at each step.
+   */
+  public static final Object SCORES   = "SCORES";
+  
+  /**
+   * Return the overall score for this state-path and it's emissions.
+   *
+   * @return the score
+   */
+  public double getScore();
+}
