c1e34c61b649fc6b7ebb63567665a7c1ccd13c13
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-03-06 17:14:59 +0000

    It now makes some attempt at documenting the demos. I can't get the demo package/html stuff to work out, though.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@546 7c6358e6-4a41-0410-a743-a5b2a554c398

--- build/builddoc.sh
+++ build/builddoc.sh
@@ -1,16 +1,18 @@
-#! /bin/sh
-PACKAGES_CORE=org.biojava.bio.seq:org.biojava.bio.seq.io:org.biojava.bio.seq.tools:org.biojava.bio.dp:org.biojava.bio.gui:org.biojava.bio.program:org.biojava.bio.program.gff
-PACKAGES_DEVELOPMENT=org.biojava.stats.svm:org.biojava.stats.svm.tools:org.biojava.utils.xml
-PACKAGES_CORBA=GNOME:org.Biocorba.Seqcore:org.biojava.bridge.GNOME:org.biojava.bridge.Biocorba.Seqcore
-PACKAGES_ACEDB=org.acedb:org.acedb.seq:org.acedb.staticobj:org.acedb.socket
+#! /bin/csh
+set PACKAGES_CORE=org.biojava.bio.seq:org.biojava.bio.seq.io:org.biojava.bio.seq.tools:org.biojava.bio.dp:org.biojava.bio.gui:org.biojava.bio.program:org.biojava.bio.program.gff
+set PACKAGES_DEVELOPMENT=org.biojava.stats.svm:org.biojava.stats.svm.tools:org.biojava.utils.xml
+set PACKAGES_CORBA=GNOME:org.Biocorba.Seqcore:org.biojava.bridge.GNOME:org.biojava.bridge.Biocorba.Seqcore
+set PACKAGES_ACEDB=org.acedb:org.acedb.seq:org.acedb.staticobj:org.acedb.socket
 
-PACKAGES=`echo ${PACKAGES_CORE} ${PACKAGES_DEVELOPMENT} ${PACKAGES_CORBA} ${PACKAGES_ACEDB} | tr ':' ' '`
+set PACKAGES=`echo ${PACKAGES_CORE} ${PACKAGES_DEVELOPMENT} ${PACKAGES_CORBA} ${PACKAGES_ACEDB} | tr ':' ' '`
 
+rm -rf docs
 mkdir docs
+mkdir docs/api
 javadoc \
  -sourcepath src \
  -classpath xml.jar:class \
- -d docs \
+ -d docs/api \
  -private -use -version -author \
  -windowtitle "Biojava API documentation" \
  -group "Core Packages" ${PACKAGES_CORE} \
@@ -19,4 +21,25 @@ javadoc \
  -group "ACeDB Connection Packages" ${PACKAGES_ACEDB} \
  ${PACKAGES}
 
-cat build/extra.css >> docs/stylesheet.css
+cat build/extra.css >> docs/api/stylesheet.css
+
+mkdir docs/demos
+set DEMOS=`find demos/* -type d -prune | grep -v 'CVS' | xargs echo`
+foreach DEMO (${DEMOS})
+  echo Found demo $DEMO
+  set CLASSES=`ls ${DEMO}/*.java`
+  mkdir docs/$DEMO
+  javadoc \
+    -sourcepath $DEMO \
+    -classpath xml.jar:class \
+    -d docs/$DEMO \
+    -author \
+    -windowtitle "$DEMO" \
+    -link docs/api \
+    -nohelp \
+    -notree \
+    -nodeprecatedlist \
+    -nodeprecated \
+    $CLASSES
+  cat build/extra.css >> docs/${DEMO}/stylesheet.css
+end
