38026f157682a9c187c77faa86c41923c46e7b2d
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-03-10 11:35:34 +0000

    A demo to show how OrderNResidueList works.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@549 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ demos/seq/TestOrderNResidueList.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+import org.biojava.bio.seq.*;
+import org.biojava.bio.seq.tools.*;
+
+/**
+ * Demonstration of the OrderNResidueList class.
+ * <P>
+ * This program generates a random DNA sequence of length 10. It then constructs
+ * views of this sequence that contain di, tri, quad and penta -nucleotides.
+ * Each view is then printed out, so that you can check that the
+ * OrderNResidueList is the correct sequence.
+ */
+public class TestOrderNResidueList {
+  public static void main(String [] args)
+  throws Exception {
+    ResidueList res = createResidueList(10);
+    
+    for(int o = 1; o < 6; o++) {
+      ResidueList view = new OrderNResidueList(res, o);
+      System.out.println("Order " + o + " view:");
+      for(int i = 1; i <= view.length(); i++) {
+        System.out.println(i + ": " + view.residueAt(i).getName());
+      }
+      System.out.println("\n");
+    }
+  }
+  
+  public static ResidueList createResidueList(int length)
+  throws IllegalResidueException {
+    List l = new ArrayList(length);
+    for(int i = 0; i < length; i++) {
+      l.add(DNATools.forIndex((int) (4.0*Math.random())));
+    }
+    return new SimpleResidueList(DNATools.getAlphabet(), l);
+  }
+}
