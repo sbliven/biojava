6d5b8541e76f42a90ab4136c7150c4f89d729869
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-03-14 12:58:35 +0000

    The public interface for making DP objects from models.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@562 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/dp/DPFactory.java
@@ -0,0 +1,40 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+
+package org.biojava.bio.dp;
+
+public class DPFactory {
+  public static DP createDP(MarkovModel model)
+  throws Exception {
+    int heads = model.heads();
+    FlatModel flat = new FlatModel(model);
+    if(heads == 1) {
+      return new SingleDP(flat);
+    } else if(heads == 2) {
+      return new PairwiseDP(flat);
+    } else {
+      throw new UnsupportedOperationException(
+        "Can't create DPFactory for models with " + heads + " heads"
+      );
+    }
+  }
+}
