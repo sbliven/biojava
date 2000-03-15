b9a4809ba426f29df85018279ccd26a54c8220f7
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-03-15 18:18:20 +0000

    Added docs
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@569 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/seq/tools/GappedResidueList.java
+++ src/org/biojava/bio/seq/tools/GappedResidueList.java
@@ -29,15 +29,42 @@ import org.biojava.bio.seq.*;
 
 /**
  * This implementation of ResidueList wrappes another one, allowing you to insert gaps.
+ * <P>
+ * You could make a ResidueList that contains the gap character in its alphabet. However, this
+ * leaves you with a nasty problem if you wish to support gap-edit opperations. Also, the orriginal
+ * ResidueList must either be coppied or lost.
+ * <P>
+ * GappedResidueList solves these problems. It wraps up a source sequence, and views it through
+ * a data-structure that places gaps. You can add and remove the gaps by using the public API.
+ *
+ * @author Matthew Pocock
  */
 public class GappedResidueList extends AbstractResidueList {
+  /**
+   * The Alphabet - the same as source but guaranteed to include the gap character.
+   */
   private final Alphabet alpha;
+  
+  /**
+   * The ResidueList to view.
+   */
   private final ResidueList source;
+  
+  /**
+   * The list of ungapped blocks that align between source and this view.
+   */
   private final ArrayList blocks;
+  
+  /**
+   * The total length of the alignment - necisary to allow leading & trailing gaps.
+   */
   private int length;
   
   /**
-   * Finds the block containing the source coordinate indx.
+   * Finds the index of the block containing the source coordinate indx.
+   *
+   * @param indx  the index to find
+   * @return the index of the Block containing indx
    */
   protected final int findSourceBlock(int indx) {
     int i = blocks.size() / 2;
@@ -66,7 +93,13 @@ public class GappedResidueList extends AbstractResidueList {
   }
 
   /**
-   * Finds the index of the Block containing indx within the view range or -1.
+   * Finds the index of the Block containing indx within the view ranges.
+   * <P>
+   * If indx is not within a view block, then it is the index of a gap. The method will
+   * return -1.
+   *
+   * @param indx  the index to find within a view range.
+   * @return  the index of the block containing index, or -1 if no block contains it
    */
   protected final int findViewBlock(int indx) {
     int i = blocks.size() / 2;
@@ -97,7 +130,10 @@ public class GappedResidueList extends AbstractResidueList {
   }
 
   /**
-   * Finds the index of the Block before the gap at indx within the source range or -1.
+   * Finds the index of the Block before the gap at indx within the following gap.
+   *
+   * @param indx  the index to find within a gap
+   * @return  the index of the block with indx in the gap
    */
   protected final int findSourceGap(int indx) {
     int i = blocks.size() / 2;
@@ -128,7 +164,14 @@ public class GappedResidueList extends AbstractResidueList {
   }
   
   /**
-   * Finds the index of the Block before the gap at indx within the view range or -2.
+   * Finds the index of the Block before the gap at indx within the view range.
+   * <P>
+   * If indx is in-fact a real residue, then there will be no Block before it. In this
+   * case, the method returns -2. It returns -1 if indx is within the leading gaps and
+   * blocks.size()-1 if it is within the trailing gaps.
+   *
+   * @param indx  the index to find within a view range
+   * @return  the index of the block with indx in the following gap
    */
   protected final int findViewGap(int indx) {
     int i = blocks.size() / 2;
@@ -162,10 +205,24 @@ public class GappedResidueList extends AbstractResidueList {
     }
   }
 
+  /**
+   * Coordinate conversion from view to source.
+   *
+   * @param b the block containing indx
+   * @param   indx the index to project
+   * @return the position of indx projected from view to source
+   */
   protected final int viewToSource(Block b, int indx) {
     return indx - b.viewStart + b.sourceStart;
   }
   
+  /**
+   * Coordinate conversion from view to source.
+   *
+   * @param   indx the index to project
+   * @return the position of indx projected from view to source
+   * @throws IndexOutOfBoundsException if indx is not a valid view index
+   */
   public final int viewToSource(int indx)
   throws IndexOutOfBoundsException {
     if(indx < 1 || indx > length()) {
@@ -184,10 +241,24 @@ public class GappedResidueList extends AbstractResidueList {
     }
   }
   
+  /**
+   * Coordinate conversion from source to view.
+   *
+   * @param b the block containing indx
+   * @param   indx the index to project
+   * @return the position of indx projected from source to view
+   */
   protected final int sourceToView(Block b, int indx) {
     return indx - b.sourceStart + b.viewStart;
   }
   
+  /**
+   * Coordinate conversion from source to view.
+   *
+   * @param   indx the index to project
+   * @return the position of indx projected from source to view
+   * @throws IndexOutOfBoundsException if indx is not a valid source index
+   */
   public final int sourceToView(int indx)
   throws IndexOutOfBoundsException {
     if(indx < 1 || indx > source.length()) {
@@ -202,6 +273,14 @@ public class GappedResidueList extends AbstractResidueList {
     );
   }
   
+  /**
+   * Renumber the view indexes from block, adding delta to each offset.
+   * <P>
+   * This adjusts viewStart and viewEnd to be += delta for each block i->blocks.size(), 
+   * and sets the total length to += delta.
+   *
+   * @param i the first 
+   */
   protected final void renumber(int i, int delta) {
     for(int j = i; j < blocks.size(); j++) {
       Block b = (Block) blocks.get(j);
@@ -216,6 +295,9 @@ public class GappedResidueList extends AbstractResidueList {
    * <P>
    * this.residueAt(pos) will then return gap. Adding a gap at 1 will prepend gaps. Adding
    * a gap at (length+1) will append a gap.
+   *
+   * @param pos  the position to add a gap before
+   * @throws IndexOutOfBoundsException if pos is not within 1->length+1
    */
   public void addGapInView(int pos)
   throws IndexOutOfBoundsException {
@@ -228,6 +310,10 @@ public class GappedResidueList extends AbstractResidueList {
    * this.residueAt(i) will then return gap for i = (pos .. pos+count-1).
    * Adding gaps at 1 will prepend gaps. Adding
    * gaps at (length+1) will append gaps.
+   *
+   * @param pos  the position to add a gap before
+   * @param length  the number of gaps to insert
+   * @throws IndexOutOfBoundsException if pos is not within 1->length+1
    */
   public void addGapsInView(int pos, int length)
   throws IndexOutOfBoundsException {
@@ -274,7 +360,10 @@ public class GappedResidueList extends AbstractResidueList {
   }
   
   /**
-   * Add length gaps at pos within the source coordinates.
+   * Add a gap at pos within the source coordinates.
+   *
+   * @param pos where to add the gap
+   * @throws IndexOutOfBoundsException if pos is not within 1->source.length()
    */
   public void addGapInSource(int pos)
   throws IndexOutOfBoundsException {
@@ -283,6 +372,10 @@ public class GappedResidueList extends AbstractResidueList {
   
   /**
    * Add length gaps at pos within the source coordinates.
+   *
+   * @param pos where to add the gap
+   * @param length  how many gaps to add
+   * @throws IndexOutOfBoundsException if pos is not within 1->source.length()
    */
   public void addGapsInSource(int pos, int length) {
     if(pos < 1 || pos > (length() + 1)) {
@@ -328,9 +421,10 @@ public class GappedResidueList extends AbstractResidueList {
   /**
    * Remove a single gap at position pos in this GappedResidueList.
    *
+   * @param pos where to remove the gap
    * @throws IndexOutOfBoundsException if pos is not within 1..length
    * @throws IllegalResidueException if the residue at pos is not a gap
-3   */
+   */
   public void removeGap(int pos)
   throws IndexOutOfBoundsException, IllegalResidueException { 
     if(pos < 1 || pos > length()) {
@@ -360,9 +454,18 @@ public class GappedResidueList extends AbstractResidueList {
     }
   }
   
+  /**
+   * Remove some gaps at position pos in this GappedResidueList.
+   *
+   * @param pos where to remove the gaps
+   * @param length how many to remove
+   * @throws IndexOutOfBoundsException if pos is not within 1..length() or if
+   *         some of the residues within pos->(pos+length-1) are not gap residues
+   * @throws IllegalResidueException if the residue at pos is not a gap
+   */
   public void removeGaps(int pos, int length)
   throws IndexOutOfBoundsException, IllegalResidueException {
-    int end = pos + length;
+    int end = pos + length - 1;
     if(
       pos < 1 ||
       length < 1 ||
@@ -430,7 +533,12 @@ public class GappedResidueList extends AbstractResidueList {
       return source.residueAt(b.sourceStart - b.viewStart + indx);
     }
   }
-  
+
+  /**
+   * Create a new GappedResidueList that will view source.
+   *
+   * @param source  the underlying sequence
+   */
   public GappedResidueList(ResidueList source) {
     this.source = source;
     this.alpha = AlphabetManager.instance().getGappedAlphabet(source.alphabet());
@@ -449,6 +557,11 @@ public class GappedResidueList extends AbstractResidueList {
   
   /**
    * An aligned block.
+   * <P>
+   * The alignment is actualy stoored as a list of these objects. Each block is contiguous
+   * with the next in the source fields, but may be gapped in the view fields.
+   *
+   * @author Matthew Pocock
    */
   private final class Block {
     public int sourceStart, sourceEnd;
