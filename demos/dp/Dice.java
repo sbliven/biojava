3965cac7ca106332b438f26111a09e4d41a15208
sh5 <sh5@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-03-21 15:51:25 +0000

    *** empty log message ***
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@582 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/dp/Dice.java
+++ demos/dp/Dice.java
@@ -7,7 +7,8 @@ public class Dice
     public static void main(String[] args) throws Exception
     {
     	Residue[] rolls=new Residue[6];
-    
+	
+    	//set up the dice alphabet
     	SimpleAlphabet diceAlphabet=new SimpleAlphabet();
     	diceAlphabet.setName("DiceAlphabet");
     
@@ -25,6 +26,7 @@ public class Dice
 	casino.addState(fair);
 	casino.addState(loaded);
 	
+	//set up transitions between states.
 	casino.createTransition(casino.magicalState(),fair);
 	casino.createTransition(casino.magicalState(),loaded);
 	casino.createTransition(fair,casino.magicalState());
@@ -34,32 +36,40 @@ public class Dice
 	casino.createTransition(fair,fair);
 	casino.createTransition(loaded,loaded);
 	
-	for(int i=1;i<rolls.length;i++)
+	//set up emission probabilities.
+	for(int i=0;i<rolls.length;i++)
 	{
 	    fair.setWeight(rolls[i],-Math.log(6));
 	    loaded.setWeight(rolls[i],-Math.log(10));
 	}
 	loaded.setWeight(rolls[5],-Math.log(2));
 	
-	casino.setTransitionScore(fair,loaded,Math.log(0.04));
-	casino.setTransitionScore(loaded,fair,Math.log(0.09));
-	casino.setTransitionScore(loaded,loaded,Math.log(0.9));
-	casino.setTransitionScore(fair,fair,Math.log(0.95));
-	
-	casino.setTransitionScore(casino.magicalState(),fair,Math.log(0.8));
+	//set up transition scores.
+	casino.setTransitionScore(casino.magicalState(),fair,  Math.log(0.8));
 	casino.setTransitionScore(casino.magicalState(),loaded,Math.log(0.2));
+
+	casino.setTransitionScore(fair,loaded,               Math.log(0.04));
+	casino.setTransitionScore(fair,fair,                 Math.log(0.95));
 	casino.setTransitionScore(fair,casino.magicalState(),Math.log(0.01));
+	
+	casino.setTransitionScore(loaded,fair,                 Math.log(0.09));
+	casino.setTransitionScore(loaded,loaded,               Math.log(0.90));
 	casino.setTransitionScore(loaded,casino.magicalState(),Math.log(0.01));
 	
 	DP dp=DPFactory.createDP(casino);
 	StatePath obs_rolls = dp.generate(300);
 	
+	ResidueList roll_sequence = obs_rolls.residueListForLabel(StatePath.SEQUENCE);
+	ResidueList[] res_array = {roll_sequence};
+	StatePath v = dp.viterbi(res_array);
+	
+	//print out obs_sequence, output, state symbols.
 	for(int i = 1; i <= obs_rolls.length(); i++) {
 	  System.out.println(i + " " +
 	    obs_rolls.residueAt(StatePath.SEQUENCE, i).getSymbol() + " " +
-	    obs_rolls.residueAt(StatePath.STATES, i).getSymbol()
+	    obs_rolls.residueAt(StatePath.STATES, i).getSymbol() + " " + 
+	    v.residueAt(StatePath.STATES, i).getSymbol()
           );
-	}
-	
+	}	
     }
 }
