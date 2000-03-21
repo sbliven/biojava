53e99dfc315dc86b175a2bbabcd3c2aaaa5cbbad
sh5 <sh5@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-03-21 16:36:15 +0000

    Dice example - added comments.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@583 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/dp/Dice.java
+++ demos/dp/Dice.java
@@ -1,6 +1,44 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
 import org.biojava.bio.seq.*;
 import org.biojava.bio.seq.tools.*;
 import org.biojava.bio.dp.*;
+
+/**
+ * This demo file is a simulation of the "The occasionally dishonest casino" example
+ * from the book by R. Durbin, S. Eddy, A. Krogh, G. Mitchison, 
+ * "Biological Sequence Analysis", 
+ * Chapter 3 Markov Chains and hidden Markov models, Section 2, pp55-57.
+ * <P>
+ * Use: <code>Dice</code>
+ * <p>
+ * The output consists of three lines:  line 1 represents the output sequence generated
+ * by the hidden markov model (f for fair and l for loaded).  Line 2 contains the name of 
+ * the die which emitted the corresponding output symbol.  Line 3 shows the state 
+ * sequence predicted by the Viterbi algorithm. 
+ * <P>
+ *
+ * @author Samiul Hasan
+ */
 
 public class Dice
 {
@@ -18,7 +56,7 @@ public class Dice
 	    diceAlphabet.addResidue(rolls[i-1]);
 	}  
   
-  int [] advance = { 1 };
+    	int [] advance = { 1 };
 	EmissionState fair   = StateFactory.DEFAULT.createState(diceAlphabet, advance, "fair");
 	EmissionState loaded = StateFactory.DEFAULT.createState(diceAlphabet, advance, "loaded");
 	
@@ -64,12 +102,19 @@ public class Dice
 	StatePath v = dp.viterbi(res_array);
 	
 	//print out obs_sequence, output, state symbols.
-	for(int i = 1; i <= obs_rolls.length(); i++) {
-	  System.out.println(i + " " +
-	    obs_rolls.residueAt(StatePath.SEQUENCE, i).getSymbol() + " " +
-	    obs_rolls.residueAt(StatePath.STATES, i).getSymbol() + " " + 
-	    v.residueAt(StatePath.STATES, i).getSymbol()
-          );
+	for(int i = 1; i <= obs_rolls.length()/60; i++) {
+	  for(int j=i*60; j<Math.min((i+1)*60, obs_rolls.length()); j++)  {
+	    System.out.print(obs_rolls.residueAt(StatePath.SEQUENCE, j+1).getSymbol());
+	  }
+	  System.out.print("\n");
+	  for(int j=i*60; j<Math.min((i+1)*60, obs_rolls.length()); j++)  {
+	    System.out.print(obs_rolls.residueAt(StatePath.STATES, j+1).getSymbol());
+	  }
+	  System.out.print("\n");
+	  for(int j=i*60; j<Math.min((i+1)*60, obs_rolls.length()); j++)  {
+	    System.out.print(v.residueAt(StatePath.STATES, j+1).getSymbol());
+	  }
+	  System.out.print("\n\n");	  
 	}	
     }
 }
