b207802b3ba940e1461e6b605baa55662b30a806
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-04-08 15:24:13 +0000

    You now have the choice whether or not to build a package, or a package and all sub-packages.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@603 7c6358e6-4a41-0410-a743-a5b2a554c398

--- build/buildpackage.sh
+++ build/buildpackage.sh
@@ -1,3 +1,3 @@
 package=`echo $1 | tr '.' '/'`
-find src/${package} -name "*.java" | xargs javac -classpath xml.jar:class -sourcepath src -d class
+ls src/${package} -name "*.java" | xargs javac -classpath xml.jar:class -sourcepath src -d class
 jar -uf biojava.jar -C class ${package}
