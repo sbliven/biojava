2a733ab48178310f955e9abd2cdb8288d5aabcf4
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-04-08 15:25:34 +0000

    And now it works!
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@604 7c6358e6-4a41-0410-a743-a5b2a554c398

--- build/buildpackage.sh
+++ build/buildpackage.sh
@@ -1,3 +1,3 @@
 package=`echo $1 | tr '.' '/'`
-ls src/${package} -name "*.java" | xargs javac -classpath xml.jar:class -sourcepath src -d class
+ls src/${package}/*.java | xargs javac -classpath xml.jar:class -sourcepath src -d class
 jar -uf biojava.jar -C class ${package}
