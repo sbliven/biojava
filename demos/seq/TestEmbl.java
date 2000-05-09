aacdd36a885b716a525db8c8c5cc6d889ba76622
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-05-09 14:37:27 +0000

    Simple test scripts for embl and genbank file formats - just counts the number of features read in.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@663 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ demos/seq/TestEmbl.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+import org.biojava.bio.*;
+import org.biojava.bio.symbol.*;
+import org.biojava.bio.seq.*;
+import org.biojava.bio.seq.io.*;
+
+public class TestEmbl {
+  public static void main(String [] args) {
+    try {
+      if(args.length != 1) {
+        throw new Exception("Use: TestEmbl emblFile");
+      }
+      
+      File emblFile = new File(args[0]);
+      SequenceFormat eFormat = new EmblFormat();
+      BufferedReader eReader = new BufferedReader(
+        new InputStreamReader(new FileInputStream(emblFile)));
+      SequenceFactory sFact = new SimpleSequenceFactory();
+      Alphabet alpha = DNATools.getAmbiguity();
+      SymbolParser rParser = alpha.getParser("token");
+      SequenceIterator seqI =
+        new StreamReader(eReader, eFormat, rParser, sFact);
+        
+      while(seqI.hasNext()) {
+        Sequence seq = seqI.nextSequence();
+        System.out.println(seq.getName() + " has " + seq.countFeatures() + " features");
+      }
+    } catch (Throwable t) {
+      t.printStackTrace();
+      System.exit(1);
+    }
+  }
+}
---
+++ demos/seq/TestGenbank.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+import org.biojava.bio.*;
+import org.biojava.bio.symbol.*;
+import org.biojava.bio.seq.*;
+import org.biojava.bio.seq.io.*;
+
+public class TestGenbank {
+  public static void main(String [] args) {
+    try {
+      if(args.length != 1) {
+        throw new Exception("Use: TestGenbank genbankFile");
+      }
+      
+      File genbankFile = new File(args[0]);
+      SequenceFormat gFormat = new GenbankFormat();
+      BufferedReader gReader = new BufferedReader(
+        new InputStreamReader(new FileInputStream(genbankFile)));
+      SequenceFactory sFact = new SimpleSequenceFactory();
+      Alphabet alpha = DNATools.getAmbiguity();
+      SymbolParser rParser = alpha.getParser("token");
+      SequenceIterator seqI =
+        new StreamReader(gReader, gFormat, rParser, sFact);
+        
+      while(seqI.hasNext()) {
+        Sequence seq = seqI.nextSequence();
+        System.out.println(seq.getName() + " has " + seq.countFeatures() + " features");
+      }
+    } catch (Throwable t) {
+      t.printStackTrace();
+      System.exit(1);
+    }
+  }
+}
