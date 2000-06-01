3272500f608e7785446851e0bdc6373af8bfe448
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-06-01 14:21:12 +0000

    Magical set of all objects in a class.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@690 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/acedb/socket/SocketClazzSet.java
@@ -0,0 +1,75 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.acedb.socket;
+
+/**
+ * AceSet representing all the objects in a class
+ */
+
+class SocketClazzSet implements AceSet {
+    private AceURL url;
+    private AceSet parent;
+    private AceSet _allMembers = null;
+
+    public SocketClazzSet(AceURL url, AceSet parent) {
+	this.url = url;
+	this.parent = parent;
+    }
+
+    private AceSet allMembers() throws AceException {
+	if (_allMembers == null)
+	    _allMembers = filter("*");
+	return _allMembers;
+    }
+
+    public int size() throws AceException {
+	return allMembers().size();
+    }
+
+    public Iterator nameIterator() throws AceException {
+	return allMembers().nameIterator();
+    }
+
+    public Iterator nameIterator() throws AceException {
+	return allMembers().nameIterator();
+    }
+
+    public boolean contains(String name) throws AceException {
+	return allMembers().contains(name);
+    }
+
+    public AceSet retrieve(String name) throws AceException {
+	return Ace.fetch(url.relative(name));
+    }
+
+    public AceSet filter(String rule) throws AceException {
+	return Ace.fetch(url.relative("?" + name));
+    }
+
+    public AceSet getParent() {
+	return parent;
+    }
+
+    public AceURL toURL() {
+	return url;
+    }
+}
