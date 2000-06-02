3f602be54b46fe6eb725f8ab59619b89adb00fbb
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-06-02 08:53:25 +0000

    Added this file - oops!
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@700 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/acedb/AceURLException.java
@@ -0,0 +1,19 @@
+package org.acedb;
+
+public class AceURLException extends AceException {
+  public AceURLException() {
+    super();
+  }
+  
+  public AceURLException(String message) {
+    super(message);
+  }
+  
+  public AceURLException(Throwable cause) {
+    super(cause);
+  }
+  
+  public AceURLException(Throwable cause, String message) {
+    super(cause, message);
+  }
+}
