737c8895fc6cfd30ad1543d9abd0f073d994aa03
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-07-12 16:01:55 +0000

    Added AnnotatedSequenceDB.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@773 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/seq/db/AnnotatedSequenceDB.java
@@ -0,0 +1,78 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio.seq.db;
+
+import org.biojava.bio.symbol.*;
+import org.biojava.bio.seq.*;
+import org.biojava.bio.*;
+import java.util.*;
+
+/**
+ * SequenceDB implementation which lazily applies a SequenceAnnotator
+ * to sequences retrieved from a SequenceDB.
+ * 
+ * @author Thomas Down
+ */
+
+public class AnnotatedSequenceDB implements SequenceDB {
+    private SequenceDB parent;
+    private SequenceAnnotator annotator;
+
+    public AnnotatedSequenceDB(SequenceDB parent, SequenceAnnotator a) {
+	this.parent = parent;
+	this.annotator = a;
+    }
+
+    public String getName() {
+	return parent.getName() + " (" + annotator.toString() + ")";
+    }
+
+    public Sequence getSequence(String id) throws BioException {
+	return doAnnotation(parent.getSequence(id));
+    }
+
+    public Set ids() {
+	return parent.ids();
+    }
+
+    public SequenceIterator sequenceIterator() {
+	return new SequenceIterator() {
+	    SequenceIterator pi = parent.sequenceIterator();
+
+	    public boolean hasNext() {
+		return pi.hasNext();
+	    }
+
+	    public Sequence nextSequence() throws BioException {
+		return doAnnotation(pi.nextSequence());
+	    }
+	} ;
+    }
+
+    protected Sequence doAnnotation(Sequence seq) throws BioException {
+	try {
+	    return annotator.annotate(seq);
+	} catch (IllegalAlphabetException ex) {
+	    throw new BioException(ex, "Couldn't apply annotator " + annotator.toString() + " to " + seq.getURN());
+	}
+    }
+}
