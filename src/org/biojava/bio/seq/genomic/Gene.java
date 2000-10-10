b47cc2f1c23f433adaf31b77d27f48e1f93b8dcb
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-10-10 12:09:57 +0000

    Gene interface
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@920 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/seq/genomic/Gene.java
@@ -0,0 +1,39 @@
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio.seq.genomic;
+
+import java.util.*;
+import org.biojava.bio.seq.*;
+
+/**
+ * The root of a feature tree representing a transcribed region of a genome.
+ *
+ * @author Thomas Down
+ * @author Matthew Pocock
+ * @since 1.1
+ */
+
+public interface Gene extends StrandedFeature {
+    public Set getExons();
+
+    public static class Template extends Feature.Template {
+    }
+}
