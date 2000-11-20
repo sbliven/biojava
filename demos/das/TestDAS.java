9cb5192018671c2b7b23a9028a616d0675b4b96a
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-11-20 11:52:49 +0000

    Added quick DAS client test
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@971 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ demos/das/TestDAS.java
@@ -0,0 +1,41 @@
+package das;
+
+import org.biojava.bio.*;
+import org.biojava.bio.seq.*;
+import org.biojava.bio.seq.db.*;
+import org.biojava.bio.symbol.*;
+import org.biojava.bio.program.das.*;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class TestDAS {
+    public static void main(String[] args) throws Exception {
+	if (args.length != 1)
+	    throw new Exception("java das.TestDAS <url>");
+	String dbURLString = args[0];
+
+	URL dbURL = new URL(dasURLString);
+	
+	DASSequenceDB dasDB = new DASSequenceDB(dbURL);
+	System.out.println("Top-level entry points:");
+	Set ids = dasDB.ids();
+	for (Iterator i = ids.iterator(); i.hasNext(); ) {
+	    System.out.println(i.next().toString());
+	}
+
+	System.out.println("All entry points:");
+	Set allIds = dasDB.allEntryPointsDB().ids();
+	for (Iterator i = allIds.iterator(); i.hasNext(); ) {
+	    System.out.println(i.next().toString());
+	}
+
+	DASSequence dasSeq = (DASSequence) dasDB.getSequence((String) ids.iterator().next());
+	// dasSeq.addAnnotationSource(annoURL);
+	// dasSeq.addAnnotationSource(miscURL);
+	System.out.println("Length: " + dasSeq.length());
+	System.out.println("1st 10 bases: " + dasSeq.subStr(1, 10));
+	System.out.println("Feature count: " + dasSeq.countFeatures());
+    }
+}
