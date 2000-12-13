7a24b6d2034c6cd7e630f57a0bbdd0809adc298a
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2000-12-13 18:03:39 +0000

    New `really simple' Symbol implementation -- this is what we use for fundamental symbols in normal alphabets.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1012 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/symbol/FundamentalAtomicSymbol.java
@@ -0,0 +1,72 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+
+package org.biojava.bio.symbol;
+
+import java.util.*;
+import java.io.*;
+
+import org.biojava.utils.*;
+import org.biojava.bio.*;
+
+/**
+ * An atomic symbol consisting only of itself.  This is the
+ * fundamental type of symbol, and needs a special implementation.
+ *
+ * @author Thomas Down
+ * @since 1.1
+ */
+
+public class FundamentalAtomicSymbol extends AbstractSymbol implements AtomicSymbol {
+    private final String name;
+    private final char token;
+    private final Annotation annotation;
+
+    public FundamentalAtomicSymbol(String name, 
+				   char token,
+				   Annotation annotation)
+    {
+	this.name = name;
+	this.token = token;
+	this.annotation = annotation;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public char getToken() {
+	return token;
+    }
+
+    public Alphabet getMatches() {
+	return new SingletonAlphabet(this);
+    }
+
+    public List getSymbols() {
+	return Collections.nCopies(1, this);
+    }
+
+    public Annotation getAnnotation() {
+	return annotation;
+    }
+}
