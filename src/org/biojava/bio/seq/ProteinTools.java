220e216677a95fc3fbb9e8e97127824600d3a228
mjones <mjones@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-01-19 16:59:35 +0000

    ProteinTools was modified to include two SymbolPropertyTables. One for Average Masses and one for MonoMasses.
    It gets the data or these tables from
    
    org/biojava/bio/symbol/ResidueProperties.xml"
    
    
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1056 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/seq/ProteinTools.java
+++ src/org/biojava/bio/seq/ProteinTools.java
@@ -10,7 +10,7 @@
  *
  * Copyright for this code is held jointly by the individual
  * authors.  These should be listed in @author doc comments.
- *
+
  * For more information on the BioJava project and its aims,
  * or to join the biojava-l mailing list, visit the home page
  * at:
@@ -25,6 +25,22 @@ import java.util.*;
 import org.biojava.bio.*;
 import org.biojava.bio.symbol.*;
 
+
+import com.sun.xml.parser.Resolver;
+import com.sun.xml.tree.XmlDocument;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import java.io.IOException;
+
+import java.util.ResourceBundle;
+import java.util.MissingResourceException;
+import java.net.URL;
+
 /**
  * The central port-of-call for all information and functionality specific to
  * SymbolLists over the protein alphabet.
@@ -32,28 +48,125 @@ import org.biojava.bio.symbol.*;
  * @author Matthew Pocock
  */
 public class ProteinTools {
-  private static final FiniteAlphabet proteinAlpha;
-  private static final FiniteAlphabet proteinTAlpha;
-  
-  static {
-    try {
-      proteinAlpha = (FiniteAlphabet) AlphabetManager.alphabetForName("PROTEIN");
-      proteinTAlpha = (FiniteAlphabet) AlphabetManager.alphabetForName("PROTEIN-TERM");
-    } catch (Exception e) {
-      throw new BioError(e, " Could not initialize ProteinTools");
+    private static final FiniteAlphabet proteinAlpha;
+    private static final FiniteAlphabet proteinTAlpha;
+
+    private static final Map propertyTableMap = new HashMap();
+
+    static {
+        try {
+            proteinAlpha = (FiniteAlphabet) AlphabetManager.alphabetForName("PROTEIN");
+            proteinTAlpha = (FiniteAlphabet) AlphabetManager.alphabetForName("PROTEIN-TERM");
+        } catch (Exception e) {
+            throw new BioError(e, " Could not initialize ProteinTools");
+        }
     }
-  }
-  /**
-  *Gets the protein alphabet
-  */
-  public static final FiniteAlphabet getAlphabet() {
-    return proteinAlpha;
-  }
-  
-  /**
-  *Gets the protein alphabet including the translation termination symbols
-  */
-  public static final FiniteAlphabet getTAlphabet() {
-    return proteinTAlpha;
-  }
+
+
+    static {
+
+        Document doc = null;
+        try {
+            URL proteaseManagerURL = ProteinTools.class.getClassLoader().getResource(
+            "org/biojava/bio/symbol/ResidueProperties.xml"
+            );
+            //If I try and do this here on compile it says "An exception can't be thrown by an initializer"
+            InputSource is = Resolver.createInputSource(proteaseManagerURL, true);
+            doc = XmlDocument.createXmlDocument(is, true);
+
+
+
+        }catch (MissingResourceException mre) {
+            System.err.println(mre.getMessage());
+        }catch(Exception e){//err
+            e.printStackTrace();
+        }
+               
+        try {
+            SimpleSymbolPropertyTable simplePropertyTable = new SimpleSymbolPropertyTable(
+            getAlphabet(),
+            SymbolPropertyTable.MONO_MASS
+            );
+            Iterator it = getAlphabet().iterator();
+            NodeList children = doc.getDocumentElement().getChildNodes();
+            
+            while(it.hasNext()){
+                Symbol s = (Symbol)it.next();
+                //  simplePropertyTable.setDoubleProperty(s, "1202.00");
+                for(int i = 0; i < children.getLength(); i++) {
+                    Element child = (Element) children.item(i);                   
+                    if(child.getNodeName().equals("residue")) {
+                        if(child.getAttribute("token").equals(s.getToken()+"")){
+                            
+                            NodeList properyNodes = child.getChildNodes();
+                            for(int j = 0; j < properyNodes.getLength(); j++)
+                            {
+                                Element el = (Element) properyNodes.item(j);
+                                String name = el.getAttribute("name");
+                                if(name.equals(SymbolPropertyTable.MONO_MASS)) {
+                                    String value = el.getAttribute("value");
+                                    simplePropertyTable.setDoubleProperty(s, value);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            propertyTableMap.put(SymbolPropertyTable.MONO_MASS, (SymbolPropertyTable) simplePropertyTable);
+
+            //Build AVG_MASS table
+            simplePropertyTable = new SimpleSymbolPropertyTable(
+            getAlphabet(),
+            SymbolPropertyTable.AVG_MASS
+            );
+            it = getAlphabet().iterator();
+
+            while(it.hasNext()){
+                Symbol s = (Symbol)it.next();
+                //  simplePropertyTable.setDoubleProperty(s, "1202.00");
+                for(int i = 0; i < children.getLength(); i++) {
+                    Element child = (Element) children.item(i);                   
+                    if(child.getNodeName().equals("residue")) {
+                        if(child.getAttribute("token").equals(s.getToken()+"")){                           
+                            NodeList properyNodes = child.getChildNodes();
+                            for(int j = 0; j < properyNodes.getLength(); j++)
+                            {
+                                Element el = (Element) properyNodes.item(j);
+                                String name = el.getAttribute("name");
+                                 
+                                if(name.equals(SymbolPropertyTable.AVG_MASS)) {
+                                    String value = el.getAttribute("value");
+                                    simplePropertyTable.setDoubleProperty(s, value);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            propertyTableMap.put(SymbolPropertyTable.AVG_MASS, (SymbolPropertyTable) simplePropertyTable);
+        } catch (Exception e) {
+            throw new BioError(e, " Could not initialize ProteinTools");
+        }
+    }
+    /**
+     *Gets the protein alphabet
+     */
+    public static final FiniteAlphabet getAlphabet() {
+        return proteinAlpha;
+    }
+
+    /**
+     *Gets the protein alphabet including the translation termination symbols
+     */
+    public static final FiniteAlphabet getTAlphabet() {
+        return proteinTAlpha;
+    }
+
+    public static final SymbolPropertyTable getSymbolPropertyTable(String name)
+    {
+        return (SymbolPropertyTable)propertyTableMap.get(name);
+    }
 }
