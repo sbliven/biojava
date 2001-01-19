c5ce726d51a4ac9543071050d8de939dfea9a0b6
mjones <mjones@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-01-19 16:25:38 +0000

    This class calculates the peptides of produced by the digestion of a protein Sequence. It adds the peptides as features onto the Sequence object. It provides methods for setting the sequence, protease, and number of mixed cleavages. If more then one mixed cleavage is allowed then both the 0 missed cleavage and the greater then 0 missed cleages are all included as features on the sequence.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1050 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/proteomics/Digest.java
@@ -0,0 +1,184 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio.proteomics;
+
+import org.biojava.bio.seq.Sequence;
+import org.biojava.bio.seq.Feature;
+import org.biojava.bio.symbol.Location;
+import org.biojava.bio.symbol.RangeLocation;
+import org.biojava.bio.Annotation;
+
+import org.biojava.bio.BioException;
+import org.biojava.utils.ChangeVetoException;
+import java.util.ListIterator;
+import java.util.LinkedList;
+
+
+/**
+ * This class contains methods for calculating the results of proteolytic digestion
+ * of a protein sequence.
+ * @author Michael Jones
+ */
+public class Digest {
+
+    private Protease protease = new Protease();
+
+    private Sequence sequence;
+
+    private int maxMissedCleavages = 0;
+    
+    public static String PEPTIDE_FEATURE_TYPE = "Peptide";
+    
+    public LinkedList peptideQue; 
+    // private
+
+    /** Creates new Digest */
+    public Digest() {
+    }
+
+    public void setProtese(Protease protease)
+    {
+        this.protease = protease;
+    }
+
+    public void setSequence(Sequence sequence)
+    {
+        this.sequence = sequence;
+    }
+
+    public Sequence getSequence()
+    {
+        return sequence;
+    }
+    
+    public void setMaxMissedCleavages(int maxMissedCleavages)
+    {
+        this.maxMissedCleavages = maxMissedCleavages;
+    }
+
+    /** Adds peptides as features to the Sequence in this class.
+     * For Example:
+     * <CODE>
+     * Protease protease = Protease.getProteaseByName(Protease.TRYPSIN);
+     *         Sequence sequence = ...
+     *         Digest bioJavaDigest = new Digest();
+     *
+     *         bioJavaDigest.setMaxMissedCleavages(2);
+     *         bioJavaDigest.setProtese(Protease.getProteaseByName(Protease.ASP_N));
+     *         bioJavaDigest.setSequence(sequence);
+     *         bioJavaDigest.addDigestFeatures();
+     * </CODE>
+     * @throws BioException
+     */
+    public void addDigestFeatures() throws BioException, ChangeVetoException 
+    {
+        peptideQue = new LinkedList();
+        String cleaveSites = protease.getCleaveageResidues();
+        boolean endoProtease = protease.isEndoProtease();
+        String notCleave = protease.getNotCleaveResidues();
+        int nTerm = 1;
+
+        if(cleaveSites == null || notCleave == null){
+            throw new BioException("Protease contains null parameter");
+        }
+        for (int j = 1; j < sequence.length(); j++) {
+                for (int i = 0; i < cleaveSites.length(); i++) {
+                        if (sequence.symbolAt(j).getToken() == cleaveSites.charAt(i)) {
+                                if (endoProtease) {
+                                        boolean cleave = true;
+                                        if (j < sequence.length() - 1)  {
+                                                if (notCleave.indexOf(sequence.symbolAt(j + 1).getToken()) != -1) {
+                                                        cleave = false;
+                                                }
+                                        }
+
+                                        if (cleave)  {
+                                                Location loc = new RangeLocation(nTerm, j);
+                                                peptideQue.add(loc);
+                                                nTerm = j + 1;
+                                        }
+                                } else {
+                                    if (j > 0) {
+                                            Location loc = new RangeLocation(nTerm, j-1);
+                                            peptideQue.add(loc);
+                                            nTerm = j;
+                                    }
+                                }
+                                break;
+                        }
+                }			
+        }				
+
+        if (nTerm < sequence.length()) {      
+             Location loc = new RangeLocation(nTerm, sequence.length());
+             peptideQue.add(loc);
+        }
+        
+        addMissedCleavages();
+        
+        //Now add the locations as Peptide freatures to the Sequence
+        for(ListIterator li = peptideQue.listIterator(); li.hasNext(); ){
+            createPeptideFeature((Location)li.next());
+        }
+    }
+    
+    private void addMissedCleavages() throws BioException
+    {   
+        LinkedList missedList = new LinkedList();
+        if(maxMissedCleavages>0){           
+            for(ListIterator li = peptideQue.listIterator(); li.hasNext(); ){
+                Location loc = (Location)li.next();
+                Location loc2 = null;
+                int min = loc.getMin();
+                int max = 0;
+
+                //Get the numMissedCleavages location ahead of the current location
+                int numAdvanced = 0;
+                for(int i=0; i<maxMissedCleavages; i++){
+                   if(li.hasNext()) {
+                      numAdvanced++;
+                      loc2 = ((Location)li.next());
+                      max = loc2.getMax();
+                      missedList.add(new RangeLocation(min, max));
+                   }                    
+                }               
+                //Revert back to the original location
+                for(int i=0; i<numAdvanced; i++){
+                   loc = ((Location)li.previous());
+                }
+            }      
+            //Add all the missed peptides to the overall list
+            peptideQue.addAll(missedList);
+        }
+    }
+    
+    private void createPeptideFeature(Location loc) 
+                    throws BioException, ChangeVetoException
+    {
+       Feature.Template template = new Feature.Template();
+       template.type = PEPTIDE_FEATURE_TYPE;
+       template.source = this.getClass().getName();
+       template.location = loc;
+       template.annotation = Annotation.EMPTY_ANNOTATION;
+       sequence.createFeature(template);
+    }
+}
