2fa2027bd5074efbf30eb6f910a6d58d63091233
gcox <gcox@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-02-15 13:58:51 +0000

    Bug fix where the four-argument version of the constructor was dropping its last argument.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1141 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/utils/ChangeEvent.java
+++ src/org/biojava/utils/ChangeEvent.java
@@ -5,16 +5,16 @@
  * terms of the GNU Lesser General Public Licence.  This should
  * be distributed with the code.  If you do not have a copy,
  * see:
- * 
+ *
  * http://www.gnu.org/copyleft/lesser.html
- * 
+ *
  * Copyright for this code is held jointly by the individual
  * authors.  These should be listed in @author doc comments.
- * 
+ *
  * For more information on the BioJava project and its aims,
  * or to join the biojava-l mailing list, visit the home page
  * at:
- * 
+ *
  * http://www.biojava.org
  */
 
@@ -23,12 +23,13 @@ package org.biojava.utils;
 import java.util.*;
 
 /**
- *  Event which encapsulates a change in any mutable BioJava object. 
+ *  Event which encapsulates a change in any mutable BioJava object.
  *
- * @author     Thomas Down 
+ * @author     Thomas Down
  * @author     Matthew Pocock
- * @created    September 29, 2000 
- * @since      1.1 
+ * @author     Greg Cox
+ * @created    September 29, 2000
+ * @since      1.1
  */
 
 public class ChangeEvent extends EventObject {
@@ -39,10 +40,10 @@ public class ChangeEvent extends EventObject {
 
 
   /**
-   *  Construct a ChangeEvent with no change details. 
+   *  Construct a ChangeEvent with no change details.
    *
-   * @param  source  The object being changed. 
-   * @param  type    The type of change being made. 
+   * @param  source  The object being changed.
+   * @param  type    The type of change being made.
    */
   public ChangeEvent(Object source, ChangeType type) {
     this(source, type, null, null, null);
@@ -50,59 +51,59 @@ public class ChangeEvent extends EventObject {
 
 
   /**
-   *  
+   *
    * Construct a ChangeEvent specifying a new value for
    * a property, or an object to be added to a collection.
    *
-   * @param  source  The object being changed. 
-   * @param  type    The type of change being made. 
-   * @param  change  The new value of the property being changed. 
+   * @param  source  The object being changed.
+   * @param  type    The type of change being made.
+   * @param  change  The new value of the property being changed.
    */
   public ChangeEvent(
-    Object source, 
-    ChangeType type, 
+    Object source,
+    ChangeType type,
     Object change
   ) {
     this(source, type, change, null, null);
   }
 
   /**
-   *  
+   *
    * Construct a ChangeEvent specifying a new value for
    * a property, and giving the previous value.
    *
-   * @param  source    The object being changed. 
-   * @param  type      The type of change being made. 
-   * @param  change    The new value of the property being changed. 
-   * @param  previous  The old value of the property being changed. 
+   * @param  source    The object being changed.
+   * @param  type      The type of change being made.
+   * @param  change    The new value of the property being changed.
+   * @param  previous  The old value of the property being changed.
    */
   public ChangeEvent(
-      Object source, 
-      ChangeType type, 
-      Object change, 
+      Object source,
+      ChangeType type,
+      Object change,
       Object previous
   ) {
-    this(source, type, change, null, null);
+    this(source, type, change, previous, null);
   }
 
 
   /**
-   *  
+   *
    * Construct a ChangeEvent to be fired because another ChangeEvent has
    * been received from a property object.
    *
-   * @param  source    The object being changed. 
-   * @param  type      The type of change being made. 
-   * @param  change    The new value of the property being changed. 
-   * @param  previous  The old value of the property being changed. 
-   * @param  chain     The event which caused this event to be fired. 
+   * @param  source    The object being changed.
+   * @param  type      The type of change being made.
+   * @param  change    The new value of the property being changed.
+   * @param  previous  The old value of the property being changed.
+   * @param  chain     The event which caused this event to be fired.
    */
 
   public ChangeEvent(
-    Object source, 
-    ChangeType type, 
-    Object change, 
-    Object previous, 
+    Object source,
+    ChangeType type,
+    Object change,
+    Object previous,
     ChangeEvent chain
   ) {
     super(source);
@@ -114,9 +115,9 @@ public class ChangeEvent extends EventObject {
 
 
   /**
-   *  Find the type of this event. 
+   *  Find the type of this event.
    *
-   * @return    The Type value 
+   * @return    The Type value
    */
   public ChangeType getType() {
     return type;
@@ -124,12 +125,12 @@ public class ChangeEvent extends EventObject {
 
 
   /**
-   *  
+   *
    * Return an object which is to be the new value of some property,
    * or is to be added to a collection.  May return <code>null</code>
    * is this is not meaningful.
    *
-   * @return    The Change value 
+   * @return    The Change value
    */
 
   public Object getChange() {
@@ -137,11 +138,11 @@ public class ChangeEvent extends EventObject {
   }
 
   /**
-   *  
+   *
    * Return the old value of a property being changed.  May return
    * <code>null</code> is this is not meaningful.
    *
-   * @return    The Previous value 
+   * @return    The Previous value
    */
 
   public Object getPrevious() {
@@ -149,18 +150,18 @@ public class ChangeEvent extends EventObject {
   }
 
   /**
-   *  
+   *
    * Return the event which caused this to be fired, or <code>null</code>
    * if this change was not caused by another event.
    *
-   * @return    The ChainedEvent value 
+   * @return    The ChainedEvent value
    */
 
   public ChangeEvent getChainedEvent() {
     return chain;
- 
+
   }
-  
+
   public String toString() {
     return
       super.toString() +
