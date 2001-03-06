3658111b2c37c8090424589fadfc33d360654257
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-03-06 16:23:06 +0000

    Same fix for _removeAnnotationSource as _addAnnotationSource. Checking for != null now.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1179 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/program/das/DASSequence.java
+++ src/org/biojava/bio/program/das/DASSequence.java
@@ -235,7 +235,7 @@ public class DASSequence implements Sequence, RealizingFeatureHolder {
 	    for (Iterator i = structure.features(); i.hasNext(); ) {
 		DASComponentFeature dcf = (DASComponentFeature) i.next();
 		DASSequence seq = dcf.getSequenceLazy();
-		if (seq == null) {
+		if (seq != null) {
 		    seq.removeAnnotationSource(dataSourceURL);
 		}
 	    }
