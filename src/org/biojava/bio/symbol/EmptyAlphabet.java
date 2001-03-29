36993ff5e5f4455cd3f3700a104351f640c14be5
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-03-29 13:59:33 +0000

    Split EmptyAlphabet into it's own compilation unit.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1255 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/symbol/EmptyAlphabet.java
@@ -0,0 +1,112 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+
+package org.biojava.bio.symbol;
+
+import java.util.*;
+import java.lang.reflect.*;
+import java.io.*;
+
+import org.biojava.utils.*;
+import org.biojava.bio.*;
+import org.biojava.bio.seq.io.*;
+
+class EmptyAlphabet implements FiniteAlphabet, Serializable {
+    public String getName() {
+	return "Empty Alphabet";
+    }
+    
+    public Annotation getAnnotation() {
+	return Annotation.EMPTY_ANNOTATION;
+    }
+    
+    public boolean contains(Symbol s) {
+	return s == AlphabetManager.getGapSymbol();
+    }
+    
+    public void validate(Symbol sym) throws IllegalSymbolException {
+	throw new IllegalSymbolException(
+					 "The empty alphabet does not contain symbol " + sym.getName());
+    }
+    
+    public SymbolParser getParser(String name) throws NoSuchElementException {
+      throw new NoSuchElementException("There is no parser for the empty alphabet. Attempted to retrieve " + name);
+    }
+
+    public int size() {
+      return 0;
+    }
+    
+    public List getAlphabets() {
+	return Collections.EMPTY_LIST;
+    }
+    
+    public Symbol getSymbol(List syms) throws IllegalSymbolException {
+	if(syms.size() != 0) {
+	    throw new IllegalSymbolException("The empty alphabet contains nothing");
+	}
+	return AlphabetManager.getGapSymbol();
+    }
+    
+    public Symbol getAmbiguity(Set syms) throws IllegalSymbolException {
+	for(Iterator i = syms.iterator(); i.hasNext(); ) {
+	    this.validate((Symbol) i.next());
+	}
+	return AlphabetManager.getGapSymbol();
+    }
+    
+    public Iterator iterator() {
+	return SymbolList.EMPTY_LIST.iterator();
+    }
+    
+    public SymbolList symbols() {
+	return SymbolList.EMPTY_LIST;
+    }
+  
+    public void addSymbol(Symbol sym) throws IllegalSymbolException {
+      throw new IllegalSymbolException(
+        "Can't add symbols to alphabet: " + sym.getName() +
+        " in " + getName()
+      );
+    }
+  
+    public void removeSymbol(Symbol sym) throws IllegalSymbolException {
+      throw new IllegalSymbolException(
+        "Can't remove symbols from alphabet: " + sym.getName() +
+        " in " + getName()
+      );
+    }
+    
+    public void addChangeListener(ChangeListener cl) {}
+    public void addChangeListener(ChangeListener cl, ChangeType ct) {}
+    public void removeChangeListener(ChangeListener cl) {}
+    public void removeChangeListener(ChangeListener cl, ChangeType ct) {}
+
+    private Object writeReplace() throws ObjectStreamException {
+      try {
+        return new StaticMemberPlaceHolder(Alphabet.class.getField("EMPTY_ALPHABET"));
+      } catch (NoSuchFieldException nsfe) {
+        throw new NotSerializableException(nsfe.getMessage());
+      }
+    }
+}
+
