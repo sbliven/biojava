55810e7a71f3a459f6e662e66f5d17250a320328
gcox <gcox@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-06-07 12:57:59 +0000

    Added commented out code that will test annotations and features.  This is more debugging code than demo code, but is still useful for demoing
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1355 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/seq/TestGenbank.java
+++ demos/seq/TestGenbank.java
@@ -32,6 +32,54 @@ public class TestGenbank {
 			{
 				Sequence seq = seqI.nextSequence();
 				System.out.println(seq.getName() + " has " + seq.countFeatures() + " features");
+				// Feature (location) testing
+//				java.util.Iterator theIterator = seq.features();
+//				SeqFileFormer theFormatter = SeqFileFormerFactory.makeFormer("Genbank");
+//				while(theIterator.hasNext())
+//				{
+//					System.out.println();
+//					Feature theFeature = (Feature)theIterator.next();
+//					try
+//					{
+//						StrandedFeature castedFeature = (StrandedFeature)theFeature;
+//						StringBuffer theBuffer = new StringBuffer("Location: ");
+//						System.out.println(theFormatter.formatLocation(theBuffer, castedFeature.getLocation(), castedFeature.getStrand()));
+//					}
+//					catch (ClassCastException cce)
+//					{
+//						System.out.println("Feature is not Stranded");
+//						StringBuffer theBuffer = new StringBuffer("Location: ");
+//						System.out.println(theFormatter.formatLocation(theBuffer, theFeature.getLocation(), StrandedFeature.POSITIVE));
+//					}
+//					Annotation featureAnnotation = theFeature.getAnnotation();
+//					java.util.Set theKeys = featureAnnotation.keys();
+//					System.out.println("Keys:");
+//					java.util.Iterator keyIterator = theKeys.iterator();
+//					while(keyIterator.hasNext())
+//					{
+//						System.out.println("\t" + keyIterator.next());
+//					}
+//
+//					System.out.println();
+//					keyIterator = theKeys.iterator();
+//					while(keyIterator.hasNext())
+//					{
+//						Object tempKey = keyIterator.next();
+//						System.out.println(tempKey + ": " + featureAnnotation.getProperty(tempKey));
+//					}
+//				}
+
+				// Annotation testing
+//				Annotation theAnnotation = seq.getAnnotation();
+//				java.util.Set theKeys = theAnnotation.keys();
+//				java.util.Iterator theOtherIterator = theKeys.iterator();
+//				while(theOtherIterator.hasNext())
+//				{
+//					System.out.println();
+//					String theKey = theOtherIterator.next().toString();
+//					System.out.println(theKey + ":");
+//					System.out.println("\t" + theAnnotation.getProperty(theKey).toString());
+//				}
 			}
 		}
 		catch (Throwable t)
