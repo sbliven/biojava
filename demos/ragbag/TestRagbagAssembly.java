232c0bca8952a5262be58c466763fe37c5965786
dhuen <dhuen@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-06-24 00:48:25 +0000

    TestRagbagAssembly.java
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1399 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/ragbag/TestRagbagAssembly.java
+++ demos/ragbag/TestRagbagAssembly.java
@@ -9,14 +9,15 @@ import org.biojava.bio.seq.io.*;
 import org.biojava.bio.symbol.*;
 import org.biojava.bio.seq.ragbag.*;
 
-public class TestRagbagDirectory {
+public class TestRagbagAssembly {
     public static void main(String[] args) 
         throws Exception
     {
       // create RagbagSequence doodad
       File f = new File(args[0]);
 
-      RagbagDirectoryHandler rs = new RagbagDirectoryHandler(f);
+      RagbagSequenceFactory sf = new RagbagSoftRefSeqFactory();
+      RagbagAssembly rs = new RagbagAssembly(f, sf);
 
       System.out.println("completed RagbagDirectoryHandler");
 
