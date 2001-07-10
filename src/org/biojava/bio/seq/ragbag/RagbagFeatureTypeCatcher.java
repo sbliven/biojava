7e0ed94a0a359c9247eef5cd591c1b2d7be2cc48
dhuen <dhuen@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-07-10 07:50:29 +0000

    implements event filtering
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1436 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/seq/ragbag/RagbagFeatureTypeCatcher.java
@@ -0,0 +1,76 @@
+/**
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+ 
+package org.biojava.bio.seq.ragbag;
+
+import org.biojava.bio.seq.Feature;
+import org.biojava.bio.seq.io.ParseException;
+import org.biojava.bio.seq.io.SequenceBuilder;
+import org.biojava.bio.seq.io.SequenceBuilderFilter;
+import org.biojava.bio.seq.io.SeqIOListener;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
+
+/**
+ * A class that assists in wrapping a feature filter
+ * that records all types encountered
+ * around a SequenceBuilder class.
+ * It may be scrapped as it was implemented to
+ * permit a getAllTypes() call which is IMPOSSIBLE
+ * with lazy instantiation.
+ */
+public class RagbagFeatureTypeCatcher
+  implements RagbagFilterFactory
+{
+  public static RagbagFeatureTypeCatcher FACTORY = new RagbagFeatureTypeCatcher();
+
+  private static Set allTypes = new HashSet();
+
+  private class RagbagFeatureFilter extends SequenceBuilderFilter
+  {
+    RagbagFeatureFilter(SequenceBuilder delegate)
+    {
+      super(delegate);
+    }
+
+    // override the startFeature method
+    public void startFeature(Feature.Template templ)
+      throws ParseException
+    {
+      allTypes.add(templ.type);
+      getDelegate().startFeature(templ);
+    }
+  }
+
+  public SequenceBuilder wrap(SequenceBuilder delegate)
+  {
+    return new RagbagFeatureFilter(delegate);
+  }
+
+  public Set getAllTypes() 
+  {
+    return Collections.unmodifiableSet(allTypes);
+  }
+}
+
+
---
+++ src/org/biojava/bio/seq/ragbag/RagbagFilteredCachedSeqFactory.java
@@ -0,0 +1,43 @@
+/**
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+ 
+package org.biojava.bio.seq.ragbag;
+
+import org.biojava.utils.cache.*;
+import java.lang.String;
+
+/**
+ * class that is passed to a RagbagAssembly to use
+ * a SoftReferenceCache-backed RagbagCachedSequences.
+ */
+public class RagbagFilteredCachedSeqFactory implements RagbagSequenceFactory
+{
+  Cache cache;
+
+  {
+    cache = new SoftReferenceCache();
+  }
+
+  public RagbagSequenceItf getSequenceObject(String name, String urn)
+  {
+    return (RagbagSequenceItf) new RagbagParsedCachedSequence(name, urn, cache, RagbagFeatureTypeCatcher.FACTORY);
+  }
+}
