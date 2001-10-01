bc5a4dc0b847d0ac6868979ddf9222d0e203af34
kdj <kdj@7c6358e6-4a41-0410-a743-a5b2a554c398>
2001-10-01 18:07:14 +0000

    Merged differences & added Thomas's improvements
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1541 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/program/ssbind/AnnotationFactory.java
+++ src/org/biojava/bio/program/ssbind/AnnotationFactory.java
@@ -1,31 +1,86 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
 package org.biojava.bio.program.ssbind;
 
-import java.util.*;
-import org.biojava.bio.*;
-import org.biojava.utils.*;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
-public class AnnotationFactory {
-    public static Annotation makeAnnotation(Map m) {
-	if (m.size() == 0) {
-	    return Annotation.EMPTY_ANNOTATION;
-	} else {
-	    Annotation anno;
-	    if (m.size() < 15) {
-		anno = new SmallAnnotation();
-	    } else {
-		anno = new SimpleAnnotation();
+import org.biojava.bio.BioError;
+import org.biojava.bio.Annotation;
+import org.biojava.bio.SmallAnnotation;
+import org.biojava.bio.SimpleAnnotation;
+import org.biojava.utils.ChangeVetoException;
+
+/**
+ * <code>AnnotationFactory</code> is a utility class for making
+ * <code>Annotation</code>s from <code>Map</code>s. Shared by the
+ * search and homology builders.
+ *
+ * @author <a href="mailto:kdj@sanger.ac.uk">Keith James</a>
+ * @author Thomas Down
+ * @since 1.2
+ */
+class AnnotationFactory
+{
+    /**
+     * <code>makeAnnotation</code> creates the annotation.
+     *
+     * @param m a <code>Map</code> of raw data.
+     * @return an <code>Annotation</code>.
+     */
+    static Annotation makeAnnotation(final Map m)
+    {
+        int elements = m.size();
+
+        if (elements == 0)
+        {
+            return Annotation.EMPTY_ANNOTATION;
+        }
+        else
+        {
+            Annotation annotation;
+
+            if (elements < 15)
+                annotation = new SmallAnnotation();
+            else
+                annotation = new SimpleAnnotation();
+
+            Set keySet = m.keySet();
+
+            try
+            {
+                for (Iterator ksi = keySet.iterator(); ksi.hasNext();)
+                {
+                    Object key = ksi.next();
+                    annotation.setProperty(key, m.get(key));
+                }
+	    }
+	    catch (ChangeVetoException cve)
+	    {
+                throw new BioError(cve, "Assert failed: couldn't modify newly created Annotation");
 	    }
 
-	    try {
-		for (Iterator mei = m.entrySet().iterator(); mei.hasNext(); ) {
-		    Map.Entry me = (Map.Entry) mei.next();
-		    anno.setProperty(me.getKey(), me.getValue());
-		}
-	    } catch (ChangeVetoException cve) {
-		throw new BioError(cve, "Assert failed: couldn't modify newly created Annotation");
-	    }
-
-	    return anno;
+            return annotation;
 	}
     }
 }
