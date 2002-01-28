0b08205a5794d428282a2a0678cfc9c1a042cb6c
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-01-28 23:56:34 +0000

    Better code now.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1645 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/seq/db/BioIndex.java
+++ src/org/biojava/bio/seq/db/BioIndex.java
@@ -18,47 +18,159 @@ public class BioIndex implements IndexStore {
   
   private int fileCount;
   private File[] fileIDToFile;
-  private Map fileToFileID;
   
-  private RandomAccessFile indxFile;
-  
-  private int recordLength;
   private FileAsList indxList;
   private Map secondaryKeyToFileAsList;
   
   private Set idSet = new ListAsSet();
-  private int commitedRecords;
+  
   private String name;
   private SequenceFormat format;
   private SequenceBuilderFactory sbFactory;
   private SymbolTokenization symbolTokenization;
   
+  {
+    fileCount = 0;
+    fileIDToFile = new File[4];
+  }
+  
+  public BioIndex(
+    File indexDirectory,
+    String namespace,
+    int idLength
+  ) throws IOException, BioException {
+    if(indexDirectory.exists()) {
+      throw new BioException(
+        "Can't create new index as directory already exists: " + 
+        indexDirectory
+      );
+    }
+    
+    // create directory
+    indexDirectory.createNewFile();
+    
+    // create BIOINDEX.dat
+    {
+      File bioindex = new File(indexDirectory, "BIOINDEX.dat");
+      bioindex.createNewFile();
+      PrintWriter pw = new PrintWriter(new FileWriter(bioindex));
+      pw.println("index\tflat/1");
+      pw.close();
+    }
+    
+    // create fileids.dat
+    PrintWriter fileidsWriter;
+    {
+      File fileids = new File(indexDirectory, "fileids.dat");
+      fileids.createNewFile();
+      fileidsWriter = new PrintWriter(
+        new FileWriter(
+          fileids
+        )
+      );
+    }
+    
+    // create config.dat
+    PrintWriter configWriter;
+    {
+      File config = new File(indexDirectory, "config.dat");
+      config.createNewFile();
+      configWriter = new PrintWriter(new FileWriter(config));
+      configWriter.println("namespace\t" + namespace);
+    }
+    
+    // create index file
+    {
+      String uniqueName = "key_" + namespace + ".key";
+      File unique = new File(indexDirectory, uniqueName);
+      unique.createNewFile();
+      
+      int recordLen =
+        idLength +                                   // id
+        1 +                                          // tab
+        4 +                                          // 9999 files
+        1 +                                          // tab
+        String.valueOf(Long.MAX_VALUE).length() +    // space for any long
+        1 +                                          // tab
+        String.valueOf(Integer.MAX_VALUE).length() + // space for any int
+        "\n".length()                                // new line (os dependant)
+        ;
+      
+      indxList = new IndexFileAsList(
+        new RandomAccessFile(unique, "rw"),
+        recordLen
+      );
+      
+      fileidsWriter.println(uniqueName + "\t" + recordLen);
+    }
+    
+    // other field initialization to get things going
+    fileCount = 0;
+    fileIDToFile = new File[4];
+    
+    configWriter.close();
+    fileidsWriter.close();
+  }
+  
+  /**
+   * Load an existing index file.
+   *
+   * If indexDirectory does not exist, or is not a bioindex stoore, this will
+   * barf.
+   */
   public BioIndex(
     File indexDirectory
   ) throws IOException, BioException {
     this.indexDirectory = indexDirectory;
     
-    // set up file set
-    fileCount = 0;
-    fileIDToFile = new File[4];
-    
-    BufferedReader fi = new BufferedReader(
-      new FileReader(
-        new File(indexDirectory, "fileids.dat")
-      )
-    );
-    for(String line = fi.readLine(); line != null; line = fi.readLine()) {
-      StringTokenizer sTok = new StringTokenizer("\t");
-      int id = Integer.parseInt(sTok.nextToken());
-      File file = new File(sTok.nextToken());
-      long fileLength = Long.parseLong(sTok.nextToken());
-      
-      if(file.length() != fileLength) {
-        throw new BioException("File length changed: " + file + " "
-        + file.length() + " vs " + fileLength);
+    if(!indexDirectory.exists()) {
+      throw new BioException(
+        "Tried to load non-existant index: " +
+        indexDirectory
+      );
+    }
+
+    // read in the global config
+    {
+      Map config = new HashMap();
+      BufferedReader fi = new BufferedReader(
+        new FileReader(
+          new File(indexDirectory, "config.dat")
+        )
+      );
+      for(String line = fi.readLine(); line != null; line = fi.readLine()) {
+        int tab = line.indexOf("\t");
+        config.put(line.substring(0, tab), line.substring(tab + 1));
       }
+      String namespace = (String) config.get("namespace");
+      RandomAccessFile indxFile = new RandomAccessFile("key_" + namespace + ".key", "rw");
+      int recLen = guessRecLen(indxFile);
+      indxList = new IndexFileAsList(indxFile, recLen);
+    }
+    
+    // set up file set
+    {
+      fileCount = 0;
+      fileIDToFile = new File[4];
       
-      fileIDToFile[id] = file;
+      BufferedReader fi = new BufferedReader(
+        new FileReader(
+          new File(indexDirectory, "fileids.dat")
+        )
+      );
+      for(String line = fi.readLine(); line != null; line = fi.readLine()) {
+        StringTokenizer sTok = new StringTokenizer("\t");
+        int id = Integer.parseInt(sTok.nextToken());
+        File file = new File(sTok.nextToken());
+        long fileLength = Long.parseLong(sTok.nextToken());
+        
+        if(file.length() != fileLength) {
+          throw new BioException("File length changed: " + file + " "
+          + file.length() + " vs " + fileLength);
+        }
+        
+        fileIDToFile[id] = file;
+      }
     }
   }
   
@@ -90,9 +202,36 @@ public class BioIndex implements IndexStore {
     return this.name;
   }
   
+  public int guessRecLen(RandomAccessFile file)
+  throws IOException {
+    file.seek(0l);
+    int b = 0;
+    while(b != '\n' && b != '\r') {
+      b  = file.read();
+    }
+    
+    int offset = (int) file.getFilePointer();
+    
+    if(b == '\n') {          // \n
+      return offset + 1;
+    } else {
+      b = file.read();
+      if(b == '\n') {        // \r\n
+        return offset + 2;
+      } else {               // \r
+        return offset + 1;
+      }
+    }
+  }
+  
   public Index fetch(String id)
   throws IllegalIDException, BioException {
-    int indx = Collections.binarySearch(indxList, id, indxList.getComparator());
+    int indx = Collections.binarySearch(
+      indxList,
+      id,
+      indxList.getComparator()
+    );
+    
     if(indx < 0) {
       throw new IllegalIDException("Can't find sequence for " + id);
     }
@@ -106,6 +245,7 @@ public class BioIndex implements IndexStore {
   
   public void commit()
   throws BioException {
+    indxList.commit();
     try {
       // write files
       {
@@ -124,11 +264,6 @@ public class BioIndex implements IndexStore {
         }
         fo.close();
       }
-      
-      // write sequences
-      Collections.sort(indxList, indxList.getComparator());
-      
-      commitedRecords = indxList.size();
     } catch (Exception e) {
       rollback();
       throw new BioException(e, "Unable to commit. Rolled back to be safe");
@@ -136,15 +271,7 @@ public class BioIndex implements IndexStore {
   }
   
   public void rollback() {
-    try {
-      indxFile.setLength((long) commitedRecords * (long) recordLength);
-    } catch (Throwable t) {
-      throw new BioError(
-        t, "Could not roll back. " +
-        "The index store will be in an inconsistent state " +
-        "and should be discarded"
-      );
-    }
+    indxList.rollback();
   }
   
   public Set getIDs() {
@@ -167,12 +294,20 @@ public class BioIndex implements IndexStore {
     return symbolTokenization;
   }
   
+  private interface Commitable {
+    public void commit()
+    throws BioException;
+    
+    public void rollback();
+  }
+  
   // records stored as:
   // seqID(\w+) \t fileID(\w+) \t start(\d+) \t length(\d+) ' ' * \n
   private abstract class FileAsList
   extends AbstractList
-  implements RandomAccess {
+  implements RandomAccess, Commitable {
     private RandomAccessFile mappedFile;
+    private int commitedRecords;
     private int lastIndx;
     private Object lastRec;
     private byte[] buffer;
@@ -191,10 +326,10 @@ public class BioIndex implements IndexStore {
         return lastRec;
       }
       
-      long offset = indx * recordLength;
+      long offset = indx * buffer.length;
       try {
-        indxFile.seek(offset);
-        indxFile.readFully(buffer);
+        mappedFile.seek(offset);
+        mappedFile.readFully(buffer);
       } catch (IOException ioe) {
         throw new BioError(ioe, "Failed to seek for record");
       }
@@ -206,7 +341,7 @@ public class BioIndex implements IndexStore {
     
     public int size() {
       try {
-        return (int) (indxFile.length() / (long) recordLength);
+        return (int) (mappedFile.length() / (long) buffer.length);
       } catch (IOException ioe) {
         throw new BioError(ioe, "Can't read file length");
       }
@@ -216,13 +351,30 @@ public class BioIndex implements IndexStore {
       generateRecord(buffer, o);
       
       try {
-        indxFile.seek(indxFile.length());
-        indxFile.write(buffer);
+        mappedFile.seek(mappedFile.length());
+        mappedFile.write(buffer);
       } catch (IOException ioe) {
         throw new BioError(ioe, "Failed to write index");
       }
       
       return true;
+    }
+    
+    public void commit() {
+      Collections.sort(indxList, indxList.getComparator());
+      commitedRecords = indxList.size();
+    }
+
+    public void rollback() {
+      try {
+        mappedFile.setLength((long) commitedRecords * (long) buffer.length);
+      } catch (Throwable t) {
+        throw new BioError(
+          t, "Could not roll back. " +
+          "The index store will be in an inconsistent state " +
+          "and should be discarded. File: " + mappedFile
+        );
+      }
     }
     
     protected abstract Object parseRecord(byte[] buffer);
@@ -275,7 +427,7 @@ public class BioIndex implements IndexStore {
       long start = Long.parseLong(new String(buffer, lastI, newI));
       
       int length = Integer.parseInt(
-        new String(buffer, newI + 1, recordLength)
+        new String(buffer, newI + 1, buffer.length)
       );
       
       return new SimpleIndex(file, start, length, id);
