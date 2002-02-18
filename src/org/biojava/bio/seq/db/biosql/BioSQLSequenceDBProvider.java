163428cb43f819984d8a3971601a6ad2bc50335a
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-02-18 14:49:09 +0000

    Added
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1719 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/seq/db/biosql/BioSQLSequenceDBProvider.java
@@ -0,0 +1,65 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio.seq.db.biosql;
+
+import java.util.*;
+
+import org.biojava.directory.*;
+import org.biojava.bio.*;
+import org.biojava.bio.seq.db.*;
+
+public class BioSQLSequenceDBProvider implements SequenceDBProvider {
+    public String getName() {
+	return "biosql";
+    }
+
+    public SequenceDB getSequenceDB(Map config)
+        throws RegistryException, BioException
+    {
+	String location = (String) config.get("location");
+	if (location == null) {
+	    throw new RegistryException("BioSQL databases require a `location' option");
+	}
+
+	String userName = (String) config.get("userName");
+	if (userName == null) {
+	    throw new RegistryException("BioSQL databases require a `userName' option");
+	}
+
+	String password = (String) config.get("password");
+	if (password == null) {
+	    password = "";
+	}
+
+	String biodatabase = (String) config.get("biodatabase");
+	if (biodatabase == null) {
+	    throw new RegistryException("BioSQL database require a `biodatabase' option");
+	}
+
+	return new BioSQLSequenceDB(location,
+				    userName,
+				    password,
+				    biodatabase,
+				    false);
+
+    }
+}
