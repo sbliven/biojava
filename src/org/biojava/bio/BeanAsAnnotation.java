e76a98a08119ef470418b074678a063d782836a3
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-02-18 14:59:13 +0000

    Added an annotation implementation that wraps a Java Bean.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1721 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/BeanAsAnnotation.java
@@ -0,0 +1,50 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio;
+
+import java.beans.IntrospectionException;
+import java.util.*;
+import java.io.*;
+
+import org.biojava.utils.*;
+
+/**
+ * @author Matthew Pocock
+ */
+
+public class BeanAsAnnotation extends AbstractAnnotation {
+  private Map properties;
+  
+  protected final Map getProperties() {
+    return properties;
+  }
+  
+  protected final boolean propertiesAllocated() {
+    return true;
+  }
+
+  public BeanAsAnnotation(Object bean)
+  throws IntrospectionException {
+    properties = new BeanAsMap(bean);
+  }
+}
+
