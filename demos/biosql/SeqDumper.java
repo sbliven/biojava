3319a1cff03b1d6c7b31c9a46d46783e459983f0
thomasd <thomasd@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-03-14 20:23:39 +0000

    Updated.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1811 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/biosql/SeqDumper.java
+++ demos/biosql/SeqDumper.java
@@ -9,12 +9,12 @@ import org.biojava.bio.seq.db.*;
 import org.biojava.bio.seq.io.*;
 import org.biojava.bio.seq.db.biosql.*;
 
-public class SeqEMBL {
+public class SeqDumper {
     public static void main(String[] args)
         throws Exception
     {
 	if (args.length < 4) {
-	    System.err.println("Usage SeqEMBL <url> <user> <password> <database> [id1] [id2] ...");
+	    System.err.println("Usage SeqDumper <url> <user> <password> <database> [id1] [id2] ...");
 	    return;
 	}
 
@@ -24,12 +24,12 @@ public class SeqEMBL {
   	String bioName = args[3];
 	
 	SequenceDB seqDB = new BioSQLSequenceDB(dbURL, dbUser, dbPass, bioName, false);
-	SequenceFormat ff = new EmblLikeFormat();
+	SequenceFormat ff = new FastaFormat();
 
 	if (args.length < 5) {
 	    SequenceIterator si = seqDB.sequenceIterator();
 	    while (si.hasNext()) {
-		ff.writeSequence(si.nextSequence(), "embl", System.out);
+		ff.writeSequence(si.nextSequence(), System.out);
 	    }
 	} else {
 	    for (int i = 4; i < args.length; ++i) {
