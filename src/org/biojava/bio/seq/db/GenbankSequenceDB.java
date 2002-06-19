7567e4481f79f5e1e6f4a83457528dbfb503b7b4
llai <llai@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-06-19 14:31:36 +0000

    The GenpeptSequenceDB class contains functions which enable to access protein sequences in Genpept format. It gets the base URLs and some arguments from the FetchURL class.  It returns a sequence object if calling getSequence() method by passing the sequence id as a parameter.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1923 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/seq/db/GenbankSequenceDB.java
@@ -0,0 +1,107 @@
+package org.biojava.bio.seq.db;
+
+import java.net.*;
+import java.io.*;
+import org.biojava.bio.symbol.*;
+import org.biojava.bio.seq.io.*;
+import org.biojava.bio.seq.DNATools;
+import org.biojava.bio.BioError;
+import org.biojava.bio.seq.Sequence;
+import org.biojava.bio.BioException;
+import org.biojava.bio.seq.SequenceIterator;
+
+/**
+ * This class contains functions accessing DNA sequences in Genbank format.
+ */
+public class GenbankSequenceDB
+{
+  private static SequenceFormat format;//return format of the sequence
+  private static String DBName="Genbank";//predefined the database name -- Genbank
+  private boolean IOExceptionFound=false;//
+  
+  static 
+  {
+    SequenceFormat format = new GenbankFormat();
+  }
+  
+  protected SequenceFormat getSequenceFormat() 
+  {
+    return format;
+  }
+  
+  protected Alphabet getAlphabet() 
+  {
+    return DNATools.getDNA();
+  }
+  
+  /**
+   * Get the URL object for locating sequence object using eutils.
+   * The default value of the return format of the sequence object is text.
+   **/
+  protected URL getAddress (String id) throws MalformedURLException
+  {
+	String defaultReturnFormat="text";
+	FetchURL seqURL = new FetchURL(DBName, defaultReturnFormat);
+	String baseurl = seqURL.getbaseURL();
+	String db = seqURL.getDB();
+	String returnFormat = seqURL.getReturnFormat();
+	
+	String url = baseurl+db+"&"+returnFormat+"&id="+id;
+	
+    return new URL (url);
+  }
+  
+  /**
+   * Get the URL object for locating sequence object using eutils.
+   * User could specify the return format of the sequence object.
+   */
+  protected URL getAddress(String id, String format) throws MalformedURLException
+  {   
+	FetchURL seqURL = new FetchURL(DBName, format);
+	String baseurl = seqURL.getbaseURL();
+	if (!(baseurl.equalsIgnoreCase("")))
+		baseurl = seqURL.getbaseURL();
+	String db = seqURL.getDB();
+	String returnFormat = seqURL.getReturnFormat();
+	
+	String url = baseurl+db+"&"+returnFormat+"&id="+id;
+	
+    return new URL (url);
+  }
+  
+  public String getName() 
+  {
+    return DBName;
+  }
+  
+  public Sequence getSequence(String id) throws Exception 
+  {
+    try 
+	{
+	  IOExceptionFound=false;
+      URL queryURL = getAddress(id);//get URL based on ID      
+    //  System.err.println("query is "+ queryURL.toString());
+      SequenceFormat sFormat = getSequenceFormat();//get incoming sequence format
+      SequenceBuilder sbuilder = new SimpleSequenceBuilder();//create a sequence builder
+	  SequenceBuilderFactory sFact=new GenbankProcessor.Factory(SimpleSequenceBuilder.FACTORY);
+      Alphabet alpha = getAlphabet();//get alphabet
+      SymbolTokenization rParser = alpha.getTokenization("token");//get SymbolTokenization
+      System.err.println("got data from "+ queryURL);
+	  DataInputStream in=new DataInputStream(queryURL.openStream());
+	  BufferedReader reader = new BufferedReader (new InputStreamReader (in));
+	  SequenceIterator seqI= SeqIOTools.readGenbank(reader);
+      return seqI.nextSequence();
+    } 
+	catch ( Exception e )
+	{
+      System.out.println (e.toString());
+	  IOExceptionFound=true;
+	  return null;
+    } 
+  }
+  
+  public boolean checkIOException()
+  {
+	return IOExceptionFound;  
+  }
+}
---
+++ src/org/biojava/bio/seq/db/GenpeptSequenceDB.java
@@ -0,0 +1,98 @@
+package org.biojava.bio.seq.db;
+
+import java.net.*;
+import java.io.*;
+import org.biojava.bio.symbol.*;
+import org.biojava.bio.seq.DNATools;
+import org.biojava.bio.BioError;
+import org.biojava.bio.seq.Sequence;
+import org.biojava.bio.BioException;
+import org.biojava.bio.seq.SequenceIterator;
+import org.biojava.bio.seq.ProteinTools;
+import org.biojava.bio.seq.io.*;
+											 
+public class GenpeptSequenceDB
+{
+  private static SequenceFormat format;
+  private static String DBName="Genpept";
+  private boolean IOExceptionFound=false;
+  
+  static 
+  {
+    SequenceFormat format = new GenbankFormat();
+  }
+  
+  protected SequenceFormat getSequenceFormat() 
+  {
+    return format;
+  }
+  
+  protected Alphabet getAlphabet() 
+  {
+    return ProteinTools.getTAlphabet();
+  }
+  
+  protected URL getAddress (String id) throws MalformedURLException
+  {
+	String defaultReturnFormat="text";
+	FetchURL seqURL = new FetchURL(DBName, defaultReturnFormat);
+	String baseurl = seqURL.getbaseURL();
+	String db = seqURL.getDB();
+	String returnFormat = seqURL.getReturnFormat();
+	
+	String url = baseurl+db+"&"+returnFormat+"&id="+id;
+	
+    return new URL (url);
+  }
+  
+  //ask user to input id and return format
+  protected URL getAddress(String id, String format) throws MalformedURLException
+  {   
+	FetchURL seqURL = new FetchURL(DBName, format);
+	String baseurl = seqURL.getbaseURL();
+	if (!(baseurl.equalsIgnoreCase("")))
+		baseurl = seqURL.getbaseURL();
+	String db = seqURL.getDB();
+	String returnFormat = seqURL.getReturnFormat();
+	
+	String url = baseurl+db+"&"+returnFormat+"&id="+id;
+	
+    return new URL (url);
+  }
+  
+  public String getName() 
+  {
+    return DBName;
+  }
+  
+  public Sequence getSequence(String id) throws BioException 
+  {
+    try 
+	{
+	  IOExceptionFound=false;
+      URL queryURL = getAddress(id);//achieve URL based on ID      
+   //   System.err.println("query is "+ queryURL.toString());
+      SequenceFormat sFormat = getSequenceFormat();//get incoming sequence format
+      SequenceBuilder sbuilder = new SimpleSequenceBuilder();//create a sequence builder
+	  SequenceBuilderFactory sFact=new GenbankProcessor.Factory(SimpleSequenceBuilder.FACTORY);
+      Alphabet alpha = getAlphabet();//get alphabet
+      SymbolTokenization rParser = alpha.getTokenization("token");//get SymbolTokenization
+      System.err.println("got data from "+ queryURL);
+	  DataInputStream in=new DataInputStream(queryURL.openStream());
+	  BufferedReader reader = new BufferedReader (new InputStreamReader (in));
+	  SequenceIterator seqI= SeqIOTools.readGenpept(reader);
+      return seqI.nextSequence();
+    } 
+	catch ( Exception e )
+	{
+	  System.out.println (e.toString());
+	  IOExceptionFound=true;
+      return null;
+    } 
+  }
+  
+  public boolean checkIOException()
+  {
+	return IOExceptionFound;
+  }
+}
