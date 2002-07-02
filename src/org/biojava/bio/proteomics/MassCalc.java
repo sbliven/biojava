6ed9176d1acdc62f5f739758110a96db06bd4267
kdj <kdj@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-07-02 16:21:54 +0000

    Changed getMass method sig. Tightened Exceptions
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1946 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/proteomics/MassCalc.java
+++ src/org/biojava/bio/proteomics/MassCalc.java
@@ -19,33 +19,27 @@
  *
  */
 
-
-/*
- * MassCalc.java
- *
- * Created on January 11, 2001, 4:18 PM
- */
-
 package org.biojava.bio.proteomics;
 
-
-import org.biojava.bio.seq.*;
-//import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import org.biojava.bio.*;
-import org.biojava.bio.symbol.*;
+import org.biojava.bio.seq.*;
 import org.biojava.bio.seq.io.*;
-
-
-import java.util.Iterator;
-import java.util.HashMap;
+import org.biojava.bio.symbol.*;
 
 /**
+ * <code>MassCalc</code> calculates the mass of peptides which for our
+ * purposes are <code>SymbolList</code>s which contain
+ * <code>Symbol</code>sfrom the protein <code>Alphabet</code>. It uses
+ * the mono-isotopic and average-isotopic masses identical to those
+ * specified at www.micromass.co.uk
  *
- * @author  mjones
+ * @author M. Jones
+ * @author Keith James (minor changes)
  */
 public class MassCalc {
-    
     //Possibly these should be put in a configurable table somewhere
     /**
      * Constant value of Carbon monoisotopic mass
@@ -90,159 +84,191 @@ public class MassCalc {
     //Save values here so that modifications are not global
     private HashMap mSymbolPropertyHash;
 
-    
-    /**
-     * If instance methods are being used this will store the 
-     * isotopicly and MH_PLUS correct treminal mass to be created. 
-     * Need to be able to handle N and C term mods in the future. 
+    /*
+     * If instance methods are being used this will store the
+     * isotopically and MH_PLUS correct terminal mass to be created.
+     * Need to be able to handle N and C term mods in the future.
      */
      private double termMass;
+    
+    /*
+     * Creates new MassCalc.
+     * @param isotopicType The type of isotopes to calculate. Either
+     * mono isotopic or average isotopic. It realy is the name of
+     * SymbolProperty table. Ex. SymbolPropertyTable.AVG_MASS or
+     * SymbolPropertyTable.MONO_MASS */
 
-    
-    /** Creates new MassCalc 
-    public MassCalc() {
-    }*/
-    
-     /** Creates new MassCalc
-     * @param isotopicType The type of isotopes to calculate. Either mono isotopic
-     * or average isotopic. It realy is the name of SymbolProperty
-     * table. Ex. SymbolPropertyTable.AVG_MASS or SymbolPropertyTable.MONO_MASS
+
+    /**
+     * Creates a new <code>MassCalc</code>.
+     *
+     * @param isotopicType a <code>String</code>. The type of isotopes
+     * to calculate. Either mono isotopic or average
+     * isotopic. Acceptable values are
+     * <code>SymbolPropertyTable.AVG_MASS</code> or
+     * <code>SymbolPropertyTable.MONO_MASS</code>.
+     * @param MH_PLUS a <code>boolean</code>.
      */
     public MassCalc(String isotopicType, boolean MH_PLUS) {
         //Calculate hydroxyl mass
-        termMass = calcTermMass(isotopicType,MH_PLUS); 
+        termMass = calcTermMass(isotopicType, MH_PLUS); 
         mSymbolPropertyHash = new HashMap();
-        
+
         SymbolPropertyTable symbolPropertyTable = 
-                ProteinTools.getSymbolPropertyTable(isotopicType);
-        
-        //Iterator symbolList = ProteinTools.getAlphabet().symbols().iterator();
+            ProteinTools.getSymbolPropertyTable(isotopicType);
         
         Iterator symbolList = ProteinTools.getAlphabet().iterator();
-	// SymbolTokenization toke = ProteinTools.getAlphabet().getTokenization("token");
-        
+
         for(; symbolList.hasNext(); )
         {
-            
-            Symbol sym = (Symbol)symbolList.next();
-            try{
+            Symbol sym = (Symbol) symbolList.next();
+            try {
                 // System.out.println(sym.getName() + ":" + toke.tokenizeSymbol(sym));
-                try{
-                Double value = new Double(symbolPropertyTable.getDoubleValue(sym));
-                
-                mSymbolPropertyHash.put(sym, value);
-                }catch (NullPointerException npe){
-                   //This seems to be happening when a amino acid is 
-                   //in the property table that doesn't have a residue value
-                   
+                try {
+                    Double value =
+                        new Double(symbolPropertyTable.getDoubleValue(sym));
+                    mSymbolPropertyHash.put(sym, value);
+                } catch (NullPointerException npe) {
+                    //This seems to be happening when a amino acid is 
+                    //in the property table that doesn't have a residue value
                 }
             }
-            catch(IllegalSymbolException ise)
+            catch (IllegalSymbolException ise)
             {
-                //Should most likley not be thrown
-                ise.printStackTrace(System.err);
+                throw new BioError(ise, "Error setting properties for Symbol " + sym);
             }
         }
     }
 
-    /** Use this to set a post translational modification for this Symbol. It will only
-     * the current MassCalc instance and will not affect the static method.
-     * @param symbolToken The one char id for this residue
-     * @param mass the mass to change the residue to
-     * @throws IllegalSymbolException
+    /**
+     * Use this to set a post translational modification for the
+     * <code>Symbol</code> represented by this character. It will only
+     * affect the current <code>MassCalc</code> instance and will not
+     * affect the static method.
+     *
+     * @param symbolToken a <code>char</code> representing a
+     * <code>Symbol</code>.
+     * @param mass a <code>double</code> to be the new mass of the
+     * residue.
+     *
+     * @exception IllegalSymbolException if the <code>Symbol</code> is
+     * not recognised.
      */
     public void setSymbolModification(char symbolToken, double mass) 
-                                        throws IllegalSymbolException
+        throws IllegalSymbolException
     {
-        //Iterator list = ProteinTools.getAlphabet().symbols().iterator();
-        
         Iterator list = ProteinTools.getAlphabet().iterator();
-	SymbolTokenization toke;
-	try {
-	    toke = ProteinTools.getAlphabet().getTokenization("token");
-	} catch (BioException ex) {
-	    throw new BioError(ex, "Expected a tokenization");
-	}
+        SymbolTokenization toke;
 
-	Symbol sym = toke.parseToken("" + symbolToken);
-	mSymbolPropertyHash.put(sym, new Double(mass));
+        try {
+            toke = ProteinTools.getAlphabet().getTokenization("token");
+        } catch (BioException ex) {
+            throw new BioError(ex, "Expected a tokenization");
+        }
+
+        Symbol sym = toke.parseToken("" + symbolToken);
+        mSymbolPropertyHash.put(sym, new Double(mass));
     }
-    
  
-    /** May need something like this if a user wants to add a non standard
+    /* May need something like this if a user wants to add a non standard
      * amino acid to a specific location on a peptide. Not very usefull
      * for woking with large batches of sequences. Currently this is not implemented
      * @param residue
      * @param mass
      * @throws IllegalSymbolException
      */
-    public void addSymbolModification(Symbol residue, double mass) 
-                                        throws IllegalSymbolException
-    {
-     //   mSymbolPropertyTable.setDoubleProperty(residue, Double.toString(mass));
-    }
+
     
-    /** Get the Mass of this peptide. This only works for the values in the
-     * ResidueProperties.xml. It is probably slightly faster but it does not
-     * handle Post Translational Modifications.
-     * @param proteinSeq The sequence
-     * @param isotopicType The type of table containing the mass values. Ex. SymbolPropertyTable.MONO_MASS
-     * @param MH_PLUS True if the value needed is the MH+ mass
-     * @return The mass of the peptide
+    /**
+     * <code>addSymbolModification</code> may be needed if a user
+     * wants to add a non standard amino acid to a specific location
+     * on a peptide. <strong>Currently this is not implemented</strong>.
+     *
+     * @param residue a <code>Symbol</code>.
+     * @param mass a <code>double</code>.
+     *
+     * @exception IllegalSymbolException if the <code>Symbol</code> is
+     * not recognised.
+     */
+    public void addSymbolModification(Symbol residue, double mass) 
+        throws IllegalSymbolException
+    {
+        //   mSymbolPropertyTable.setDoubleProperty(residue, Double.toString(mass));
+    }
+
+    /**
+     * <code>getMass</code> calculates the mass of this peptide. This
+     * only works for the values in the ResidueProperties.xml
+     * configuration file. It is probably slightly faster than the
+     * instance method, but it does not handle post-translational
+     * modifications.
+     *
+     * @param proteinSeq a <code>SymbolList</code> whose mass is to be
+     * calculated. This should use the protein alphabet.
+     * @param isotopicType a <code>String</code> The type of isotopes
+     * to calculate. Either mono isotopic or average
+     * isotopic. Acceptable values are
+     * <code>SymbolPropertyTable.AVG_MASS</code> or
+     * <code>SymbolPropertyTable.MONO_MASS</code>.
+     * @param MH_PLUS a <code>boolean</code> true if the value needed
+     * is the MH+ mass.
+     *
+     * @return a <code>double</code> mass of the peptide.
+     *
+     * @exception IllegalSymbolException if the
+     * <code>SymbolList</code> contains illegal
+     * <code>Symbol</code>s.
      */
     public static final double getMass(SymbolList proteinSeq, 
-                                        String isotopicType, 
-                                        boolean MH_PLUS)
-	                       throws IllegalSymbolException
+                                       String isotopicType, 
+                                       boolean MH_PLUS)
+        throws IllegalSymbolException
     {
-        if(!proteinSeq.getAlphabet().getName().equals("PROTEIN")){
-            //throw exception
+        if (! proteinSeq.getAlphabet().getName().equals("PROTEIN")) {
+            throw new IllegalSymbolException("The SymbolList was not using the protein alphabet");
         }
-        
+
         double pepMass = 0.0;
-        
+
         SymbolPropertyTable sPT = 
-                ProteinTools.getSymbolPropertyTable(isotopicType);
-        try
-        {
-            for(Iterator it = proteinSeq.iterator(); it.hasNext(); ){
-                pepMass += sPT.getDoubleValue((Symbol)it.next());
-            }
+            ProteinTools.getSymbolPropertyTable(isotopicType);
+
+        for (Iterator it = proteinSeq.iterator(); it.hasNext(); ) {
+            pepMass += sPT.getDoubleValue((Symbol) it.next());
         }
-        catch(IllegalSymbolException ise)
-        {
-            //Throw
-            throw ise;
-        }
-        
+
         //Calculate hydroxyl mass
-	double termMass = calcTermMass(isotopicType,MH_PLUS);
-        
-        if (pepMass != 0.0){
-	    pepMass += termMass;
+        double termMass = calcTermMass(isotopicType, MH_PLUS); 
+
+        if (pepMass != 0.0) {
+            pepMass += termMass;
         }
 
         return pepMass;
     }
     
-    /** Get the Mass of this peptide. Use this if you want to set fixed modifications
-     * and have created an instance of MassCalc. The static method may be faster.
+    /**
+     * Get the Mass of this peptide. Use this if you want to set fixed
+     * modifications and have created an instance of MassCalc. The
+     * value is calculated using the value of MH_PLUS defined in the
+     * constructor. The static method may be faster.
+     *
      * @param proteinSeq The sequence for mass calculation
-     * @param MH_PLUS true if you want to calculate MH+ values
-     * @return The mass of the sequence
-     */
-    public double getMass(SymbolList proteinSeq, boolean MH_PLUS)
+     *
+     * @return The mass of the sequence */
+    public double getMass(SymbolList proteinSeq)
+        throws IllegalSymbolException
     {
-        if(!proteinSeq.getAlphabet().getName().equals("PROTEIN")){
-            //throw exception
+        if (! proteinSeq.getAlphabet().getName().equals("PROTEIN")){
+            throw new IllegalSymbolException("The SymbolList was not using the protein alphabet");
         }
-        
+
         double pepMass = 0.0;
-        
+
         HashMap symbolPropertyMap = getSymbolPropertyMap();
 
-        for(Iterator it = proteinSeq.iterator(); it.hasNext(); ){
-            Double mass = (Double)symbolPropertyMap.get((Symbol)it.next());
+        for (Iterator it = proteinSeq.iterator(); it.hasNext(); ) {
+            Double mass = (Double) symbolPropertyMap.get((Symbol) it.next());
             pepMass += mass.doubleValue();
         }
         pepMass += getTermMass();
@@ -256,32 +282,33 @@ public class MassCalc {
     }
 
     /**
-     * Use this method to double check the aded teminal masses. 
+     * <code>getTermMass</code> returns the terminal mass being used
+     * by the instance methods.
      *
+     * @return a <code>double</code> mass.
      */
     public double getTermMass(){
-	return termMass;
+        return termMass;
     }
 
-    private static double calcTermMass(String isotopicType, boolean MH_PLUS){
-	double termMass = 0.0;
-	if(isotopicType.equals(SymbolPropertyTable.AVG_MASS)){
-	    //Add the C-terminal OH and N-Term H
-	    termMass += Havg + Oavg + Havg;
-	    //Add the extra H
-	    if(MH_PLUS){
-	      termMass += Havg;	
-	    }
+    private static double calcTermMass(String isotopicType, boolean MH_PLUS) {
+        double termMass = 0.0;
+        if (isotopicType.equals(SymbolPropertyTable.AVG_MASS)) {
+            //Add the C-terminal OH and N-Term H
+            termMass += Havg + Oavg + Havg;
+            //Add the extra H
+            if (MH_PLUS) {
+                termMass += Havg;	
+            }
         }
-        else if(isotopicType.equals(SymbolPropertyTable.MONO_MASS)){
-	    //Add the C-terminal OH and N-Term H
-	    termMass += Hmono + Omono + Hmono;
-	    //Add the extra H
-	    if(MH_PLUS){
-	      termMass += Hmono;
-	    }
+        else if (isotopicType.equals(SymbolPropertyTable.MONO_MASS)) {
+            //Add the C-terminal OH and N-Term H
+            termMass += Hmono + Omono + Hmono;
+            //Add the extra H
+            if (MH_PLUS) {
+                termMass += Hmono;	
+            }
         }
-
-	return termMass;
+        return termMass;
     }
 }
