1d9bc6c01b90a4612d96b9db45c3d805bcee578b
nks31 <nks31@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-07-08 19:09:04 +0000

    Introduced an alternate interface for reading and writing sequences and alignments.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1954 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/seq/io/SeqIOTools.java
+++ src/org/biojava/bio/seq/io/SeqIOTools.java
@@ -259,4 +259,308 @@ public class SeqIOTools  {
         StreamWriter sw = new StreamWriter(os, new GenbankFormat());
         sw.writeStream(in);
     }
+
+
+    /**
+     * The following methods provide an alternate interface for reading and writing
+     * sequences and alignments.
+     *
+     * @author Nimesh Singh
+     */
+
+    //constants representing different filetypes
+    public static final int FASTADNA = 1;
+    public static final int FASTAPROTEIN = 2;
+    public static final int EMBL = 3;
+    public static final int GENBANK = 4;
+    public static final int SWISSPROT = 5;
+    public static final int GENPEPT = 6;
+    public static final int MSFDNA = 7;
+    public static final int FASTA = 8;
+    public static final int FASTAALIGNDNA = 9;
+    public static final int MSFPROTEIN = 10;
+    public static final int FASTAALIGNPROTEIN = 11;
+
+    /**
+     * Attempts to guess the filetype of a file given the name.  For use with
+     * the functions below that take an int fileType as a parameter.  The
+     * constants used are above.
+     */
+    public static int guessFileType(String fileName) throws Exception {
+        //First tries by matching an extension
+        if ((new RE(".*\\u002eem.*")).match(fileName)) {
+            return EMBL;
+        }
+        else if ((new RE(".*\\u002egb.*")).match(fileName)) {
+            return GENBANK;
+        }
+        else if ((new RE(".*\\u002esp.*")).match(fileName)) {
+            return SWISSPROT;
+        }
+        else if ((new RE(".*\\u002egp.*")).match(fileName)) {
+            return GENPEPT;
+        }
+        else if ((new RE(".*\\u002efa.*")).match(fileName)) {
+            return guessFastaType(fileName);
+        }
+        else if ((new RE(".*\\u002emsf.*")).match(fileName)) {
+            return guessMsfType(fileName);
+        }
+
+        //Reads the file to guess based on content
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line1 = br.readLine();
+        String line2 = br.readLine();
+
+        if (line1.startsWith(">")) {
+            return guessFastaType(fileName);
+        }
+        else if (line1.startsWith("PileUp")) {
+            return guessMsfType(fileName);
+        }
+        else if (line1.startsWith("!!AA_MULTIPLE_ALIGNMENT")) {
+            return MSFPROTEIN;
+        }
+        else if (line1.startsWith("!!NA_MULTIPLE_ALIGNMENT")) {
+            return MSFDNA;
+        }
+        else if (line1.startsWith("ID")) {
+            for (int i = 0; i < line1.length(); i++) {
+                if (Character.toUpperCase(line1.charAt(i)) == 'P' &&
+                    Character.toUpperCase(line1.charAt(i+1)) == 'R' &&
+                    Character.toUpperCase(line1.charAt(i+2)) == 'T') {
+                        return SWISSPROT;
+                }
+            }
+            return EMBL;
+        }
+        else if (line1.toUpperCase().startsWith("LOCUS")) {
+            for (int i = 0; i < line1.length(); i++) {
+                if (Character.toUpperCase(line1.charAt(i)) == 'A' &&
+                    Character.toUpperCase(line1.charAt(i+1)) == 'A') {
+                        return GENPEPT;
+                }
+            }
+            return GENBANK;
+        }
+        else if (line1.length() >= 45 &&
+                 line1.substring(19, 45).equalsIgnoreCase("GENETIC SEQUENCE DATA BANK")) {
+            return guessGenType(fileName);
+        }
+        else {
+            System.out.println("guessFileType -- Could not guess file type.");
+            return 0;
+        }
+    }
+
+    /**
+     * Helper function for guessFileName.
+     */
+    private static int guessFastaType(String fileName) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line = br.readLine();
+        line = br.readLine();
+        for (int i = 0; i < line.length(); i++) {
+            if (Character.toUpperCase(line.charAt(i)) == 'F' ||
+                Character.toUpperCase(line.charAt(i)) == 'L' ||
+                Character.toUpperCase(line.charAt(i)) == 'I' ||
+                Character.toUpperCase(line.charAt(i)) == 'P' ||
+                Character.toUpperCase(line.charAt(i)) == 'Q' ||
+                Character.toUpperCase(line.charAt(i)) == 'E') {
+                    return FASTAPROTEIN;
+            }
+        }
+        return FASTADNA;
+    }
+
+    /**
+     * Helper function for guessFileName.
+     */
+    private static int guessMsfType(String fileName) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line = br.readLine();
+        if (line.startsWith("!!NA_MULTIPLE_ALIGNMENT")) {
+            return MSFDNA;
+        }
+        else if (line.startsWith("!!AA_MULTIPLE_ALIGNMENT")) {
+            return MSFPROTEIN;
+        }
+        else {
+            while (line.indexOf("Type: ") == -1) {
+                line = br.readLine();
+            }
+            int typeIndex = line.indexOf("Type: ") + 6;
+            if (line.startsWith("N")) {
+                return MSFDNA;
+            }
+            else if (line.startsWith("P")) {
+                return MSFPROTEIN;
+            }
+            else {
+                System.out.println("guessFileType -- Could not guess file type.");
+                return 0;
+            }
+        }
+    }
+
+    /**
+     * Helper function for guessFileName.
+     */
+    private static int guessGenType(String fileName) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line = br.readLine();
+        while (line.indexOf("LOCUS") == -1) {
+            line = br.readLine();
+        }
+        for (int i = 0; i < line.length(); i++) {
+            if (Character.toUpperCase(line.charAt(i)) == 'A' &&
+                Character.toUpperCase(line.charAt(i+1)) == 'A') {
+                    return GENPEPT;
+            }
+        }
+        return GENBANK;
+    }
+
+    /**
+     * Reads a file and returns the corresponding Biojava object.
+     */
+    public static Object fileToBiojava(int fileType, BufferedReader br) throws Exception {
+        switch (fileType) {
+            case MSFDNA:
+            case MSFPROTEIN:
+            case FASTAALIGNDNA:
+            case FASTAALIGNPROTEIN:
+                return fileToAlign(fileType, br);
+            case FASTADNA:
+            case FASTAPROTEIN:
+            case EMBL:
+            case GENBANK:
+            case SWISSPROT:
+            case GENPEPT:
+                return fileToSeq(fileType, br);
+            default:
+                System.out.println("fileToBiojava -- File type not recognized.");
+                System.exit(0);
+            return null;
+        }
+    }
+
+    /**
+     * Converts a file to an Biojava alignment.
+     */
+    private static Alignment fileToAlign(int fileType, BufferedReader br) throws Exception{
+        switch(fileType) {
+            case MSFDNA:
+            case MSFPROTEIN:
+                return (new MSFAlignmentFormat()).read(br);
+            case FASTAALIGNDNA:
+            case FASTAALIGNPROTEIN:
+                return (new FastaAlignmentFormat()).read(br);
+            default:
+                System.out.println("fileToAlign -- File type not recognized.");
+                System.exit(0);
+                return null;
+        }
+    }
+
+    /**
+     * Converts a file to a Biojava sequence.
+     */
+    private static SequenceIterator fileToSeq(int fileType, BufferedReader br) {
+        switch (fileType) {
+            case FASTADNA:
+                return SeqIOTools.readFastaDNA(br);
+            case FASTAPROTEIN:
+                return SeqIOTools.readFastaProtein(br);
+            case EMBL:
+                return SeqIOTools.readEmbl(br);
+            case GENBANK:
+                return SeqIOTools.readGenbank(br);
+            case SWISSPROT:
+                return SeqIOTools.readSwissprot(br);
+            case GENPEPT:
+                return SeqIOTools.readGenpept(br);
+            default:
+                System.out.println("fileToSeq -- File type not recognized.");
+                System.exit(0);
+                return null;
+        }
+    }
+
+    /**
+     * Converts a Biojava object to the given filetype.
+     */
+    public static void biojavaToFile(int fileType, OutputStream os, Object biojava) throws Exception {
+        switch (fileType) {
+            case MSFDNA:
+            case MSFPROTEIN:
+            case FASTAALIGNDNA:
+            case FASTAALIGNPROTEIN:
+                alignToFile(fileType, os, (Alignment) biojava);
+                break;
+            case FASTA:
+            case FASTADNA:
+            case FASTAPROTEIN:
+            case EMBL:
+            case GENBANK:
+            case SWISSPROT:
+            case GENPEPT:
+                seqToFile(fileType, os, (SequenceIterator) biojava);
+                break;
+            default:
+                System.out.println("biojavaToFile -- File type not recognized.");
+                System.exit(0);
+        }
+    }
+
+    /**
+     * Converts a Biojava alignment to the given filetype.
+     */
+    private static void alignToFile(int fileType, OutputStream os, Alignment align) throws Exception{
+        switch(fileType) {
+            case MSFDNA:
+                (new MSFAlignmentFormat()).writeDna(os, align);
+                break;
+            case MSFPROTEIN:
+                (new MSFAlignmentFormat()).writeProtein(os, align);
+                break;
+            case FASTAALIGNDNA:
+                (new FastaAlignmentFormat()).writeDna(os, align);
+                break;
+            case FASTAALIGNPROTEIN:
+                (new FastaAlignmentFormat()).writeProtein(os, align);
+                break;
+            default:
+                System.out.println("alignToFile -- File type not recognized.");
+                System.exit(0);
+        }
+    }
+
+    /**
+     * Converts a Biojava sequence to the given filetype.
+     */
+    private static void seqToFile(int fileType, OutputStream os, SequenceIterator seq) throws IOException, BioException {
+        switch (fileType) {
+            case FASTADNA:
+            case FASTAPROTEIN:
+            case FASTA:
+                SeqIOTools.writeFasta(os, seq);
+                break;
+            case EMBL:
+                SeqIOTools.writeEmbl(os, seq);
+                break;
+            case SWISSPROT:
+                SeqIOTools.writeSwissprot(os, seq);
+                break;
+            case GENBANK:
+                SeqIOTools.writeGenbank(os, seq);
+                break;
+            case GENPEPT:
+                SeqIOTools.writeGenpept(os, seq);
+                break;
+            default:
+                System.out.println("seqToFile -- File type not recognized.");
+                System.exit(0);
+        }
+    }
 }
