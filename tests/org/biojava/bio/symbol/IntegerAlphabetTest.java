1a0afce3953914f0d3e4e20c42a0e2fc0944d236
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-07-17 21:43:17 +0000

    Added test for integer alphabet.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@1974 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ tests/org/biojava/bio/symbol/IntegerAlphabetTest.java
@@ -0,0 +1,99 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio.symbol;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests IntegerAlphabet to make sure we can get it, that it is cannonical, that
+ * symbols it returns are cannonical, that finite ranges are cannonical and that
+ * symbols in finite ranges are cannonical.
+ *
+ * @author <a href="mailto:kdj@sanger.ac.uk">Keith James</a>
+ * @since 1.2
+ */
+public class IntegerAlphabetTest extends TestCase
+{
+  public IntegerAlphabetTest(String name) {
+    super(name);
+  }
+  
+  
+  public void testCannonicalAlphabet() {
+    assertEquals(
+      AlphabetManager.alphabetForName("INTEGER"),
+      IntegerAlphabet.getInstance()
+    );
+    
+    assertEquals(
+      IntegerAlphabet.getInstance(),
+      IntegerAlphabet.getInstance()
+    );
+  }
+  
+  public void testCanonicalSymbols() {
+    Symbol[] syms = new Symbol[10];
+    for(int i = 0; i < syms.length; i++) {
+      syms[i] = IntegerAlphabet.getInstance().getSymbol(i);
+    }
+
+    for(int i = 0; i < syms.length; i++) {
+      assertEquals(
+        syms[i],
+        IntegerAlphabet.getInstance().getSymbol(i)
+      );
+    }
+  }
+  
+  public void testCannonicalSubAlphabet() {
+    int min = 200;
+    int max = 300;
+    
+    IntegerAlphabet alpha = IntegerAlphabet.getInstance();
+    
+    Alphabet a1 = alpha.getSubAlphabet(min, max);
+    Alphabet a2 = alpha.getSubAlphabet(min, max);
+    
+    assertEquals(a1, a2);
+  }
+  
+  public void testSubAlphabetSymbolsCannonical()
+  throws IllegalSymbolException {
+    int min = 400;
+    int max = 500;
+    
+    IntegerAlphabet alpha = IntegerAlphabet.getInstance();
+    
+    IntegerAlphabet.SubIntegerAlphabet a1 = alpha.getSubAlphabet(min, max);
+    for(int i = min; i <= max; i++) {
+      assertEquals(
+        a1.getSymbol(i),
+        a1.getSymbol(i)
+      );
+
+      assertEquals(
+        a1.getSymbol(i),
+        alpha.getSymbol(i)
+      );
+    }
+  }
+}
