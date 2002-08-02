bbf101aa8fb0c820566cb35749775b662859d1c6
lc1 <lc1@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-08-02 08:43:53 +0000

    First commit for the HMMER parsers
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2004 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/program/hmmer/HmmerProfileHMM.java
@@ -0,0 +1,97 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio.program.hmmer;
+
+import org.biojava.utils.*;
+import org.biojava.bio.*;
+import org.biojava.bio.symbol.*;
+import org.biojava.bio.dist.*;
+import org.biojava.bio.dp.*;
+
+import java.util.*; 
+import java.io.*; 
+ 
+
+
+
+
+/** This is a class for representing HMMER generated Profile HMM.
+ *  It differs from the normal ProfileHMM only in the states which are connected:
+ *   - there are no insert <-> delete transitions allowed
+ *   - there is no iO initial insert state (between begin and initial match states)
+ *   - there is not iN final insert state (between final match state and end state)
+ *   @author Lachlan Coin
+ */
+public class HmmerProfileHMM extends ProfileHMM {
+  HmmerProfileHMM(
+    Alphabet alpha, int columns,
+    DistributionFactory matchFactory, DistributionFactory insertFactory, String name
+  ) throws IllegalSymbolException, IllegalTransitionException,
+  IllegalAlphabetException {
+      super(alpha,columns, matchFactory,insertFactory,name);
+  }
+
+ 
+
+    /** This is called by constructor in setting up the allowed transitions in the model */
+  protected void connectModel() throws 
+	ChangeVetoException, IllegalSymbolException, IllegalTransitionException,IllegalAlphabetException{
+	EmissionState mO = getMatch(0);
+	removeState(getInsert(0));
+	removeState(getInsert(columns()));
+	DotState dO = null;
+	EmissionState iO =null;
+	for(int i = 1; i <= columns(); i++){
+	    EmissionState mN = getMatch(i);
+	    EmissionState iN = getInsert(i);
+	    DotState dN = getDelete(i);
+
+	    // from a model state
+	    createTransition(mO, mN);
+	    if(i < columns())
+		createTransition(mN, iN);
+	    createTransition(mO, dN);
+      
+	    // from an insert state
+	    if(i<columns())
+		createTransition(iN, iN);
+	   
+	    
+	    // from a delete state
+	    if(i > 1) {
+		createTransition(dO, dN);
+		createTransition(dO, mN);
+		createTransition(iO, mN);
+	    }        
+	    mO = mN;
+	    iO = iN;
+	    dO = dN;
+	    // transitions to and from magical states and all match states
+	    if(i>1)
+		createTransition(magicalState(),mN);
+	    createTransition(mN, magicalState());
+	}
+	// for the transitions to end
+	createTransition(dO, magicalState());
+    }
+
+}
