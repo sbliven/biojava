5277a5ec10e7a6863cafcd92c35760f6d1b0c47a
kdj <kdj@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-08-08 20:47:29 +0000

    Added LGPL header
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2012 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/program/tagvalue/AnnotationBuilder.java
+++ src/org/biojava/bio/program/tagvalue/AnnotationBuilder.java
@@ -1,3 +1,24 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
 package org.biojava.bio.program.tagvalue;
 
 import java.util.*;
@@ -38,7 +59,7 @@ public class AnnotationBuilder
    *
    * @param type  the AnnotationType stating what will be built and how
    * @throws ClassCastException if any of the tag-value events are of
-   *         inapropreate type
+   *         inappropriate type
    */
   public AnnotationBuilder(AnnotationType type) {
     this.type = type;
