48a8db573600d1a56213a28a2f2edea06eabd652
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-08-12 19:36:37 +0000

    A simple stupid embl parser that doesn't do much yet.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2021 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ demos/tagvalue/ParseEmbl.java
@@ -0,0 +1,44 @@
+package tagvalue;
+
+import java.io.*;
+import org.biojava.bio.*;
+import org.biojava.bio.program.tagvalue.*;
+
+public class ParseEmbl {
+  public static void main(String[] args)
+  throws Exception {
+    BufferedReader reader = new BufferedReader(
+      new FileReader(
+        new File(args[0])
+      )
+    );
+    
+    
+    LineSplitParser lsp = LineSplitParser.EMBL;
+    
+    
+    AnnotationBuilder annBuilder = new AnnotationBuilder(
+      new AnnotationType.AnyOfType(
+        new PropertyConstraint.IsCollectionOf(
+          java.util.ArrayList.class,
+          PropertyConstraint.ANY
+        )
+      )
+    );
+    TagDelegator td = new TagDelegator(annBuilder);
+    
+    LineSplitParser ftParser = new LineSplitParser();
+    ftParser.setSplitOffset(15);
+    ftParser.setTrimTag(true);
+    ftParser.setTrimValue(true);
+    ftParser.setContinueOnEmptyTag(true);
+    ftParser.setMergeSameTag(false);
+    
+    td.setParserListener("FT", ftParser, annBuilder);
+    
+    Parser parser = new Parser();
+    parser.read(reader, lsp, td);
+    
+    System.out.println(annBuilder.getLast());    
+  }
+}
