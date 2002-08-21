22cd6847368d6783f87f71f8d462e480f80f1593
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-08-21 21:35:52 +0000

    *** empty log message ***
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2079 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/symbol/SimpleSymbolList.java
+++ src/org/biojava/bio/symbol/SimpleSymbolList.java
@@ -36,7 +36,7 @@ import org.biojava.bio.seq.io.*;
  *  <p>
  * SimpleSymbolList is now editable. edit() has been implemented
  * in a way that edits are relatively inefficient, but symbolAt() is
- * very efficient. 
+ * very efficient.
  * </p>
  * <p>
  * A new constructor SimpleSymbolList(SymbolParser,String) has
@@ -59,11 +59,11 @@ import org.biojava.bio.seq.io.*;
  * subList() is implemented such that subLists are views of the original until
  * such time as the underlying SymbolList is edited in a way that would modify
  * the subList, at which point the subList gets its own array of Symbols and
- * does not reflect the edit to the original. When subList() is called on another 
+ * does not reflect the edit to the original. When subList() is called on another
  * subList (which is a veiw SimpleSymbolList) the new SimpleSymbolList is a view
  * of the original, not the subList.
  * </p>
- * 
+ *
  * @author Thomas Down
  * @author David Waring
  */
@@ -110,12 +110,12 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
      *
      * @param alpha The alphabet of legal symbols for this list.
      * @param rList A Java List of symbols.
-     * 
+     *
      * @throws IllegalSymbolException if a Symbol is not in the specified alphabet.
      * @throws ClassCastException if rList contains objects which do not implement Symbol.
      */
 
-    public SimpleSymbolList(Alphabet alpha, List rList) 
+    public SimpleSymbolList(Alphabet alpha, List rList)
         throws IllegalSymbolException
     {
         this.alphabet = alpha;
@@ -131,24 +131,24 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
         this.viewOffset = 0;
         addListener();
     }
-    
+
     /**
      * Construct a SymbolList from a string.
      *
      * @param parser A SymbolParser for whatever your string is -- e.g. alphabet.getParser("token").
      * @param rList A Java List of symbols.
-     * 
+     *
      * @throws IllegalSymbolException if a Symbol is not in the specified alphabet.
      */
 
-    public SimpleSymbolList(SymbolTokenization parser, String seqString) 
-        throws IllegalSymbolException 
-    { 
-	if (parser.getTokenType() == SymbolTokenization.CHARACTER) {
-	    symbols = new Symbol[seqString.length()];
-	} else {
-	    symbols = new Symbol[INCREMENT];
-	}
+    public SimpleSymbolList(SymbolTokenization parser, String seqString)
+        throws IllegalSymbolException
+    {
+        if (parser.getTokenType() == SymbolTokenization.CHARACTER) {
+            symbols = new Symbol[seqString.length()];
+        } else {
+            symbols = new Symbol[INCREMENT];
+        }
         char[] charArray = new char[1024];
         int segLength = seqString.length();
         StreamParser stParser = parser.parseStream(new SSLIOListener());
@@ -160,8 +160,8 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
             seqString.getChars(charCount, charCount + chunkLength, charArray, 0);
             stParser.characters(charArray, 0, chunkLength);
             charCount += chunkLength;
-	}
-	stParser.close();
+        }
+        stParser.close();
 
         this.alphabet = parser.getAlphabet();
         this.isView = false;
@@ -186,7 +186,7 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
         this.viewOffset = 0;
         addListener();
     }
-    
+
     /**
      * Construct construct a SimpleSymbolList that is a veiw of the original.
      *    this is used by subList();
@@ -224,7 +224,7 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
 
     /**
      * Find a symbol at a specified offset in the SymbolList.
-     * 
+     *
      * @param pos Position in biological coordinates (1..length)
      */
 
@@ -237,13 +237,13 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
 //        }
       return symbols[viewOffset + pos - 1];
     }
-    
-    
+
+
     /**
     * create a subList of the original, this will be a view until
     * either the original symbolList or the sublist is edited
     */
-    
+
     public SymbolList subList(int start, int end){
         SimpleSymbolList sl = new SimpleSymbolList(this,viewOffset+start,viewOffset+end);
         if (isView){
@@ -251,7 +251,7 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
         }else{
             this.addChangeListener(sl);
         }
-        return sl;    
+        return sl;
     }
     /**
     * Apply and edit to the SymbolList as specified by Edit.
@@ -259,13 +259,13 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
     *   edit() is now supported using the ChangeEvent system. SubLists do NOT reflect edits.
     * </p>
     */
-    
-    public void edit(Edit edit)throws IndexOutOfBoundsException, IllegalAlphabetException,ChangeVetoException {
+
+    public synchronized void edit(Edit edit)throws IndexOutOfBoundsException, IllegalAlphabetException,ChangeVetoException {
         ChangeSupport cs;
         ChangeEvent cevt;
         Symbol[] dest;
         int newLength;
-        
+
         // first make sure that it is in bounds
         if ((edit.pos + edit.length > length +1 ) || (edit.pos <= 0) || edit.length < 0){
             throw new IndexOutOfBoundsException();
@@ -274,15 +274,15 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
         if (( edit.replacement.getAlphabet() != alphabet) &&  (edit.replacement != SymbolList.EMPTY_LIST)){
             throw new IllegalAlphabetException();
         }
-        
+
         // give the listeners a change to veto this
          // create a new change event ->the EDIT is a static final variable of type ChangeType in SymbolList interface
         cevt = new ChangeEvent(this, SymbolList.EDIT, edit);
         cs = getChangeSupport(SymbolList.EDIT);
-        
+
         // let the listeners know what we want to do
-        cs.firePreChangeEvent(cevt);            
-        
+        cs.firePreChangeEvent(cevt);
+
         // if nobody complained lets continue
         // if we are a view we convert to a real SimpleSymbolList
         if (isView){
@@ -294,17 +294,17 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
         int posRightFragInDestArray5 = posRightFragInSourceArray5 + edit.replacement.length() - edit.length;
         int posReplaceFragInDestArray5 = edit.pos - 1;
         int replaceFragLength = edit.replacement.length();
-        
+
         if ((length + replaceFragLength - edit.length) > symbols.length){
             // extend the array
             dest = new Symbol[(length + replaceFragLength - edit.length + INCREMENT)];
-            
+
             // copy symbols before the edit no need to do this if we didn't have to build a new array
             System.arraycopy(symbols,0,dest,0,(edit.pos -1));
         }else{
             dest = symbols;  // array copy works when copying from an array to itself
         }
-        
+
         // copy the symbols after the edit
         if (rightFragLength > 0){
             System.arraycopy(symbols, posRightFragInSourceArray5, dest, posRightFragInDestArray5,rightFragLength);
@@ -313,7 +313,7 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
         for (int i = 1; i <= replaceFragLength; i++){
             dest[posReplaceFragInDestArray5 + i - 1] = edit.replacement.symbolAt(i);
         }
-        
+
         // if there was a net deletion we have to get rid of the remaining symbols
         newLength = length + replaceFragLength - edit.length;
         for (int j = newLength; j < length; j++){
@@ -321,17 +321,17 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
         }
         length = newLength;
         symbols = dest;
-        cs.firePostChangeEvent(cevt);                    
+        cs.firePostChangeEvent(cevt);
     }
-    
+
     /**
     *  On preChange() we convert the SymolList to a non-veiw version, giving it its own copy of symbols
     */
-    
+
     public void preChange(ChangeEvent cev) throws ChangeVetoException{
         // let the listeners know that they have to be converted or veto the change;
         ChangeSupport cs = getChangeSupport(SymbolList.EDIT);
-        
+
         // lets not bother making any changes if the edit would not effect us or our children
         Object change = cev.getChange();
         if( (change != null) && (change instanceof Edit) ) {
@@ -342,16 +342,16 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
             if ((e.pos < viewOffset) && (e.length - e.replacement.length() == 0)){
                 return;
             }
-        
+
         // subLists of views are listeners to the original so we don't have to forward the message
         makeReal();
         }
     }
-    
+
     // we don't do anything on the postChange we don't want to reflect the changes
     public void postChange(ChangeEvent cev){
     }
-    
+
     /**
     *  Converts a view symbolList to a real one
     *  that means it gets its own copy of the symbols array
@@ -367,26 +367,26 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
             referenceSymbolList = null;
         }
     }
-            
-    
+
+
     /**
      * Add a new Symbol to the end of this list.
-     * 
+     *
      * @param sym Symbol to add
      * @throws IllegalSymbolException if the Symbol is not in this list's alphabet
      */
 
-      public void addSymbol(Symbol sym) 
+      public void addSymbol(Symbol sym)
           throws IllegalSymbolException, ChangeVetoException
       {
           try {
-	      SymbolList extraSymbol = new SimpleSymbolList(getAlphabet(), Collections.nCopies(1, sym));
-	      edit(new Edit(length() + 1, 0, extraSymbol));
-	  } catch (IllegalAlphabetException ex) {
-	      throw new IllegalSymbolException(ex, sym, "Couldn't add symbol");
-	  } catch (IndexOutOfBoundsException ex) {
-	      throw new BioError("Assertion failure: couldn't add symbol at end of list");
-	  }
+              SymbolList extraSymbol = new SimpleSymbolList(getAlphabet(), Collections.nCopies(1, sym));
+              edit(new Edit(length() + 1, 0, extraSymbol));
+          } catch (IllegalAlphabetException ex) {
+              throw new IllegalSymbolException(ex, sym, "Couldn't add symbol");
+          } catch (IndexOutOfBoundsException ex) {
+              throw new BioError("Assertion failure: couldn't add symbol at end of list");
+          }
       }
 
 
@@ -394,20 +394,20 @@ public class SimpleSymbolList extends AbstractSymbolList implements ChangeListen
      * Simple inner class for channelling sequence notifications from
      * a StreamParser.
      */
-    
-    private class SSLIOListener extends SeqIOAdapter {
-	public void addSymbols(Alphabet alpha,Symbol[] syms,int start, int length){
-	    if(symbols.length < SimpleSymbolList.this.length + length) {
-		Symbol[] dest;
-		dest = new Symbol [((int) (1.5 * SimpleSymbolList.this.length)) + length];
-		System.arraycopy(symbols, 0, dest, 0, SimpleSymbolList.this.length);
-		System.arraycopy(syms, start, dest, SimpleSymbolList.this.length, length);
-		symbols = dest;
-	    }else{
-		System.arraycopy(syms, start, symbols, SimpleSymbolList.this.length, length);
-	    }   
 
-	    SimpleSymbolList.this.length += length;
-	}
+    private class SSLIOListener extends SeqIOAdapter {
+        public void addSymbols(Alphabet alpha,Symbol[] syms,int start, int length){
+            if(symbols.length < SimpleSymbolList.this.length + length) {
+                Symbol[] dest;
+                dest = new Symbol [((int) (1.5 * SimpleSymbolList.this.length)) + length];
+                System.arraycopy(symbols, 0, dest, 0, SimpleSymbolList.this.length);
+                System.arraycopy(syms, start, dest, SimpleSymbolList.this.length, length);
+                symbols = dest;
+            }else{
+                System.arraycopy(syms, start, symbols, SimpleSymbolList.this.length, length);
+            }
+
+            SimpleSymbolList.this.length += length;
+        }
     }
 }
