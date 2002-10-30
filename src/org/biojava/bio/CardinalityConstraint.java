52eecfe79d8622ce136d1c17b9a08bb93e73b7dc
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-10-30 22:42:48 +0000

    Adding this cardinality class. Not sure if it's the right way to do things, but at least it's a small api and can be moved arround at will.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2219 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/CardinalityConstraint.java
@@ -0,0 +1,57 @@
+package org.biojava.bio;
+
+/**
+ * A constraint on the number of values a property can have.
+ *
+ * @author Matthew Pocock
+ * @since 1.3
+ */
+public class CardinalityConstraint {
+  public static final CardinalityConstraint ZERO
+    = new CardinalityConstraint(0, 0);
+  public static final CardinalityConstraint ZERO_OR_ONE
+    = new CardinalityConstraint(0, 1);
+  public static final CardinalityConstraint ZERO_OR_MORE
+    = new CardinalityConstraint(0, Integer.MAX_VALUE);
+  public static final CardinalityConstraint ONE
+    = new CardinalityConstraint(1, 1);
+  public static final CardinalityConstraint ONE_OR_MORE
+    = new CardinalityConstraint(1, Integer.MAX_VALUE);
+    
+  private final int min;
+  private final int max;
+  
+  public CardinalityConstraint(int min, int max) {
+    this.min = min;
+    this.max = max;
+  }
+  
+  public int getMin() {
+    return min;
+  }
+  
+  public int getMax() {
+    return max;
+  }
+  
+  public int hashCode() {
+    return min * max;
+  }
+  
+  public boolean subConstraintOf(CardinalityConstraint subConstraint) {
+    return
+      getMin() <= subConstraint.getMin() &&
+      getMax() >= subConstraint.getMax();
+  }
+  
+  public boolean equals(Object o) {
+    if(o == this) return true;
+    
+    if(o instanceof CardinalityConstraint) {
+      CardinalityConstraint that = (CardinalityConstraint) o;
+      return this.getMin() == that.getMin() && this.getMax() == that.getMax();
+    }
+    
+    return false;
+  }
+}
