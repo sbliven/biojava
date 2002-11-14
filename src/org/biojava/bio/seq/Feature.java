6da6c6552d373ceb512ba0130ca0a8094ba311c3
kdj <kdj@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-11-14 22:16:31 +0000

    Moved EMBL-specific Comparator used for seqio to seqio package
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2311 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/seq/Feature.java
+++ src/org/biojava/bio/seq/Feature.java
@@ -22,10 +22,9 @@
 package org.biojava.bio.seq;
 
 import java.io.Serializable;
-import java.util.Comparator;
 import java.lang.reflect.*;
-
 import java.util.*;
+
 import org.biojava.bio.*;
 import org.biojava.bio.symbol.*;
 import org.biojava.utils.*;
@@ -321,20 +320,11 @@ public interface Feature extends FeatureHolder, Annotatable {
         new ByLocationComparator();
 
     /**
-     * <code>byEmblOrder</code> contains a <code>Feature</code>
-     * comparator which compares by the minimum base position of their
-     * <code>Location</code>, but places <code>Feature</code>s with
-     * type "source" first.
-     */
-    public static final ByEmblOrderComparator byEmblOrder =
-        new ByEmblOrderComparator();
-
-    /**
      * <code>ByLocationComparator</code> compares
      * <code>Feature</code>s by the minimum base position of their
      * <code>Location</code>.
      *
-     * @author <a href="mailto:kdj@sanger.ac.uk">Keith James</a>
+     * @author Keith James
      * @since 1.2
      */
     public static final class ByLocationComparator implements Comparator
@@ -349,46 +339,6 @@ public interface Feature extends FeatureHolder, Annotatable {
             // and the result could wrap around. Convert to Long if
             // necessary.
             if (f1.getLocation().getMin() > f2.getLocation().getMin())
-                return 1;
-            else if (f1.getLocation().getMin() < f2.getLocation().getMin())
-                return -1;
-            else
-                return 0;
-        }
-    }
-
-    /**
-     * <code>ByEmblOrderComparator</code> compares
-     * <code>Feature</code>s by the minimum base position of their
-     * <code>Location</code>, but places <code>Feature</code>s with
-     * type "source" first.
-     *
-     * @author <a href="mailto:kdj@sanger.ac.uk">Keith James</a>
-     * @since 1.2
-     */
-    public static final class ByEmblOrderComparator implements Comparator
-    {
-        public int compare(Object o1, Object o2)
-        {
-            Feature f1 = (Feature) o1;
-            Feature f2 = (Feature) o2;
-
-            boolean source1 = f1.getType().equals("source") ? true : false;
-            boolean source2 = f2.getType().equals("source") ? true : false;
-
-            // We don't subtract one coordinate from another as one or
-            // both may be set to Integer.MAX_VALUE/Integer.MIN_VALUE
-            // and the result could wrap around. Convert to Long if
-            // necessary.
-            if (! source1 && source2)
-            {
-                return 1;
-            }
-            else if (source1 && ! source2)
-            {
-                return -1;
-            }
-            else if (f1.getLocation().getMin() > f2.getLocation().getMin())
                 return 1;
             else if (f1.getLocation().getMin() < f2.getLocation().getMin())
                 return -1;
