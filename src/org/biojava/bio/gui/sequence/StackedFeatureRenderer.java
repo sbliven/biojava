5b0267a6efa984f148d62da725d0668f7be96460
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-11-23 20:54:45 +0000

    A new renderer that renders multiple feature glyphs one ontop of the other
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2350 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/gui/sequence/StackedFeatureRenderer.java
@@ -0,0 +1,98 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio.gui.sequence;
+
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+
+import org.biojava.utils.*;
+import org.biojava.bio.*;
+import org.biojava.bio.symbol.*;
+import org.biojava.bio.seq.*;
+import org.biojava.bio.gui.*;
+
+import java.util.List;
+
+public class StackedFeatureRenderer
+extends AbstractChangeable
+implements FeatureRenderer {
+  public static final ChangeType RENDERERS = new ChangeType(
+    "The renderers have changed",
+    StackedFeatureRenderer.class,
+    "RENDERERS",
+    SequenceRenderContext.LAYOUT
+  );
+  
+  private List renderers = new ArrayList();
+  
+  public StackedFeatureRenderer() {}
+  
+  // fixme: events?
+  public void addRenderer(FeatureRenderer renderer) {
+    renderers.add(renderer);
+  }
+  
+  public void removeRenderer(FeatureRenderer renderer) {
+    renderers.remove(renderer);
+  }
+  
+  public double getDepth(SequenceRenderContext src) {
+    double depth = 0.0;
+    
+    for(Iterator i = renderers.iterator(); i.hasNext(); ) {
+      FeatureRenderer fr = (FeatureRenderer) i.next();
+      depth += fr.getDepth(src);
+    }
+    
+    return depth;
+  }
+  
+  public void renderFeature(
+    Graphics2D g,
+    Feature f, 
+    SequenceRenderContext src
+  ) {
+    AffineTransform at = g.getTransform();
+    Shape oldClip = g.getClip();
+    
+    for(Iterator i = renderers.iterator(); i.hasNext(); ) {
+      FeatureRenderer fr = (FeatureRenderer) i.next();
+      fr.renderFeature(g, f, src);
+
+      double depth = fr.getDepth(src);
+      g.translate(0.0, depth);
+    }
+    
+    g.setClip(oldClip);
+    g.setTransform(at);
+  }
+  
+  public FeatureHolder processMouseEvent(
+    FeatureHolder hits,
+    SequenceRenderContext src,
+    MouseEvent me
+  ) {
+    return hits;
+  }
+}
