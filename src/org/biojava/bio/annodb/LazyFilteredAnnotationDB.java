e6c609b82dbd338470e75eab58a983974e29452e
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-12-12 00:02:12 +0000

    Moved things from .bio.ann to .bio.annodb
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2425 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/annodb/LazyFilteredAnnotationDB.java
@@ -0,0 +1,99 @@
+package org.biojava.bio.ann;
+
+import java.util.*;
+import org.biojava.bio.*;
+
+public class LazyFilteredAnnotationDB
+implements AnnotationDB {
+  private final String name;
+  private final AnnotationDB source;
+  private final AnnotationType schema;
+  private AnnotationDB result;
+  
+  public LazyFilteredAnnotationDB(String name, AnnotationDB source, AnnotationType schema) {
+    this.name = name;
+    this.source = source;
+    this.schema = schema;
+  }
+  
+  public String getName() {
+    return "";
+  }
+  
+  public AnnotationType getSchema() {
+    if(result == null) {
+      return this.schema;
+    } else {
+      return result.getSchema();
+    }
+  }
+  
+  public Iterator iterator() {
+    if(result == null) {
+      populate();
+    }
+    
+    return result.iterator();
+  }
+  
+  public int size() {
+    if(result == null) {
+      populate();
+    }
+    
+    return result.size();
+  }
+  
+  public AnnotationDB filter(AnnotationType at) {
+    if(result == null) {
+      return new LazyFilteredAnnotationDB(
+        "",
+        source,
+        AnnotationTools.intersection(schema, at)
+      );
+    } else {
+      return new LazyFilteredAnnotationDB(
+        "",
+        result,
+        at
+      );
+    }
+  }
+  
+  public AnnotationDB search(AnnotationType at) {
+    if(result == null) {
+      return new LazySearchedAnnotationDB(
+        "",
+        this,
+        at
+      );
+    } else {
+      return new LazySearchedAnnotationDB(
+        "",
+        result,
+        at
+      );
+    }
+  }
+  
+  private void populate() {
+    if(result != null) {
+      return;
+    }
+      
+    Set hits = new  HashSet();
+    
+    for(Iterator i = iterator(); i.hasNext(); ) {
+      Annotation ann = (Annotation) i.next();
+      if(schema.instanceOf(ann)) {
+        hits.add(ann);
+      }
+    }
+    
+    if(hits.isEmpty()) {
+      result = AnnotationDB.EMPTY;
+    } else {
+      result = new SimpleAnnotationDB("", hits, schema);
+    }
+  }
+}
