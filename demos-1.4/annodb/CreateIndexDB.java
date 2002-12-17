dc1004fd6a9f308a836353382b8e659d45d5de36
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2002-12-17 17:03:45 +0000

    A demmo to show how to create an advanced index & AnnotationDB instance.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2443 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ demos-1.4/annodb/CreateIndexDB.java
@@ -0,0 +1,42 @@
+package annodb;
+
+import java.util.*;
+import java.io.*;
+
+import org.biojava.bio.*;
+import org.biojava.bio.annodb.*;
+import org.biojava.bio.program.tagvalue.*;
+
+public class CreateIndexDB {
+  public static void main(String[] args)
+  throws Exception {
+//    if(args.length != 1) {
+//      System.err.println("use: annodb.CreateIndexedDB indexConfig.xml");
+//    }
+
+    File indexDir = new File(args[0]);
+    List fileList = new ArrayList();
+    for(int i = 1; i < args.length; i++) {
+      fileList.add(new File(args[i]));
+    } 
+
+    Index2Model model = new Index2Model();
+    model.setPrimaryKeyName("ID");
+    model.addKeyPath("ID", new String[] { "ID" });
+    model.addKeyPath("AC", new String[] { "AC" });
+    model.addKeyPath("SV", new String[] { "SV" });
+    model.addKeyPath("db_xref", new String[] { "FT", "source", "db_xref" });
+
+    AnnotationDB annoDB = new IndexedAnnotationDB(
+      "Embl",
+      indexDir,
+      model,
+      fileList,
+      15,
+      Formats.EMBL_TYPE,
+      new IndexedAnnotationDB.StaticMethodRPFactory(Formats.class.getMethod(
+        "createEmblParserListener", new Class[] { TagValueListener.class }
+      ))
+    );
+  }
+} 
