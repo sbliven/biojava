65d74e6e11c7930428377f24030a90b7a54305b0
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2003-02-07 16:59:40 +0000

    Re-wired the feature guts to be better about projection and so that we can optionaly create features on the gapped view or on the underlying view.
    I still need to write the code for projecting gapped locations down, and to
    hook up feature creation on the projected feature holder so that it does
    the munge.
    
    
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2492 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/seq/SimpleGappedSequence.java
+++ src/org/biojava/bio/seq/SimpleGappedSequence.java
@@ -40,127 +40,141 @@ import java.util.*;
 public class SimpleGappedSequence
 extends SimpleGappedSymbolList
 implements GappedSequence {
-    private Sequence sequence;
-    private FeatureHolder features;
-
-    public SimpleGappedSequence(Sequence seq) {
-	super(seq);
-	this.sequence = seq;
+  private Sequence sequence;
+  
+  private MergeFeatureHolder features;
+  private FeatureHolder localFeatures;
+  private FeatureHolder projectedFeatures;
+  
+  private boolean createOnUnderlying;
+  
+  public SimpleGappedSequence(Sequence seq) {
+    super(seq);
+    this.sequence = seq;
+    createOnUnderlying = false;
+  }
+  
+  public boolean getCreateOnUnderlyingSequence() {
+    return createOnUnderlying;
+  }
+  
+  public void setCreateOnUnderlyingSequence(boolean underlying) {
+    this.createOnUnderlying = underlying;
+  }
+  
+  public Annotation getAnnotation() {
+    return sequence.getAnnotation();
+  }
+  
+  public String getName() {
+    return sequence.getName();
+  }
+  
+  public String getURN() {
+    return sequence.getURN();
+  }
+  
+  private FeatureHolder getFeatures() {
+    if (features == null) {
+      features = makeFeatures();
     }
-
-    public Annotation getAnnotation() {
-	return sequence.getAnnotation();
-    }
-
-    public String getName() {
-	return sequence.getName();
-    }
-
-    public String getURN() {
-	return sequence.getURN();
+    return features;
+  }
+  
+  private MergeFeatureHolder makeFeatures() {
+    projectedFeatures = new ProjectedFeatureHolder(sequence, this, 0, false) {
+      public FeatureHolder getParent(Feature f) {
+        return SimpleGappedSequence.this;
+      }
+      
+      public Sequence getSequence(Feature f) {
+        return SimpleGappedSequence.this;
+      }
+      
+      public Location getLocation(Feature f) {
+        return locationToGapped(f.getLocation());
+      }
+      
+      public StrandedFeature.Strand getStrand(StrandedFeature f) {
+        return f.getStrand();
+      }
+      
+      public Annotation getAnnotation(Feature f) {
+        return f.getAnnotation();
+      }
+      
+      public FeatureHolder projectChildFeatures(Feature f, FeatureHolder parent) {
+        return FeatureHolder.EMPTY_FEATURE_HOLDER;
+      }
+      
+      public Feature createFeature(Feature f, Feature.Template templ)
+      throws ChangeVetoException, BioException
+      {
+        throw new ChangeVetoException("NO");
+      }
+      
+      public void removeFeature(Feature f, Feature f2)
+      throws ChangeVetoException
+      {
+        throw new ChangeVetoException("NO");
+      }
+      
+      public FeatureFilter getSchema(Feature f) {
+        return f.getSchema();
+      }
+    };
+    
+    localFeatures = new SimpleFeatureHolder();
+    
+    features = new MergeFeatureHolder();
+    
+    try {
+      features.addFeatureHolder(projectedFeatures);
+      features.addFeatureHolder(localFeatures);
+    } catch (ChangeVetoException cve) {
+      throw new BioError(cve, "Assertion Failure: Should be able to do this");
     }
     
-    private FeatureHolder getFeatures() {
-	if (features == null) {
-	    features = makeFeatures();
-	}
-	return features;
+    return features;
+  }
+  
+  public Iterator features() {
+    return getFeatures().features();
+  }
+  
+  public FeatureHolder filter(FeatureFilter ff) {
+    return getFeatures().filter(ff);
+  }
+  
+  public FeatureHolder filter(FeatureFilter ff, boolean recurse) {
+    return getFeatures().filter(ff, recurse);
+  }
+  
+  public int countFeatures() {
+    return getFeatures().countFeatures();
+  }
+  
+  public boolean containsFeature(Feature f) {
+    return getFeatures().containsFeature(f);
+  }
+  
+  public FeatureFilter getSchema() {
+    return getFeatures().getSchema();
+  }
+  
+  public void removeFeature(Feature f)
+  throws ChangeVetoException
+  {
+    getFeatures().removeFeature(f);
+  }
+  
+  public Feature createFeature(Feature.Template templ)
+  throws ChangeVetoException, BioException
+  {
+    if(createOnUnderlying) {
+      return projectedFeatures.createFeature(templ);
+    } else {
+      return localFeatures.createFeature(templ);
     }
-    
-    private FeatureHolder makeFeatures() {
-	ProjectionContext pc = new ProjectionContext() {
-		public FeatureHolder getParent(Feature f) {
-		    return SimpleGappedSequence.this;
-		}
-
-		public Sequence getSequence(Feature f) {
-		    return SimpleGappedSequence.this;
-		}
-
-		public Location getLocation(Feature f) {
-		    return locationToGapped(f.getLocation());
-		}
-
-		public StrandedFeature.Strand getStrand(StrandedFeature f) {
-		    return f.getStrand();
-		}
-
-		public Annotation getAnnotation(Feature f) {
-		    return f.getAnnotation();
-		}
-
-		public FeatureHolder projectChildFeatures(Feature f, FeatureHolder parent) {
-		    return FeatureHolder.EMPTY_FEATURE_HOLDER;
-		}
-
-		public Feature createFeature(Feature f, Feature.Template templ)
-		    throws ChangeVetoException, BioException
-		{
-		    throw new ChangeVetoException("NO");
-		}
-		    
-		public void removeFeature(Feature f, Feature f2)
-		    throws ChangeVetoException
-		{
-		    throw new ChangeVetoException("NO");
-		}
-        
-        public FeatureFilter getSchema(Feature f) {
-            return f.getSchema();
-        }
-        
-        // THOMASD -- should hook into a forwarding system.
-        
-        public void addChangeListener(Feature f, ChangeListener cl, ChangeType ct) {}
-        public void removeChangeListener(Feature f, ChangeListener cl, ChangeType ct) {}
-	    } ;
-
-	try {
-	    SimpleFeatureHolder results = new SimpleFeatureHolder();
-	    for (Iterator i = sequence.features(); i.hasNext(); ) {
-		final Feature f = (Feature) i.next();
-		results.addFeature(ProjectionEngine.DEFAULT.projectFeature(f, pc));
-	    }
-		
-	    return results;
-	} catch (ChangeVetoException cve) {
-	    throw new BioError("Assertion failure: can't modify newly created feature holder");
-	}
-    }
-
-    public Iterator features() {
-	return getFeatures().features();
-    }
-
-    public FeatureHolder filter(FeatureFilter ff) {
-        return getFeatures().filter(ff);
-    }
-    
-    public FeatureHolder filter(FeatureFilter ff, boolean recurse) {
-	    return getFeatures().filter(ff, recurse);
-    }
-
-    public int countFeatures() {
-	return getFeatures().countFeatures();
-    }
-
-    public boolean containsFeature(Feature f) {
-	return getFeatures().containsFeature(f);
-    }
-    
-    public FeatureFilter getSchema() {
-        return getFeatures().getSchema();
-    }
-    
-    public void removeFeature(Feature f)
-        throws ChangeVetoException
-    {
-	getFeatures().removeFeature(f);
-    }
-
-    public Feature createFeature(Feature.Template templ)
-        throws ChangeVetoException, BioException
-    {
-	return getFeatures().createFeature(templ);
-    }
+  }
 }
