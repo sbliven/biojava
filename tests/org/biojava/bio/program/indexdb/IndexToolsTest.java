a5fdac50870c0dec4c658ffe98e317eeae014c68
kdj <kdj@7c6358e6-4a41-0410-a743-a5b2a554c398>
2003-02-21 05:32:07 +0000

    Test update
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2542 7c6358e6-4a41-0410-a743-a5b2a554c398

--- tests/org/biojava/bio/program/indexdb/IndexToolsTest.java
+++ tests/org/biojava/bio/program/indexdb/IndexToolsTest.java
@@ -54,7 +54,7 @@ public class IndexToolsTest extends TestCase
 
     public void testIndexFastaDNA() throws Exception
     {
-        SequenceDBLite db = createFastaDB(SeqIOConstants.DNA,
+        SequenceDBLite db = createFastaDB(SeqIOConstants.DNA, "dna",
                                           new String [] { "dna1.fasta",
                                                           "dna2.fasta" });
         Sequence seq1 = db.getSequence("id1");
@@ -62,14 +62,18 @@ public class IndexToolsTest extends TestCase
         Sequence seq2 = db.getSequence("id2");
         assertEquals("ggcgcgcgcg", seq2.seqString());
         Sequence seq3 = db.getSequence("id3");
-        assertEquals("tttttcgatt", seq3.seqString());
+        assertEquals("ccccccccta", seq3.seqString());
         Sequence seq4 = db.getSequence("id4");
-        assertEquals("ggttcgcgcg", seq4.seqString());
+        assertEquals("tttttcgatt", seq4.seqString());
+        Sequence seq5 = db.getSequence("id5");
+        assertEquals("ggttcgcgcg", seq5.seqString());
+        Sequence seq6 = db.getSequence("id6");
+        assertEquals("nnnnnnttna", seq6.seqString());
     }
 
     public void testIndexFastaRNA() throws Exception
     {
-        SequenceDBLite db = createFastaDB(SeqIOConstants.RNA,
+        SequenceDBLite db = createFastaDB(SeqIOConstants.RNA, "rna",
                                           new String [] { "rna1.fasta",
                                                           "rna2.fasta" });
         Sequence seq1 = db.getSequence("id1");
@@ -84,7 +88,7 @@ public class IndexToolsTest extends TestCase
 
     public void testIndexFastaProtein() throws Exception
     {
-        SequenceDBLite db = createFastaDB(SeqIOConstants.AA,
+        SequenceDBLite db = createFastaDB(SeqIOConstants.AA, "protein",
                                           new String [] { "protein1.fasta",
                                                           "protein2.fasta" });
         Sequence seq1 = db.getSequence("id1");
@@ -97,17 +101,79 @@ public class IndexToolsTest extends TestCase
         assertEquals("NGGDEEFDTN", seq4.seqString());
     }
 
-    private SequenceDBLite createFastaDB(int alphabetId, String [] fileNames)
-        throws FileNotFoundException, IOException, BioException
+    public void testIndexEmblDNA() throws Exception
     {
-        File [] files = new File [fileNames.length];
+        File [] files = getDBFiles(new String [] { "part1.embl",
+                                                   "part2.embl" });
+        IndexTools.indexEmblDNA(new File(location), files);
 
-        for (int i = 0; i < files.length; i++)
-        {
-            URL seqURL = getClass().getResource(fileNames[i]);
-            files[i] = new File(seqURL.getFile());
-            
-        }
+        SequenceDBLite db = new FlatSequenceDB(location, "embl");
+
+        Sequence seq1 = db.getSequence("A16SRRNA");
+        assertEquals(1497, seq1.length());
+        Sequence seq2 = db.getSequence("A16STM112");
+        assertEquals(1346, seq2.length());
+        Sequence seq3 = db.getSequence("A16STM146");
+        assertEquals(1352, seq3.length());
+
+        Sequence seq4 = db.getSequence("AY080928");
+        assertEquals(557, seq4.length());
+        Sequence seq5 = db.getSequence("AY080929");
+        assertEquals(556, seq5.length());
+        Sequence seq6 = db.getSequence("AY080930");
+        assertEquals(557, seq6.length());
+    }
+
+    public void testIndexGenbankDNA() throws Exception
+    {
+        File [] files = getDBFiles(new String [] { "part1.gb",
+                                                   "part2.gb" });
+        IndexTools.indexGenbankDNA(new File(location), files);
+
+        SequenceDBLite db = new FlatSequenceDB(location, "genbank");
+
+        Sequence seq1 = db.getSequence("");
+        assertEquals(, seq1.length());
+        Sequence seq2 = db.getSequence("");
+        assertEquals(, seq2.length());
+        Sequence seq3 = db.getSequence("");
+        assertEquals(, seq3.length());
+
+        Sequence seq4 = db.getSequence("");
+        assertEquals(, seq4.length());
+        Sequence seq5 = db.getSequence("");
+        assertEquals(, seq5.length());
+        Sequence seq6 = db.getSequence("");
+        assertEquals(, seq6.length());
+    }
+
+    public void testIndexSwissprot() throws Exception
+    {
+        File [] files = getDBFiles(new String [] { "part1.swiss",
+                                                   "part2.swiss" });
+        IndexTools.indexSwissprot(new File(location), files);
+
+        SequenceDBLite db = new FlatSequenceDB(location, "swiss");
+
+        Sequence seq1 = db.getSequence("104K_THEPA");
+        assertEquals(924, seq1.length());
+        Sequence seq2 = db.getSequence("108_LYCES");
+        assertEquals(102, seq2.length());
+        Sequence seq3 = db.getSequence("10KD_VIGUN");
+        assertEquals(75, seq3.length());
+        Sequence seq4 = db.getSequence("110K_PLAKN");
+        assertEquals(296, seq4.length());
+        Sequence seq5 = db.getSequence("11S3_HELAN");
+        assertEquals(493, seq5.length());
+        Sequence seq6 = db.getSequence("11SB_CUCMA");
+        assertEquals(480, seq6.length());
+    }
+
+    private SequenceDBLite createFastaDB(int alphabetId, String name,
+                                         String [] fileNames)
+        throws Exception
+    {
+        File [] files = getDBFiles(fileNames);
 
         switch (alphabetId)
         {
@@ -127,7 +193,19 @@ public class IndexToolsTest extends TestCase
                                                    + "'");
         }
 
-
-        return new FlatSequenceDB(location, "test");
+        return new FlatSequenceDB(location, name);
     }
+
+    private File [] getDBFiles(String [] fileNames) throws Exception
+    {
+        File [] files = new File [fileNames.length];
+
+        for (int i = 0; i < files.length; i++)
+        {
+            URL seqURL = getClass().getResource(fileNames[i]);
+            files[i] = new File(seqURL.getFile());
+        }
+
+        return files;
+    }            
 }
