123fd7a48080b0d7d4dc1d2fed6fcc574f626ce9
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2003-04-06 03:48:43 +0000

    Rationalization of the Protease package.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2616 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/proteomics/Digest.java
+++ src/org/biojava/bio/proteomics/Digest.java
@@ -1,122 +1,228 @@
 /*
+
  *                    BioJava development code
+
  *
+
  * This code may be freely distributed and modified under the
+
  * terms of the GNU Lesser General Public Licence.  This should
+
  * be distributed with the code.  If you do not have a copy,
+
  * see:
+
  *
+
  *      http://www.gnu.org/copyleft/lesser.html
+
  *
+
  * Copyright for this code is held jointly by the individual
+
  * authors.  These should be listed in @author doc comments.
+
  *
+
  * For more information on the BioJava project and its aims,
+
  * or to join the biojava-l mailing list, visit the home page
+
  * at:
+
  *
+
  *      http://www.biojava.org/
+
  *
+
  */
+
+
 
 package org.biojava.bio.proteomics;
 
+
+
 import org.biojava.bio.seq.Sequence;
+
 import org.biojava.bio.seq.Feature;
+
 import org.biojava.bio.seq.io.SymbolTokenization;
+
 import org.biojava.bio.symbol.Location;
+
 import org.biojava.bio.symbol.RangeLocation;
+
 import org.biojava.bio.symbol.Symbol;
+
 import org.biojava.bio.Annotation;
 
+
+
 import org.biojava.bio.BioException;
+
 import org.biojava.utils.ChangeVetoException;
+
 import java.util.ListIterator;
+
 import java.util.LinkedList;
+
 import java.util.List;
+import org.biojava.bio.*;
+
+
+
 
 
 /**
  * This class contains methods for calculating the results of proteolytic digestion
- * of a protein sequence.
+ * of a protein sequence
+ *
+ * <b> this class is not designed to be thread safe </b>
+ *
  * @author Michael Jones
+ * @author Mark Schreiber (refactoring, some documentation)
  */
+
 public class Digest {
-    
+
+
+
     private Protease protease;
-    
+
+
+
     private Sequence sequence;
-    
+
+
+
     private int maxMissedCleavages = 0;
-    
+
+
+
     public static String PEPTIDE_FEATURE_TYPE = "Peptide";
-    
-    public LinkedList peptideQue;
-    // private
-    
-    /** Creates new Digest */
+
+
+
+    private LinkedList peptideQue;
+
+    /** Creates a new Digest Bean*/
+
     public Digest() {
+
         /*try{
+
             protease = new Protease();
-	}catch (Exception e){
+
+        }catch (Exception e){
+
             //Should never happen
+
             e.printStackTrace();
-	    }*/
+
+            }*/
+
     }
-    
-    public void setProtese(Protease protease) {
+
+
+
+    public void setProtease(Protease protease) {
+
         this.protease = protease;
+
     }
-    
+
+
+
     public void setSequence(Sequence sequence) {
+
         this.sequence = sequence;
+
     }
-    
+
+
+
     public Sequence getSequence() {
+
         return sequence;
+
     }
-    
-    public void setMaxMissedCleavages(int maxMissedCleavages) {
-        this.maxMissedCleavages = maxMissedCleavages;
-    }
-    
-    /** Adds peptides as features to the Sequence in this class.
-     * For Example:
-     * <CODE>
-     * 
-     *         Sequence sequence = ...
-     *         Digest bioJavaDigest = new Digest();
-     *
-     *         bioJavaDigest.setMaxMissedCleavages(2);
-     *         bioJavaDigest.setProtese(Protease.getProteaseByName(Protease.ASP_N));
-     *         bioJavaDigest.setSequence(sequence);
-     *         bioJavaDigest.addDigestFeatures();
-     * </CODE>
-     * @throws BioException
+
+
+    /**
+     * Sets the maximum number of partial digest products to be annotated.
+     * @param maxMissedCleavages the max number of partial digest products
      */
+    public void setMaxMissedCleavages(int maxMissedCleavages) {
+
+        this.maxMissedCleavages = maxMissedCleavages;
+
+    }
+
+
+
+    /** Adds peptides as features to the Sequence in this class. The feature will
+     * contain a small annotation specifying the protease with the key "protease".
+
+     * For Example:
+
+     * <PRE>
+
+     *
+
+     *         Sequence sequence = ...
+
+     *         Digest bioJavaDigest = new Digest();
+
+     *
+
+     *         bioJavaDigest.setMaxMissedCleavages(2);
+
+     *         bioJavaDigest.setProtease(ProteaseManager.getProteaseByName(Protease.ASP_N));
+
+     *         bioJavaDigest.setSequence(sequence);
+
+     *         bioJavaDigest.addDigestFeatures();
+
+     * </PRE>
+
+     * @throws BioException if the Protease or Sequence are null.
+
+     */
+
     public void addDigestFeatures() throws BioException, ChangeVetoException {
+
         peptideQue = new LinkedList();
-        
+
         if(protease == null){
-            throw new BioException("Protease is null");
+            throw new BioException("Protease is null, use Digest.setProtease()");
         }
-        
+        if(sequence == null){
+            throw new BioException("Sequence is null, use Digest.setSequence()");
+        }
+
+
         List cleaveSites = protease.getCleaveageResidues().toList();
         boolean endoProtease = protease.isEndoProtease();
-        
-	List notCleave = protease.getNotCleaveResidues().toList();
-	//Returns null if the list is empty
-	if(notCleave == null){
-	    notCleave = new LinkedList();
-	}
+
+        List notCleave = protease.getNotCleaveResidues().toList();
+        //Returns null if the list is empty
+
+        if(notCleave == null){
+            notCleave = new LinkedList();
+        }
+
         int nTerm = 1;
-        
+
         if(cleaveSites == null || notCleave == null){
             throw new BioException("Protease contains null parameter");
         }
-        
+
+
         for (int j = 1; j <= sequence.length(); j++) {
             Symbol aa = sequence.symbolAt(j);
+
             if(cleaveSites.contains(aa)){
                 if (endoProtease) {
                     boolean cleave = true;
@@ -126,45 +232,49 @@ public class Digest {
                             cleave = false;
                         }
                     }
-                    
+
                     if (cleave)  {
                         Location loc = new RangeLocation(nTerm, j);
                         peptideQue.add(loc);
                         nTerm = j + 1;
                     }
+
                 } else {
                     if (j > 1) {
                         Location loc = new RangeLocation(nTerm, j-1);
                         peptideQue.add(loc);
-                        System.out.println(peptideQue);
+                        //System.out.println(peptideQue);
                         nTerm = j;
                     }
                 }
             }
         }
-        
+
         if (nTerm <= sequence.length()) {
             Location loc = new RangeLocation(nTerm, sequence.length());
             peptideQue.add(loc);
         }
-        
+
         addMissedCleavages();
-        
+
         //Now add the locations as Peptide freatures to the Sequence
         for(ListIterator li = peptideQue.listIterator(); li.hasNext(); ){
             createPeptideFeature((Location)li.next());
         }
     }
-    
+
+
+
     private void addMissedCleavages() throws BioException {
         LinkedList missedList = new LinkedList();
+
         if(maxMissedCleavages>0){
             for(ListIterator li = peptideQue.listIterator(); li.hasNext(); ){
                 Location loc = (Location)li.next();
                 Location loc2 = null;
                 int min = loc.getMin();
                 int max = 0;
-                
+
                 //Get the numMissedCleavages location ahead of the current location
                 int numAdvanced = 0;
                 for(int i=0; i<maxMissedCleavages; i++){
@@ -180,18 +290,33 @@ public class Digest {
                     loc = ((Location)li.previous());
                 }
             }
+
             //Add all the missed peptides to the overall list
             peptideQue.addAll(missedList);
         }
     }
-    
+
+
+
     private void createPeptideFeature(Location loc)
+
     throws BioException, ChangeVetoException {
+        Annotation anno = new SmallAnnotation();
+        anno.setProperty("Protease",this.protease.getName());
+
         Feature.Template template = new Feature.Template();
+
         template.type = PEPTIDE_FEATURE_TYPE;
+
         template.source = this.getClass().getName();
+
         template.location = loc;
-        template.annotation = Annotation.EMPTY_ANNOTATION;
+
+        template.annotation = anno;
+
         sequence.createFeature(template);
+
     }
+
 }
+
--- src/org/biojava/bio/proteomics/Protease.java
+++ src/org/biojava/bio/proteomics/Protease.java
@@ -1,222 +1,285 @@
 /*
+
  *                    BioJava development code
+
  *
+
  * This code may be freely distributed and modified under the
+
  * terms of the GNU Lesser General Public Licence.  This should
+
  * be distributed with the code.  If you do not have a copy,
+
  * see:
+
  *
+
  *      http://www.gnu.org/copyleft/lesser.html
+
  *
+
  * Copyright for this code is held jointly by the individual
+
  * authors.  These should be listed in @author doc comments.
+
  *
+
  * For more information on the BioJava project and its aims,
+
  * or to join the biojava-l mailing list, visit the home page
+
  * at:
+
  *
+
  *      http://www.biojava.org/
+
  *
+
  */
+
+
 
 package org.biojava.bio.proteomics;
 
-//import com.sun.xml.parser.Resolver;
-//import com.sun.xml.tree.XmlDocument;
-import org.biojava.bio.BioException;
+import java.io.*;
+
+
+import org.biojava.bio.*;
+import org.biojava.bio.seq.*;
+import org.biojava.bio.seq.io.*;
 import org.biojava.bio.symbol.*;
-import org.biojava.bio.seq.io.SymbolTokenization;
-//import org.biojava.bio.seq.*
 
-import javax.xml.parsers.*;
 
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ResourceBundle;
-import java.util.MissingResourceException;
-import java.net.URL;
-import java.util.ArrayList;
 
 
 /** The protease class stores parameters needed by Digest to digest a protein sequence.
- * A custom protease can be created or one derived from the attributes set in the
- * ProteaseManager.xml resource.
- * @author Michael Jones
- */
-public class Protease {
-    
-    static Document doc = null;
-    
-    static {
-         
-          try {
-              InputStream tablesStream = Protease.class.getClassLoader().getResourceAsStream(
-                "org/biojava/bio/proteomics/ProteaseManager.xml"
-              );
-              if(tablesStream == null ) {
-                throw new BioException("Couldn't locate ProteaseManager.xml.");
-              }
 
-              InputSource is = new InputSource(tablesStream);
-              DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-	      doc = parser.parse(is);
-            }catch (MissingResourceException mre) {
-                System.err.println(mre.getMessage());
-            }catch(Exception e){//err
-                e.printStackTrace();
-            }
-    }
-    
-    public static String TRYPSIN = "Trypsin";
-    public static String LYS_C = "Lys-C";
-    public static String Arg_C = "Arg-C";
-    public static String ASP_N = "Asp-N";
-    public static String GLU_C_BICARB = "Glu-C-bicarbonate";
-    public static String GLU_C_PHOS = "Glu-C-phosphate";
-    public static String CHYMOTRYP = "Chymostrypsin";
-    public static String CNBr = "CNBr"; 
-    
+ * A custom protease can be created or one derived from the attributes set in the
+
+ * ProteaseManager.xml resource.
+
+ * @author Michael Jones
+ * @author Mark Schreiber (refactoring to ProteaseManager)
+
+ */
+
+public class Protease implements Serializable {
+
+    public static final String TRYPSIN = ProteaseManager.TRYPSIN;
+    public static final String LYS_C = ProteaseManager.LYS_C;
+    public static final String ARG_C = ProteaseManager.ARG_C;
+    public static final String ASP_N = ProteaseManager.ASP_N;
+    public static final String GLU_C_BICARB = ProteaseManager.GLU_C_BICARB;
+    public static final String GLU_C_PHOS = ProteaseManager.GLU_C_PHOS;
+    public static final String CHYMOTRYP = ProteaseManager.CHYMOTRYP;
+    public static final String CNBr = ProteaseManager.CNBr;
+
     private SymbolList cleavageResidues;
+
     private SymbolList notCleaveResidues;
+
     private boolean endoProtease = true;
-    
-    /** Creates new Protease */
-    public Protease(SymbolList cleaveRes, 
-                    boolean endoProtease, 
-                    SymbolList notCleaveRes) 
-                                   throws IllegalSymbolException, BioException {
-        this.cleavageResidues = cleaveRes;
-        this.endoProtease = endoProtease;
-        this.notCleaveResidues = notCleaveRes;
+    private String name;
+
+
+    protected Protease(SymbolList cleaveRes,
+                    boolean endoProtease,
+                    SymbolList notCleaveRes,
+                    String name)throws IllegalSymbolException{
+
+      if(cleaveRes.getAlphabet() != ProteinTools.getAlphabet()){
+        throw new IllegalSymbolException
+        ("Cleaveage residues must be from the PROTEIN alphabet");
+      }
+      if(notCleaveRes.getAlphabet() != ProteinTools.getAlphabet() &&
+         notCleaveRes.getAlphabet() != Alphabet.EMPTY_ALPHABET){
+        throw new IllegalSymbolException
+        ("Cleaveage residues must be from the PROTEIN alphabet or an EmptySymbolList");
+      }
+      this.cleavageResidues = cleaveRes;
+      this.endoProtease = endoProtease;
+      this.notCleaveResidues = notCleaveRes;
+      this.name = name;
     }
-    
-    
-    public Protease(String cleaveRes, 
-                    boolean endoProtease, 
-                    String notCleaveRes) 
+
+    /**
+     * @deprecated Creating a Protease with this constructor will not register it
+     * with the ProteaseManager (use ProteaseManager.createProtease())
+     */
+
+    public Protease(SymbolList cleaveRes,
+
+                    boolean endoProtease,
+
+                    SymbolList notCleaveRes)
+
                                    throws IllegalSymbolException, BioException {
+
+
+
+    }
+
+
+
+
+    /**
+     * @deprecated Creating a Protease with this constructor will not register it
+     * with the ProteaseManager (use ProteaseManager.createProtease())
+     */
+
+    public Protease(String cleaveRes,
+
+                    boolean endoProtease,
+
+                    String notCleaveRes)
+
+                                   throws IllegalSymbolException, BioException {
+
         this.cleavageResidues = createSymbolList(cleaveRes);
+
         this.endoProtease = endoProtease;
+
         this.notCleaveResidues = createSymbolList(notCleaveRes);
+
     }
-    
-    /** Creates new Protease */
-    public Protease(String cleavageRes, boolean endoProtease) 
+
+
+
+    /**
+     * @deprecated Creating a Protease with this constructor will not register it
+     * with the ProteaseManager (use ProteaseManager.createProtease())
+     */
+
+    public Protease(String cleavageRes, boolean endoProtease)
+
                                   throws IllegalSymbolException, BioException {
+
         this.cleavageResidues = createSymbolList(cleavageRes);
+
         this.endoProtease = endoProtease;
+
         this.notCleaveResidues = createSymbolList("");
+
     }
-    
+
+
+    /**
+     * The list of residues that the protease will cleave at.
+     * @return the residues as a SymbolList
+     */
     public SymbolList getCleaveageResidues()
+
     {
         return cleavageResidues;
     }
-    
+
+    /**
+     * Gets the name of this Protease
+     * @return the name as a String
+     */
+    public String getName(){
+      return name;
+    }
+
+
+    /**
+     * The list of residues that will prevent cleavage if they follow the cleavage
+     * residue.
+     */
     public SymbolList getNotCleaveResidues()
+
     {
         return notCleaveResidues;
     }
-    
+
+
+
     public boolean isEndoProtease()
+
     {
+
         return endoProtease;
+
     }
-    
-    
-    /** Get the list of proteases defined in the ProteaseManager.xml file.
-     * @return An array of protease names
+
+
+
+
+
+    /**
+     * Get the list of Protease names defined in the ProteaseManager
+     * (Internally calls ProteaseManager.
+     * @return A String array of protease names
      */
+
     public static String[] getProteaseList(){
-      ArrayList list = new ArrayList();
-
-          NodeList children = doc.getDocumentElement().getChildNodes();
-          for(int i = 0; i < children.getLength(); i++) {
-            Node cnode = (Node) children.item(i); 
-            if(! (cnode instanceof Element)) {
-                continue;
-            }
-            Element child = (Element) cnode;
-            if(child.getNodeName().equals("protease")) {
-                String name = child.getAttribute("name");
-                list.add(name);
-            }
-         }
-
-        String[] names = new String[list.size()];
-        return (String[])list.toArray(names);
+      java.util.Set s = ProteaseManager.getNames();
+      String[] names = new String[s.size()];
+      return (String[])s.toArray(names);
     }
-    
-    /** Creates a protease instance based on the parameters defined in the
-     * ProteaseManager.xml
-     * @param proteaseName A protease name that is defined in the ProteaseManager.xml
-     * @return A protease instance for the given protease name.
+
+
+
+    /**
+     * Retrieves a reference to the named Protease.
+     * (Internally calls ProteaseManager.getProteaseByName())
+     * @param proteaseName A protease name that is registered in the ProteaseManager (case sensitive)
+     * @return A Protease instance for the given protease name
      */
-    public static Protease getProteaseByName(String proteaseName) 
-                                 throws IllegalSymbolException, BioException {
-        Protease protease = null;
-        
-            NodeList children = doc.getDocumentElement().getChildNodes();
-            for(int i = 0; i < children.getLength(); i++) {
-                Node cnode = (Node) children.item(i); 
-                if(! (cnode instanceof Element)) {
-                    continue;
-                }
-                Element child = (Element) cnode;
-                if(child.getNodeName().equals("protease")) {
-                    if(child.getAttribute("name").equals(proteaseName))
-                    {
-                        //Parameters
-                        String cleavRes = null;
-                        String exceptRes = null;
-                        boolean endo = false;
-                        NodeList proteaseNodes = child.getChildNodes();
-                        for(int j = 0; j < proteaseNodes.getLength(); j++)
-                        {
-                            Node cnode2 = (Node) proteaseNodes.item(j); 
-                            if(! (cnode2 instanceof Element)) {
-                                continue;
-                            }
-                            Element el = (Element) cnode2;
-                            String name = el.getNodeName();
-                            String content = el.getFirstChild().getNodeValue();
-                            if(name.equals("cleaveRes")) {
-                                cleavRes = content.trim();
-                            }else if(name.equals("exceptRes")) {
-                                exceptRes = content.trim();
-                            }else if(name.equals("endo")) {
-                                endo = new Boolean(content).booleanValue();
-                            }
-                        }
-                        
-                        if(cleavRes != null && exceptRes != null){
-                            protease = new Protease(cleavRes ,endo, exceptRes);
-                        }else if(cleavRes != null && exceptRes == null){
-                            protease = new Protease(cleavRes ,endo);                            
-                        }
-                        
-                    }
-                }
-            }
-        return protease;
-    }    
-    
-    private SymbolList createSymbolList(String seq) 
+
+    public static final Protease getProteaseByName(String proteaseName)
+                                 throws BioException {
+      return ProteaseManager.getProteaseByName(proteaseName);
+
+    }
+
+
+
+    private SymbolList createSymbolList(String seq)
+
                                   throws IllegalSymbolException, BioException {
-	SymbolList sList;
-	FiniteAlphabet prot 
+
+        if(seq == null || seq.trim().equals("")){
+          return SymbolList.EMPTY_LIST;
+        }
+
+        SymbolList sList;
+
+        FiniteAlphabet prot
+
                  = (FiniteAlphabet)AlphabetManager.alphabetForName("PROTEIN");
 
+
+
         SymbolTokenization tokenization = prot.getTokenization("token");
-      	sList = new SimpleSymbolList (tokenization, seq); 
-        return sList; 
+
+        sList = new SimpleSymbolList (tokenization, seq);
+
+        return sList;
+
     }
+
+    /**
+     * Prevent duplication of the object during Serialization
+     * @throws ObjectStreamException
+     */
+    protected Object readResolve() throws ObjectStreamException{
+      try {
+        if(ProteaseManager.registered(this.getName())){
+          return ProteaseManager.getProteaseByName(this.getName());
+        }else{
+          ProteaseManager.registerProtease(this);
+          return this;
+        }
+      }
+      catch (BioException ex) {
+        throw new BioError(ex,
+          "Assertion Error: Cannot register Protease instance following de serialization"
+        );
+      }
+    }
+
 }
+
