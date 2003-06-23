b5892c1a800e343fe6bffbb9ef924827e8765d83
dhuen <dhuen@7c6358e6-4a41-0410-a743-a5b2a554c398>
2003-06-23 21:10:47 +0000

    new functionality
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2744 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojava/bio/program/homologene/HomoloGroupFilter.java
@@ -0,0 +1,67 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojava.bio.program.homologene;
+
+import java.util.Set;
+
+public interface HomoloGroupFilter
+{
+    public boolean accept(HomoloGroup group);
+
+    public final static class AcceptAll implements HomoloGroupFilter
+    {
+        public boolean accept(HomoloGroup group) { return true; }
+    }
+
+    public final static class ByTaxon implements HomoloGroupFilter
+    {
+        Taxon taxon;
+
+        public ByTaxon(Taxon taxon)
+        {
+            this.taxon = taxon;
+        }
+
+        public boolean accept(HomoloGroup group)
+        {
+            Set taxa = group.getTaxa();
+
+            return taxa.contains(taxon);
+        }
+    }
+
+    public final static class ByMinIdentity implements HomoloGroupFilter
+    {
+        double minValue;
+
+        public ByMinIdentity(double minValue)
+        {
+            this.minValue = minValue;
+        }
+
+        public boolean accept(HomoloGroup group)
+        {
+            return (group.getMinIdentity() >= minValue);
+        }
+    }
+
+}
