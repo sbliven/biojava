8a1a425be1720e3ef8c001ef465c9191ed1b7ac9
mrp <mrp@7c6358e6-4a41-0410-a743-a5b2a554c398>
2003-10-14 16:17:47 +0000

    Added some documentation. Hopefully this class can be deprecated soon, but it seemed like a good idea to flag up what is going on right now.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@2969 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/utils/JDBCConnectionPool.java
+++ src/org/biojava/utils/JDBCConnectionPool.java
@@ -24,100 +24,126 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 /**
- * Really simple connection pool for JDBC databases
+ * Really simple connection pool for JDBC databases.
+ *
+ * <h2>Use:</h2>
+ * <pre>
+ * JDBCConnectionPool pool = new JDBCConnectionPool(jdbcURL, userName, passwd);
+ * ...
+ * Connection conn = pool.takeConnection();
+ * // do stuff with conn
+ * pool.putConnection(conn);
+ * // don't use conn from here on
+ *
+ * Statement stmt = pool.takeStatement();
+ * // do stuff with stmt
+ * pool.putStatement(stmt);
+ * // don't do anything else with stmt
+ * </pre>
+ *
+ * <p>It is not a good idea to call <code>close()</code> on a connection you
+ * get from a pool. This would prevent it from being re-used. Also, we have
+ * seen some odd behavior with connections involved in transactions being
+ * re-used. We have not yet identified exactly how you can safely use a
+ * pooled connection for transaction-safe code.</p>  
+ *
+ * <p><em>Note:</em> We should probably be moving to a propper connection pool
+ * API. Let's standardise on one soon.</p>
  *
  * @author Thomas Down
+ * @author Matthew Pocock
  */
 
 public class JDBCConnectionPool {
-    private final String dbURL;
-    private final String dbUser;
-    private final String dbPass;
+  private final String dbURL;
+  private final String dbUser;
+  private final String dbPass;
 
-    private List connectionPool;
+  private LinkedList connectionPool;
 
-    {
-	connectionPool = new ArrayList();
+  {
+    connectionPool = new LinkedList();
+  }
+
+  public JDBCConnectionPool(String url, String user, String pass)
+  {
+    dbURL = url;
+    dbUser = user;
+    dbPass = pass;
+  }
+
+  public JDBCConnectionPool(String url)
+  {
+    this(url, null, null);
+  }
+
+  //
+  // Manage a pool of transactions with the database.
+  //
+
+  public Connection takeConnection()
+          throws SQLException
+  {
+    Connection conn = null;
+
+    synchronized (connectionPool) {
+      if (connectionPool.size() > 0) {
+        conn = (Connection) connectionPool.removeFirst();
+      }
     }
 
-    public JDBCConnectionPool(String url, String user, String pass) 
-    {
-	dbURL = url;
-	dbUser = user;
-	dbPass = pass;
-    }
+    // We don't perform the isClosed in the synchronized block in case the
+    // network is being slow.
 
-    public JDBCConnectionPool(String url)
-    {
-        this(url, null, null);
-    }
-
+    //if (conn != null) {
+    //    if (!conn.isClosed()) {
+    //            // hack for turfing out bad connections
+    //            Statement stmt = conn.createStatement();
+    //            stmt.execute("SELECT 1");
+    //            return conn;
     //
-    // Manage a pool of transactions with the database.
-    //
+    //    } else {
+    //	// We simply drop conn on the floor.  It should be safely collected.
+    //	return takeConnection();
+    //    }
+    //}
 
-    public Connection takeConnection()
-        throws SQLException
-    {
-	Connection conn = null;
+    // Statement-pool was empty -- let's create a new connection.
 
-	synchronized (connectionPool) {
-	    if (connectionPool.size() > 0) {
-		conn = (Connection) connectionPool.remove(0);
-	    }
-	}
-
-	// We don't perform the isClosed in the synchronized block in case the
-	// network is being slow.
-
-	//if (conn != null) {
-	//    if (!conn.isClosed()) {
-        //            // hack for turfing out bad connections
-        //            Statement stmt = conn.createStatement();
-        //            stmt.execute("SELECT 1");
-        //            return conn;
-        //        
-	//    } else {
-	//	// We simply drop conn on the floor.  It should be safely collected.
-	//	return takeConnection();
-	//    }
-	//}
-
-	// Statement-pool was empty -- let's create a new connection.
-
-        if(dbUser != null) {
- 	  conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
-        } else {
-          conn = DriverManager.getConnection(dbURL);
-        }
-	Statement st = conn.createStatement();
-	// st.execute("SET OPTION SQL_BIG_SELECTS=1");
-	st.close();
-	return conn;
+    if(dbUser != null) {
+      conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+    } else {
+      conn = DriverManager.getConnection(dbURL);
     }
+    Statement st = conn.createStatement();
+    // st.execute("SET OPTION SQL_BIG_SELECTS=1");
+    st.close();
+    return conn;
+  }
 
-    public void putConnection(Connection c) 
-        throws SQLException
-    {
-	synchronized (connectionPool) {
-	    connectionPool.add(c);
-	}
+  public void putConnection(Connection c)
+          throws SQLException
+  {
+    if(!c.isClosed()) {
+      synchronized (connectionPool) {
+        connectionPool.addLast(c);
+      }
     }
+  }
 
-    public Statement takeStatement() 
-        throws SQLException
-    {
-	return takeConnection().createStatement();
-    }
+  public Statement takeStatement()
+          throws SQLException
+  {
+    return takeConnection().createStatement();
+  }
 
-    public void putStatement(Statement st)
-        throws SQLException
-    {
-	putConnection(st.getConnection());
-	st.close();
-    }
+  public void putStatement(Statement st)
+          throws SQLException
+  {
+    putConnection(st.getConnection());
+    st.close();
+  }
 }
