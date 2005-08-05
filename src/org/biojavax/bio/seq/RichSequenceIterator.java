ec26f933325e95c5c88627235bba4d68aba86fb2
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2005-08-05 05:03:29 +0000

    initial commit
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3468 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojavax/bio/seq/RichSequenceIterator.java
@@ -0,0 +1,46 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+/*
+ * RichSequenceIterator.java
+ *
+ * Created on August 5, 2005, 1:46 PM
+ */
+
+package org.biojavax.bio.seq;
+import org.biojava.bio.seq.Sequence;
+import org.biojava.bio.seq.SequenceIterator;
+import org.biojavax.bio.seq.RichSequence;
+
+/**
+ * Essentially the same as <code>SequenceIterator</code>. It provides a new
+ * method that returns <code>RichSequence</code> objects without the need for
+ * explicit casting. Implementations of this interface should <b>always</b>
+ * return <code>RichSequence</code> objects for both the <code>nextSequence()</code>
+ * and <code>nextRichSequence</code> methods.
+ *
+ * @author Mark Schreiber
+ * @see org.biojava.bio.seq.SequenceIterator
+ */
+public interface RichSequenceIterator extends SequenceIterator{
+    public Sequence nextSequence();
+    public RichSequence nextRichSequence();
+}
