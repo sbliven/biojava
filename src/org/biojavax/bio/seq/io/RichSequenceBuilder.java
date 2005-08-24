829fb27ef4965d18c151cd9923b00c58602c5f2b
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2005-08-24 07:57:59 +0000

    initial commit
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3518 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojavax/bio/seq/io/RichSequenceBuilder.java
@@ -0,0 +1,36 @@
+/*
+ * RichSequenceBuilder.java
+ *
+ * Created on August 24, 2005, 4:43 PM
+ */
+
+package org.biojavax.bio.seq.io;
+
+import org.biojava.bio.BioException;
+import org.biojava.bio.seq.Sequence;
+import org.biojava.bio.seq.io.SequenceBuilder;
+import org.biojavax.bio.seq.RichSequence;
+
+/**
+ * An interface for objects that can build <code>RichSequence</code>s.
+ * @author Mark Schreiber
+ */
+public interface RichSequenceBuilder extends RichSeqIOListener, SequenceBuilder{
+    
+    /**
+     * Inherited from <code>SequenceBuilder</code>.
+     * Implementations of this for a <code>RichSequenceBuilder</code> should
+     * call <code>makeRichSequence()</code>
+     * @return a <code>RichSequence</code>
+     * @see makeRichSequence()
+     * @throws BioException if it is not possible to build a <code>RichSequence</code>
+     */
+    public Sequence makeSequence() throws BioException;
+    
+    /**
+     * Build a <code>RichSequence</code>.
+     * @return a <code>RichSequence</code>
+     * @throws BioException if it is not possible to build a <code>RichSequence</code>
+     */
+    public RichSequence makeRichSequence() throws BioException;
+}
