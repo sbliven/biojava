449e8cc033dd2449184ffe525d739ba2448f687e
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2005-08-31 07:56:26 +0000

    static implementations
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3533 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojavax/bio/seq/io/RichSequenceBuilderFactory.java
+++ src/org/biojavax/bio/seq/io/RichSequenceBuilderFactory.java
@@ -26,18 +26,36 @@ import org.biojava.bio.symbol.PackedSymbolListFactory;
 /**
  * Simple factory for constructing new RichSequenceBuilder objects.
  * @author Richard Holland
+ * @author Mark Schreiber
+ * @since 1.5
  */
 public interface RichSequenceBuilderFactory extends SequenceBuilderFactory {
+    
+    /**
+     * The value that will be used as a threshold for the <code>THRESHOLD</code>
+     * builder. Set to 5000.
+     */
+    public final static int THRESHOLD_VALUE = 5000;
     
     /**
      * Accessor for the default factory. This implementation will not 
      * do any compression of a sequence regardless of size.
      */
-    public final static RichSeqIOListener FACTORY = new SimpleRichSequenceBuilder();
+    public final static RichSequenceBuilderFactory FACTORY = new SimpleRichSequenceBuilderFactory();
     
     /**
      * Accessor for a factory that produces builders that compress the
-     * SymbolList of a Sequence
+     * <code>SymbolList</code> of a <code>RichSequence</code>.
      */
-    public final static RichSeqIOListener PACKED = new SimpleRichSequenceBuilder(new PackedSymbolListFactory());
+    public final static RichSequenceBuilderFactory PACKED = new SimpleRichSequenceBuilderFactory(new PackedSymbolListFactory(), 0);
+    
+    /**
+     * Accessor for a factory that produces builders that compress the
+     * <code>SymbolList</code> of a <code>RichSequence</code> when the length of the
+     * <code>SymbolList</code> exceeds <code>THRESHOLD</code>.
+     */
+    public final static RichSequenceBuilderFactory THRESHOLD = new SimpleRichSequenceBuilderFactory(new PackedSymbolListFactory(), THRESHOLD_VALUE);
+    
+    
+    
 }
