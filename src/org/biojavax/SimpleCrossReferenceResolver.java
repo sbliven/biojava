760eca218aa7d7022d67a3efd1cf3a2f72356b75
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2005-09-01 08:02:04 +0000

    intitial checkin
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3545 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ src/org/biojavax/SimpleCrossReferenceResolver.java
@@ -0,0 +1,71 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
+package org.biojavax;
+import java.util.Iterator;
+import org.biojava.bio.symbol.Alphabet;
+import org.biojava.bio.symbol.FiniteAlphabet;
+import org.biojava.bio.symbol.SymbolList;
+import org.biojavax.bio.BioEntry;
+import org.biojavax.bio.db.RichObjectFactory;
+import org.biojavax.bio.seq.InifinitelyAmbiguousSymbolList;
+import org.biojavax.bio.seq.RichSequence;
+
+/**
+ * {@inheritDoc}
+ * @author Richard Holland
+ * @author Mark Schreiber
+ * @since 1.5
+ */
+public class SimpleCrossReferenceResolver implements CrossReferenceResolver {
+    
+    /**
+     * {@inheritDoc}
+     */
+    public SymbolList getRemoteSymbolList(CrossRef cr, FiniteAlphabet a) {
+        Namespace ns = (Namespace)RichObjectFactory.getObject(Namespace.class, new Object[]{cr.getDbname()});
+        String accession = cr.getAccession();
+        for (Iterator i = ns.getMembers().iterator(); i.hasNext(); ) {
+            BioEntry be = (BioEntry)i.next();
+            if (be instanceof RichSequence) {
+                RichSequence rs = (RichSequence)be;
+                if (rs.getAccession().equals(accession) && rs.getVersion() == cr.getVersion()) return rs;
+            }
+        }
+        // If we get here we didn't find it, so we must create a dummy sequence instead
+        if (!(a instanceof FiniteAlphabet)) throw new IllegalArgumentException("Cannot construct dummy symbol list for a non-finite alphabet");
+        return new InifinitelyAmbiguousSymbolList((FiniteAlphabet)a);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public BioEntry getRemoteBioEntry(CrossRef cr){
+        Namespace ns = (Namespace)RichObjectFactory.getObject(Namespace.class, new Object[]{cr.getDbname()});
+        String accession = cr.getAccession();
+        for (Iterator i = ns.getMembers().iterator(); i.hasNext(); ) {
+            BioEntry be = (BioEntry)i.next();  
+            if (be.getAccession().equals(accession) && be.getVersion() == cr.getVersion()) return be;
+        }
+        return null;
+    }
+}
+
