ec77804706083c34749fc09e9ff5a6232cbf76fd
mszugat <mszugat@7c6358e6-4a41-0410-a743-a5b2a554c398>
2005-10-23 19:05:29 +0000

    Initial commit of the code to execute external programs
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3634 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ tests/org/biojava/utils/process/AllTests.java
@@ -0,0 +1,40 @@
+/*    
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*
+ *    AllTests.java
+ *    Copyright (C) 2005 BioWeka.org
+ */
+package org.biojava.utils.process;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(AllTests.suite());
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for org.biojava.utils.process");
+        //$JUnit-BEGIN$
+        suite.addTestSuite(ExternalProcessTest.class);
+        //$JUnit-END$
+        return suite;
+    }
+
+}
