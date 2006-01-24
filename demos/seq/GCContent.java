bff250c7c318381cd5dbae83068f97fc6f5ecd25
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2006-01-24 13:44:46 +0000

    updated to biojavax, added biojava boiler plate
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3756 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/seq/GCContent.java
+++ demos/seq/GCContent.java
@@ -1,36 +1,59 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
 package seq;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import org.biojava.bio.seq.DNATools;
+import org.biojava.bio.seq.Sequence;
+import org.biojava.bio.symbol.Symbol;
+import org.biojavax.RichObjectFactory;
+import org.biojavax.bio.seq.RichSequence;
+import org.biojavax.bio.seq.RichSequenceIterator;
 
-import org.biojava.bio.seq.io.*;
-import org.biojava.bio.seq.*;
-import org.biojava.bio.symbol.*;
 
+
+/**
+ * Counts the GC content of a sequence in fasta format
+ * @author Mark Schreiber
+ */ 
 public class GCContent {
+    
+    /**
+     * Run the program
+     * @param args a fasta file
+     */
     public static void main(String[] args)
         throws Exception
     {
         if (args.length != 1)
-	    throw new Exception("usage: java GCContent filename.fa");
+	    throw new Exception("usage: java seq.GCContent filename.fa");
 	String fileName = args[0];
-       
-	// Set up stream reader
-
-	Alphabet dna = DNATools.getDNA();
-	SymbolTokenization dnaParser = dna.getTokenization("token");
-	BufferedReader br = new BufferedReader(
-			        new FileReader(fileName));
-	SequenceBuilderFactory fact = new FastaDescriptionLineParser.Factory(
-					      SimpleSequenceBuilder.FACTORY);
-	StreamReader stream = new StreamReader(br,
-					       new FastaFormat(),
-					       dnaParser,
-					       fact);
-
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+	RichSequenceIterator it = 
+                RichSequence.IOTools.readFastaDNA(br, RichObjectFactory.getDefaultNamespace());
+        
 	// Iterate over all sequences in the stream
-
-	while (stream.hasNext()) {
-	    Sequence seq = stream.nextSequence();
+	while (it.hasNext()) {
+	    Sequence seq = it.nextRichSequence();
 	    System.out.println("Length: " + seq.length());
 	    int gc = 0;
 	    for (int pos = 1; pos <= seq.length(); ++pos) {
@@ -40,7 +63,7 @@ public class GCContent {
 	    }
 	    System.out.println(seq.getName() + ": " + 
 			       ((gc * 100.0) / seq.length()) + 
-			       "%");
+			       " %");
 	}
     }			       
 }
