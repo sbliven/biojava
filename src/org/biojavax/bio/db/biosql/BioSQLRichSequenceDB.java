a50933f15499847f546bf30b75fe0d1bed8e3d23
holland <holland@7c6358e6-4a41-0410-a743-a5b2a554c398>
2006-01-24 03:00:30 +0000

    Added GPL licence to BioSQLRichSequenceDB and BioSQLBioEntryDB.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3750 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojavax/bio/db/biosql/BioSQLRichSequenceDB.java
+++ src/org/biojavax/bio/db/biosql/BioSQLRichSequenceDB.java
@@ -1,7 +1,22 @@
 /*
- * BioSQLRichSequenceDB.java
+ *                    BioJava development code
  *
- * Created on January 24, 2006, 9:41 AM
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
  */
 
 package org.biojavax.bio.db.biosql;
