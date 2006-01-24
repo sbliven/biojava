a65e293c439173643b15b8940bc4a7ded3ea84a9
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2006-01-24 13:06:19 +0000

    updated for biojavax
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3754 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/seq/FetchSeq.java
+++ demos/seq/FetchSeq.java
@@ -1,33 +1,44 @@
 package seq;
 
-import org.biojava.utils.*;
-import org.biojava.bio.*;
-import org.biojava.bio.seq.*;
-import org.biojava.bio.seq.db.*;
-import org.biojava.bio.seq.io.*;
+import org.biojavax.RichObjectFactory;
+import org.biojavax.bio.db.ncbi.GenbankRichSequenceDB;
+import org.biojavax.bio.seq.RichSequence;
+
 
 /**
  * Fetch sequences by accession from the NCBI efetch server and dump them as genbank.
+ * Updated to use RichSequence and BioJavaX.
  *
  * Messages go to stderr and all data output goes to stdout.
  *
  * @author Matthew Pocock
+ * @author Mark Schreiber
  */
+
 public class FetchSeq {
+
+  /**
+   * Run the program
+   * @param args one or more genbank accessions
+   */
   public static void main(String[] args) {
-    GenbankSequenceDB genbank = new GenbankSequenceDB();
+
+    GenbankRichSequenceDB genbank = new GenbankRichSequenceDB();
 
     for(int i = 0; i < args.length; i++) {
+
       try {
+
         System.err.println("Fetching: " + args[i]);
-        
-        Sequence seq = genbank.getSequence(args[i]);
-        SeqIOTools.writeGenbank(System.out, seq);
+        RichSequence seq = genbank.getRichSequence(args[i]);
+        RichSequence.IOTools.writeGenbank(System.out, seq, RichObjectFactory.getDefaultNamespace());
         
         System.err.println("Done.");
+
       } catch (Throwable t) {
         t.printStackTrace(System.err);
       }
     }
   }
 }
+
