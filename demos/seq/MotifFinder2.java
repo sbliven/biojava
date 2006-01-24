8d703ce274285b63147f729f06a070ac62d140a2
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2006-01-24 13:57:06 +0000

    updated for biojavax
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3757 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/seq/MotifFinder2.java
+++ demos/seq/MotifFinder2.java
@@ -1,25 +1,55 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
 package seq;
 
 import java.io.*;
 import java.util.*;
-
-import org.biojava.bio.*;
-import org.biojava.bio.seq.*;
-import org.biojava.bio.seq.io.*;
-import org.biojava.bio.symbol.*;
+import org.biojava.bio.BioException;
+import org.biojava.bio.seq.DNATools;
+import org.biojava.bio.symbol.FiniteAlphabet;
+import org.biojava.bio.symbol.Symbol;
+import org.biojava.bio.symbol.SymbolList;
+import org.biojavax.RichObjectFactory;
+import org.biojavax.bio.seq.RichSequence;
+import org.biojavax.bio.seq.RichSequenceIterator;
 
 /**
- Modified version of MotifFinder.java
- This program will search a fasta file for a motiff.
- It will match motiffs and/or sequences containing ambiguity symbols.
- Modified by Andy Hammer, 7 Nov 2002.
+ *Modified version of MotifFinder.java
+ *This program will search a fasta file for a motif.
+ *It will match motifs and/or sequences containing ambiguity symbols.
+ *@author Matthew Pocock
+ *@author Andy Hammer
+ *@author Mark Schreiber
  */
 
 public class MotifFinder2 {
 
+  /**
+   * Run the Program
+   * @param args a motif (eg acgtnnc) and a fasta file
+   */
   public static void main(String[] args) throws BioException, IOException {
     if (args.length < 2) {
-      System.err.println("Usage: MotifFinder2 <motif> <seqfile.fasta>");
+      System.err.println("Usage: seq.MotifFinder2 <motif> <seqfile.fasta>");
     }
     SymbolList motif = DNATools.createDNA(args[0]);
     int motLength = motif.length();
@@ -28,9 +58,10 @@ public class MotifFinder2 {
     String fileName = args[1];
     FileReader stream = new FileReader(fileName);
     BufferedReader br = new BufferedReader(stream);
-    SequenceIterator si = SeqIOTools.readFastaDNA(br);
+    RichSequenceIterator si = RichSequence.IOTools.readFastaDNA(br, RichObjectFactory.getDefaultNamespace());
+    
     while (si.hasNext()) {
-      Sequence sequence = si.nextSequence();
+      RichSequence sequence = si.nextRichSequence();
       int seqLength = sequence.length();
       for (int pos = 1; pos <= seqLength - motLength + 1; ++pos) {
         SymbolList sub = sequence.subList(pos, pos + motLength - 1);
