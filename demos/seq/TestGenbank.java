16dc822fa6eb6a01224bacd05111e53a3ff6ef65
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2006-01-25 09:56:34 +0000

    updated for biojavax
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3767 7c6358e6-4a41-0410-a743-a5b2a554c398

--- demos/seq/TestGenbank.java
+++ demos/seq/TestGenbank.java
@@ -1,98 +1,94 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
 package	seq;
 
 import java.io.*;
 import java.util.*;
+import org.biojavax.Namespace;
+import org.biojavax.RichAnnotation;
+import org.biojavax.RichObjectFactory;
+import org.biojavax.bio.seq.RichFeature;
+import org.biojavax.bio.seq.RichSequence;
+import org.biojavax.bio.seq.RichSequenceIterator;
 
-import org.biojava.bio.*;
-import org.biojava.bio.symbol.*;
-import org.biojava.bio.seq.*;
-import org.biojava.bio.seq.io.*;
 
+
+
+/**
+ * Demo GenBank reading
+ * @author Matthew Pocock
+ * @author Mark Schreiber
+ */ 
 public class TestGenbank {
-	public static void main(String [] args)
-	{
-		try
+    public static void main(String [] args) {
+        try {
+            if(args.length !=	1) {
+                throw new Exception("Use: seq.TestGenbank genbankFile");
+            }
+            
+            File genbankFile = new File(args[0]);
+            BufferedReader gReader = new BufferedReader(
+                    new	InputStreamReader(new FileInputStream(genbankFile)));
+            
+            Namespace ns = RichObjectFactory.getDefaultNamespace();
+            RichSequenceIterator seqI =
+                    RichSequence.IOTools.readGenbankDNA(gReader, ns);
+            
+            while(seqI.hasNext()) {
+                RichSequence seq = seqI.nextRichSequence();
+                System.out.println(
+                        seq.toString() +
+                        " has " + seq.countFeatures() + 
+                        " features");
+                
+                for(Iterator i = seq.features(); i.hasNext(); ) {
+                    RichFeature f = (RichFeature) i.next();
+                    System.out.println(
+                            "\t" + f.getType() + 
+                            "\t" + f.getLocation() + 
+                            "\t" + f.getAnnotation().asMap());
+                }
+                
+                
+                
+                // Annotation testing
+		RichAnnotation theAnnotation = (RichAnnotation)seq.getAnnotation();
+		java.util.Set theKeys = theAnnotation.keys();
+		java.util.Iterator theOtherIterator = theKeys.iterator();
+		while(theOtherIterator.hasNext())
 		{
-			if(args.length !=	1)
-			{
-				throw new Exception("Use: TestGenbank genbankFile");
-			}
-
-			File genbankFile = new File(args[0]);
-			SequenceFormat gFormat = new GenbankFormat();
-			BufferedReader gReader = new BufferedReader(
-					new	InputStreamReader(new FileInputStream(genbankFile)));
-			SequenceBuilderFactory sbFact =
-					new GenbankProcessor.Factory(SimpleSequenceBuilder.FACTORY);
-			Alphabet alpha = DNATools.getDNA();
-			SymbolTokenization rParser = alpha.getTokenization("token");
-			SequenceIterator seqI =
-					new	StreamReader(gReader, gFormat, rParser,	sbFact);
-
-			while(seqI.hasNext())
-			{
-				Sequence seq = seqI.nextSequence();
-				System.out.println(seq.getName() + " has " + seq.countFeatures() + " features");
-        
-        for(Iterator i = seq.features(); i.hasNext(); ) {
-          Feature f = (Feature) i.next();
-          System.out.println("\t" + f.getType() + "\t" + f.getLocation() + "\t" + f.getAnnotation().asMap());
+		    System.out.println();
+		    Object note = theOtherIterator.next();
+		    System.out.println(note);
+		}
+                
+                System.out.println("\n\nRE-GENERATING GENPEPT");
+                RichSequence.IOTools.writeGenbank(System.out, seq, ns);
+            }
+            
+            
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
         }
-        
-				// Feature (location) testing
-//				java.util.Iterator theIterator = seq.features();
-//				SeqFileFormer theFormatter = SeqFileFormerFactory.makeFormer("Genbank");
-//				while(theIterator.hasNext())
-//				{
-//					System.out.println();
-//					Feature theFeature = (Feature)theIterator.next();
-//					try
-//					{
-//						StrandedFeature castedFeature = (StrandedFeature)theFeature;
-//						StringBuffer theBuffer = new StringBuffer("Location: ");
-//						System.out.println(theFormatter.formatLocation(theBuffer, castedFeature.getLocation(), castedFeature.getStrand()));
-//					}
-//					catch (ClassCastException cce)
-//					{
-//						System.out.println("Feature is not Stranded");
-//						StringBuffer theBuffer = new StringBuffer("Location: ");
-//						System.out.println(theFormatter.formatLocation(theBuffer, theFeature.getLocation(), StrandedFeature.POSITIVE));
-//					}
-//					Annotation featureAnnotation = theFeature.getAnnotation();
-//					java.util.Set theKeys = featureAnnotation.keys();
-//					System.out.println("Keys:");
-//					java.util.Iterator keyIterator = theKeys.iterator();
-//					while(keyIterator.hasNext())
-//					{
-//						System.out.println("\t" + keyIterator.next());
-//					}
-//
-//					System.out.println();
-//					keyIterator = theKeys.iterator();
-//					while(keyIterator.hasNext())
-//					{
-//						Object tempKey = keyIterator.next();
-//						System.out.println(tempKey + ": " + featureAnnotation.getProperty(tempKey));
-//					}
-//				}
-
-				// Annotation testing
-//				Annotation theAnnotation = seq.getAnnotation();
-//				java.util.Set theKeys = theAnnotation.keys();
-//				java.util.Iterator theOtherIterator = theKeys.iterator();
-//				while(theOtherIterator.hasNext())
-//				{
-//					System.out.println();
-//					String theKey = theOtherIterator.next().toString();
-//					System.out.println(theKey + ":");
-//					System.out.println("\t" + theAnnotation.getProperty(theKey).toString());
-//				}
-			}
-		}
-		catch (Throwable t)
-		{
-			t.printStackTrace();
-			System.exit(1);
-		}
-	}
+    }
 }
--- demos/seq/TestRefSeqPrt.java
+++ demos/seq/TestRefSeqPrt.java
@@ -1,92 +1,86 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+
 package	seq;
 
 import java.io.*;
+import org.biojavax.Namespace;
+import org.biojavax.RichAnnotation;
+import org.biojavax.RichObjectFactory;
+import org.biojavax.SimpleNamespace;
+import org.biojavax.bio.seq.RichSequence;
+import org.biojavax.bio.seq.RichSequenceIterator;
 
-import org.biojava.bio.*;
-import org.biojava.bio.symbol.*;
-import org.biojava.bio.seq.*;
-import org.biojava.bio.seq.io.*;
 
+/**
+ * Demo of reading RefSeqProtein
+ * @author Matthew Pocock
+ * @author Greg Cox
+ * @author Mark Schreiber
+ */
 public class TestRefSeqPrt {
-	public static void main(String [] args)
-	{
-		try
+    
+    /**
+     * @param args a refseq protein file
+     */
+    public static void main(String [] args) {
+        try {
+            if(args.length !=	1) {
+                throw new Exception("Use: seq.TestRefSeqPrt refseqFile");
+            }
+            
+            File genbankFile = new File(args[0]);
+            
+            BufferedReader gReader = new BufferedReader(
+                    new	InputStreamReader(new FileInputStream(genbankFile)));
+            
+            Namespace ns = (Namespace)RichObjectFactory.getObject(
+                             SimpleNamespace.class, 
+                             new Object[]{"ref"}
+                          );
+            
+            RichSequenceIterator seqI =
+                    RichSequence.IOTools.readGenbankProtein(gReader, ns);
+            
+            while(seqI.hasNext()) {
+                RichSequence seq = seqI.nextRichSequence();
+                System.out.println(
+                        seq.toString() + 
+                        " has " + seq.countFeatures() + " features");
+                System.out.println("\tand is " + seq.length());
+                
+                // Annotation testing
+		RichAnnotation theAnnotation = (RichAnnotation)seq.getAnnotation();
+		java.util.Set notes = seq.getNoteSet();
+		java.util.Iterator theOtherIterator = notes.iterator();
+		while(theOtherIterator.hasNext())
 		{
-			if(args.length !=	1)
-			{
-				throw new Exception("Use: TestRefSeqPrt refseqFile");
-			}
-
-			File genbankFile = new File(args[0]);
-			SequenceFormat gFormat = new GenbankFormat();
-			BufferedReader gReader = new BufferedReader(
-					new	InputStreamReader(new FileInputStream(genbankFile)));
-			SequenceBuilderFactory sbFact =
-					new ProteinRefSeqProcessor.Factory(SimpleSequenceBuilder.FACTORY);
-			Alphabet alpha = ProteinTools.getTAlphabet();
-			SymbolTokenization rParser = alpha.getTokenization("token");
-			SequenceIterator seqI =
-					new	StreamReader(gReader, gFormat, rParser,	sbFact);
-
-			while(seqI.hasNext())
-			{
-				Sequence seq = seqI.nextSequence();
-				System.out.println(seq.getName() + " has " + seq.countFeatures() + " features");
-				System.out.println("\tand is " + seq.length());
-				// Feature (location) testing
-//				java.util.Iterator theIterator = seq.features();
-//				SeqFileFormer theFormatter = SeqFileFormerFactory.makeFormer("Genbank");
-//				while(theIterator.hasNext())
-//				{
-//					System.out.println();
-//					Feature theFeature = (Feature)theIterator.next();
-//					try
-//					{
-//						StrandedFeature castedFeature = (StrandedFeature)theFeature;
-//						StringBuffer theBuffer = new StringBuffer("Location: ");
-//						System.out.println(theFormatter.formatLocation(theBuffer, castedFeature.getLocation(), castedFeature.getStrand()));
-//					}
-//					catch (ClassCastException cce)
-//					{
-//						System.out.println("Feature is not Stranded");
-//						StringBuffer theBuffer = new StringBuffer("Location: ");
-//						System.out.println(theFormatter.formatLocation(theBuffer, theFeature.getLocation(), StrandedFeature.POSITIVE));
-//					}
-//					Annotation featureAnnotation = theFeature.getAnnotation();
-//					java.util.Set theKeys = featureAnnotation.keys();
-//					System.out.println("Keys:");
-//					java.util.Iterator keyIterator = theKeys.iterator();
-//					while(keyIterator.hasNext())
-//					{
-//						System.out.println("\t" + keyIterator.next());
-//					}
-//
-//					System.out.println();
-//					keyIterator = theKeys.iterator();
-//					while(keyIterator.hasNext())
-//					{
-//						Object tempKey = keyIterator.next();
-//						System.out.println(tempKey + ": " + featureAnnotation.getProperty(tempKey));
-//					}
-//				}
-
-				// Annotation testing
-//				Annotation theAnnotation = seq.getAnnotation();
-//				java.util.Set theKeys = theAnnotation.keys();
-//				java.util.Iterator theOtherIterator = theKeys.iterator();
-//				while(theOtherIterator.hasNext())
-//				{
-//					System.out.println();
-//					String theKey = theOtherIterator.next().toString();
-//					System.out.println(theKey + ":");
-//					System.out.println("\t" + theAnnotation.getProperty(theKey).toString());
-//				}
-			}
+			System.out.println();
+			Object note = theOtherIterator.next();
+			System.out.println(note);
 		}
-		catch (Throwable t)
-		{
-			t.printStackTrace();
-			System.exit(1);
-		}
-	}
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
 }
