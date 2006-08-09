f0d037ef0824ce87e1a03508db0f065b599284ae
andreas <andreas@7c6358e6-4a41-0410-a743-a5b2a554c398>
2006-08-09 11:38:08 +0000

    now uses magic numbers at the beginning of files to determine which compression used in the file
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@3955 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/utils/io/InputStreamProvider.java
+++ src/org/biojava/utils/io/InputStreamProvider.java
@@ -22,6 +22,7 @@
  */
 package org.biojava.utils.io;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -35,7 +36,6 @@ import java.util.zip.ZipFile;
 
 
 /** A class that provides an InputStream from a File. The file can be compressed or uncompressed. 
- * The file extension is checked to determine which compression is being used.
  *  
  * Currently supported
  * compressions:
@@ -55,6 +55,12 @@ import java.util.zip.ZipFile;
  */
 public class InputStreamProvider {
     
+    /**
+     * The magic number found at the start of a GZIP stream.
+     */
+    public static final int GZIP_MAGIC = 0x1f8b;
+    
+    
     public InputStreamProvider() {
         super();
         
@@ -72,6 +78,30 @@ public class InputStreamProvider {
         File f = new File(pathToFile);
         return getInputStream(f);
     }
+        
+    
+    /** open the file and read the magic number from the beginning
+     * this is used to determine the compression type
+     * 
+     * @param in an input stream to read from
+     * @return the magic number
+     * @throws IOException
+     */
+    private int getMagicNumber(InputStream in) 
+    throws IOException {
+              
+        
+        int t = in.read();
+        if (t < 0) throw new EOFException("Failed to read magic number");
+        int magic = (t & 0xff) << 8;
+        t = in.read();
+        if (t < 0) throw new EOFException("Failed to read magic number");
+        magic += t & 0xff;
+        
+        return magic;
+    }
+    
+    
     
     /** get an InputStream for the file
      * 
@@ -82,19 +112,37 @@ public class InputStreamProvider {
     public InputStream getInputStream(File f) 
     throws IOException
     {
+        // use the magic numbers to determine the compression type, 
+        // use file extension only as 2nd choice 
         
-        String fileName = f.getName();
+        int magic = 0;
+        
+        try {
+            InputStream test = new FileInputStream(f);
+            magic = getMagicNumber(test);
+            test.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        };
+        
         InputStream inputStream = null;
+       
+        String fileName = f.getName();
         
-        if ( fileName.endsWith(".gz")) {
-      
-            FileInputStream is = new FileInputStream(f);
-            inputStream = new GZIPInputStream(is);
-            
+        if (magic == UncompressInputStream.LZW_MAGIC ) {
+            // a Z compressed file
+            return openCompressedFile(f);
+        }
+        
+        else if (magic == GZIP_MAGIC ) {
+            return openGZIPFile(f); 
+        }
+        
+        else if ( fileName.endsWith(".gz")) {
+            return openGZIPFile(f);
         } 
         
         else if ( fileName.endsWith(".zip")){
-            
           
             ZipFile zipfile = new ZipFile(f);
             
@@ -129,8 +177,7 @@ public class InputStreamProvider {
         
         else if ( fileName.endsWith(".Z")) {
             // unix compressed 
-            InputStream is = new FileInputStream(f);
-            inputStream =  new UncompressInputStream(is);
+            return openCompressedFile(f);
             
         }
         
@@ -143,5 +190,22 @@ public class InputStreamProvider {
         return inputStream;
     }
     
+
+    
+    private InputStream openCompressedFile(File f)
+    throws IOException{
+        
+        InputStream is           = new FileInputStream(f);
+        InputStream inputStream =  new UncompressInputStream(is);
+        return inputStream;
+    }
+    
+    private InputStream openGZIPFile(File f) 
+    throws IOException{
+        
+        FileInputStream is      = new FileInputStream(f);
+        InputStream inputStream = new GZIPInputStream(is);
+        return inputStream;
+    }
    
 }
