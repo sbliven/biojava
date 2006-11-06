219cd473b0b8f77cb3a453abd9d3e93cf9ebfded
holland <holland@7c6358e6-4a41-0410-a743-a5b2a554c398>
2006-11-06 15:09:05 +0000

    Moved ENSJ out into own project.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@4005 7c6358e6-4a41-0410-a743-a5b2a554c398

--- ensj/src/org/ensembl/datamodel/create_cursor_files.pl
+++
@@ -1,6 +0,0 @@
-# Create Cursor classes from SNPCursor template
-foreach $TYPE ("Gene", "Exon", "Feature", "Marker", "SimilarityFeature", "Transcript", "Translation", "RepeatFeature", "Clone") {
-  $cmd = "perl -pe \"s/SNP/".$TYPE."/;\" SNPCursor.java > " . $TYPE . "Cursor.java";
-  print $cmd . "\n";
-  system $cmd;
-}
