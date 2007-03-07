f644a6a51da981aed515c712d48abb31309e8366
dhuen <dhuen@7c6358e6-4a41-0410-a743-a5b2a554c398>
2007-03-07 16:33:33 +0000

    test always fails because the test is wrong.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@4071 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/symbol/CrossProductAlphabetIndex.java
+++ src/org/biojava/bio/symbol/CrossProductAlphabetIndex.java
@@ -47,119 +47,116 @@ import org.biojava.utils.ChangeVetoException;
 //    we index might hold reference and this object will never be garbage
 //    collected ?
 // 2) just dont rethrow the IndexOutOfBounds exception in the
-//    symbolForIndex method    
+//    symbolForIndex method
 
 class CrossProductAlphabetIndex extends AbstractChangeable
-implements AlphabetIndex, ChangeListener, Serializable
-{	
-	// The alphabet we are indexing
-	final FiniteAlphabet	Alpha;
-	// list holding AlphabetIndexes for the alphabets making upp the alphabet we index
-	List		alphaIndexes;
-  List    revIndexes;
-
-	public CrossProductAlphabetIndex( FiniteAlphabet theAlpha ) {
-		// the alpha we index over
-		Alpha = theAlpha;
-		// listen to changes in our alpha
-		Alpha.addChangeListener( this, Alphabet.SYMBOLS );
-		// we need this to compute the index
-		alphaIndexes = buildIndexList( theAlpha );
-    revIndexes = new ArrayList(alphaIndexes);
-    Collections.reverse(revIndexes);
-	}
-
-	public FiniteAlphabet getAlphabet() {
-		return Alpha;
-	}
-
-	/**
-	* This just takes the alphabet, and splits it up into its sub alphabets
-	* then gets an AlphabetIndex for each sub alphabet and stores that in an
-	* List. This list is then used for the index computation
-	*/
-	private List buildIndexList( FiniteAlphabet fA ) {
-		List	subAlphas 	= fA.getAlphabets();
-		List	retList		= new ArrayList();
-
-		for( Iterator it = subAlphas.iterator() ; it.hasNext() ; ) {
-			// no need to check its FiniteAlphabets as we start with a FiniteAlphabet
-			FiniteAlphabet currAlpha = ( FiniteAlphabet )it.next();
-			AlphabetIndex currInd = AlphabetManager.getAlphabetIndex( currAlpha );
-			retList.add( currInd );
-		}
-		//System.out.println( retList );
-		return retList; 
-	}
-	
-	public void preChange( ChangeEvent cE ) throws ChangeVetoException {
-		// Should perhaps ask eventual listeners to this object if they think
-		// the change is ok, and refuse if they dont think it is ????????????????
-	}
-
-	public void postChange( ChangeEvent cE ) {
-		// ok, alphabet have changed, so we better rebuild or list of AlphabetIndexes 
-		alphaIndexes = buildIndexList( Alpha );
-    revIndexes = new ArrayList(alphaIndexes);
-    Collections.reverse(revIndexes);
-	}
-
-	public int indexForSymbol( Symbol s ) throws IllegalSymbolException {
-		// ok, split the symbol upp into its factors
-		List	factors = ( ( BasisSymbol )s ).getSymbols();
-    if(factors.size() != getAlphabet().size()) {
-      getAlphabet().validate(s);
+        implements AlphabetIndex, ChangeListener, Serializable {
+    // The alphabet we are indexing
+    final FiniteAlphabet	Alpha;
+    // list holding AlphabetIndexes for the alphabets making upp the alphabet we index
+    List		alphaIndexes;
+    List    revIndexes;
+    
+    public CrossProductAlphabetIndex( FiniteAlphabet theAlpha ) {
+        // the alpha we index over
+        Alpha = theAlpha;
+        // listen to changes in our alpha
+        Alpha.addChangeListener( this, Alphabet.SYMBOLS );
+        // we need this to compute the index
+        alphaIndexes = buildIndexList( theAlpha );
+        revIndexes = new ArrayList(alphaIndexes);
+        Collections.reverse(revIndexes);
     }
-
-		int	index	= 0; // this is the index we want to compute
-		
-		// iterate over each factor symbol, and at the same time iterate over the AlphabetIndexList
-		// and feed each symbol to its corresponding AlphabetIndex this should perhasp be iteration
-		// via arrays instead ? if that speeds it up
-		Iterator indexIt  = alphaIndexes.iterator();
-		Iterator symbolIt = factors.iterator();
-		
-    try {
-      while( symbolIt.hasNext() ) {
-        Symbol 		currentSymbol 	= ( Symbol )symbolIt.next();
-        AlphabetIndex 	currentAlphaInd = ( AlphabetIndex )indexIt.next();
-        FiniteAlphabet	currentAlphabet	= currentAlphaInd.getAlphabet();
+    
+    public FiniteAlphabet getAlphabet() {
+        return Alpha;
+    }
+    
+    /**
+     * This just takes the alphabet, and splits it up into its sub alphabets
+     * then gets an AlphabetIndex for each sub alphabet and stores that in an
+     * List. This list is then used for the index computation
+     */
+    private List buildIndexList( FiniteAlphabet fA ) {
+        List	subAlphas 	= fA.getAlphabets();
+        List	retList		= new ArrayList();
         
-        index = index * currentAlphabet.size() + currentAlphaInd.indexForSymbol( currentSymbol );
-      }
-    } catch (IllegalSymbolException ise) {
-      getAlphabet().validate(s);
+        for( Iterator it = subAlphas.iterator() ; it.hasNext() ; ) {
+            // no need to check its FiniteAlphabets as we start with a FiniteAlphabet
+            FiniteAlphabet currAlpha = ( FiniteAlphabet )it.next();
+            AlphabetIndex currInd = AlphabetManager.getAlphabetIndex( currAlpha );
+            retList.add( currInd );
+        }
+        //System.out.println( retList );
+        return retList;
     }
     
-    if(symbolIt.hasNext()) {
-      getAlphabet().validate(s);
-      throw new BioError("Assertion failure: Ran out of indexers for symbols");
+    public void preChange( ChangeEvent cE ) throws ChangeVetoException {
+        // Should perhaps ask eventual listeners to this object if they think
+        // the change is ok, and refuse if they dont think it is ????????????????
     }
     
-		return index;
-	}
-
-	public Symbol symbolForIndex( int index ) throws IndexOutOfBoundsException {
-		List	symbols	= new ArrayList();
-		try { 
-			for ( Iterator it = revIndexes.iterator() ; it.hasNext() ; ) {
-				AlphabetIndex	curAlpIndex 	= ( AlphabetIndex ) it.next();
-				int		curAlpSize 	= curAlpIndex.getAlphabet().size();
-				int		curSymIndValue 	= index % curAlpSize;
-				index = index / curAlpSize;
-				symbols.add( 0, curAlpIndex.symbolForIndex( curSymIndValue ) );
-			}
-		
-			return( Alpha.getSymbol( symbols ) );
-		}
-		catch( IllegalSymbolException isE ) {
-			throw new BioError(isE);
-		}
-		catch( IndexOutOfBoundsException ioobE ) {
-			// this is most likely bad, but i just rethrow the exception, i should do something
-			// clever here
-			throw ioobE;
-		}
-		
-	}
+    public void postChange( ChangeEvent cE ) {
+        // ok, alphabet have changed, so we better rebuild or list of AlphabetIndexes
+        alphaIndexes = buildIndexList( Alpha );
+        revIndexes = new ArrayList(alphaIndexes);
+        Collections.reverse(revIndexes);
+    }
+    
+    public int indexForSymbol( Symbol s ) throws IllegalSymbolException {
+        // ok, split the symbol up into its factors
+        List factors = ( ( BasisSymbol )s ).getSymbols();
+        if(factors.size() != getAlphabet().getAlphabets().size()) {
+            getAlphabet().validate(s);
+        }
+        
+        int	index	= 0; // this is the index we want to compute
+        
+        // iterate over each factor symbol, and at the same time iterate over the AlphabetIndexList
+        // and feed each symbol to its corresponding AlphabetIndex this should perhasp be iteration
+        // via arrays instead ? if that speeds it up
+        Iterator indexIt  = alphaIndexes.iterator();
+        Iterator symbolIt = factors.iterator();
+        
+        try {
+            while( symbolIt.hasNext() ) {
+                Symbol 		currentSymbol 	= ( Symbol )symbolIt.next();
+                AlphabetIndex 	currentAlphaInd = ( AlphabetIndex )indexIt.next();
+                FiniteAlphabet	currentAlphabet	= currentAlphaInd.getAlphabet();
+                
+                index = index * currentAlphabet.size() + currentAlphaInd.indexForSymbol( currentSymbol );
+            }
+        } catch (IllegalSymbolException ise) {
+            getAlphabet().validate(s);
+        }
+        
+        if(symbolIt.hasNext()) {
+            getAlphabet().validate(s);
+            throw new BioError("Assertion failure: Ran out of indexers for symbols");
+        }
+        
+        return index;
+    }
+    
+    public Symbol symbolForIndex( int index ) throws IndexOutOfBoundsException {
+        List	symbols	= new ArrayList();
+        try {
+            for ( Iterator it = revIndexes.iterator() ; it.hasNext() ; ) {
+                AlphabetIndex	curAlpIndex 	= ( AlphabetIndex ) it.next();
+                int		curAlpSize 	= curAlpIndex.getAlphabet().size();
+                int		curSymIndValue 	= index % curAlpSize;
+                index = index / curAlpSize;
+                symbols.add( 0, curAlpIndex.symbolForIndex( curSymIndValue ) );
+            }
+            
+            return( Alpha.getSymbol( symbols ) );
+        } catch( IllegalSymbolException isE ) {
+            throw new BioError(isE);
+        } catch( IndexOutOfBoundsException ioobE ) {
+            // this is most likely bad, but i just rethrow the exception, i should do something
+            // clever here
+            throw ioobE;
+        }
+        
+    }
 }
