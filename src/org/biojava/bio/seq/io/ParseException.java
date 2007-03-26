dc540bdb81df983959e327641a8ce6267c5000e7
mark_s <mark_s@7c6358e6-4a41-0410-a743-a5b2a554c398>
2007-03-26 06:26:12 +0000

    Added argument for comments
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@4080 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/seq/io/ParseException.java
+++ src/org/biojava/bio/seq/io/ParseException.java
@@ -55,15 +55,17 @@ public class ParseException extends BioException {
    * @param format the format object that was doing the parsing
    * @param accession the accession number of the record that failed
    * @param identifier the identifier of the sequence that failed (eg the GI number for genbank)
+   * @param comments any additional information
    * @param parseBlock the chunk of the file the parser was trying to parse when the error occured
    * @return the formatted error message
    */
-  public static String newMessage(Class format, String accession, String identifier, String parseBlock){
+  public static String newMessage(Class format, String accession, String identifier, String comments, String parseBlock){
       message = new StringBuffer();
       message.append(intro);
       message.append("Format_object=").append(format.getName()).append('\n');
       message.append("Accession=").append(accession).append('\n');
       message.append("Id=").append(identifier).append('\n');
+      message.append("Comments=").append(comments).append('\n');
       message.append("Parse_block=").append(parseBlock).append('\n');
       message.append("Stack trace follows ....\n\n");
       return message.toString();
