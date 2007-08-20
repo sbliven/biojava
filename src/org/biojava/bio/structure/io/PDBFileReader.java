704653d4a759dfb0d399df2bf9daafd4f93cc690
andreas <andreas@7c6358e6-4a41-0410-a743-a5b2a554c398>
2007-08-20 17:56:32 +0000

    now contains a flag to automatically fetch locally missing PDB files from the EBI  ftp server
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@4210 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/structure/io/PDBFileReader.java
+++ src/org/biojava/bio/structure/io/PDBFileReader.java
@@ -21,18 +21,21 @@
  * @author Andreas Prlic
  *
  *
- * some interesting PDB files ...
- * /nfs/disk100/pdb/pdbent/pdb1dw9.ent
- * chain a first group -> selenomethionine
  */
 package org.biojava.bio.structure.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.GZIPOutputStream;
 
 import org.biojava.bio.structure.AminoAcid;
 import org.biojava.bio.structure.Chain;
@@ -53,9 +56,9 @@ import org.biojava.utils.io.InputStreamProvider;
  * A:
  * <pre>
  String filename =  "path/to/pdbfile.ent" ;
- 
+
  PDBFileReader pdbreader = new PDBFileReader();
- 
+
  try{
      Structure struc = pdbreader.getStructure(filename);
      System.out.println(struc);
@@ -67,205 +70,290 @@ import org.biojava.utils.io.InputStreamProvider;
  *
  */
 public class PDBFileReader implements StructureIOFile {
-    
-    String path                     ;
-    List<String> extensions            ;
-    boolean parseSecStruc;
-    
-    public static void main(String[] args){
-        String filename =  "/path/to/5pti.pdb" ;
-        
-        PDBFileReader pdbreader = new PDBFileReader();
-        pdbreader.setParseSecStruc(true);
-        
-        try{
-            Structure struc = pdbreader.getStructure(filename);
-            System.out.println(struc);
-            
-            GroupIterator gi = new GroupIterator(struc);
-            while (gi.hasNext()){
-                Group g = (Group) gi.next();
-                Chain  c = g.getParent();
-                if ( g instanceof AminoAcid ){
-                    AminoAcid aa = (AminoAcid)g;
-                    Map sec = aa.getSecStruc();
-                    System.out.println(c.getName() + " " + g + " " + sec);
-                }
-                
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    
-    public PDBFileReader() {
-        extensions    = new ArrayList<String>();
-        path = "" ;
-        extensions.add(".ent");
-        extensions.add(".pdb");
-        extensions.add(".ent.gz");
-        extensions.add(".pdb.gz");
-        extensions.add(".ent.Z");
-        extensions.add(".pdb.Z");
-        parseSecStruc = false;
-        
-    }
-    
-    
-    
-    public boolean isParseSecStruc() {
-        return parseSecStruc;
-    }
+
+	String path                     ;
+	List<String> extensions            ;
+	boolean parseSecStruc;
+	boolean autoFetch;
+
+	public static void main(String[] args){
+		String filename =  "/path/to/5pti.pdb" ;
+
+		PDBFileReader pdbreader = new PDBFileReader();
+		pdbreader.setParseSecStruc(true);
+
+		try{
+			Structure struc = pdbreader.getStructure(filename);
+			System.out.println(struc);
+
+			GroupIterator gi = new GroupIterator(struc);
+			while (gi.hasNext()){
+				Group g = (Group) gi.next();
+				Chain  c = g.getParent();
+				if ( g instanceof AminoAcid ){
+					AminoAcid aa = (AminoAcid)g;
+					Map<String,String> sec = aa.getSecStruc();
+					System.out.println(c.getName() + " " + g + " " + sec);
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 
-    public void setParseSecStruc(boolean parseSecStruc) {
-        this.parseSecStruc = parseSecStruc;
-    }
+	public PDBFileReader() {
+		extensions    = new ArrayList<String>();
+		path = "" ;
+		extensions.add(".ent");
+		extensions.add(".pdb");
+		extensions.add(".ent.gz");
+		extensions.add(".pdb.gz");
+		extensions.add(".ent.Z");
+		extensions.add(".pdb.Z");
+		parseSecStruc = false;
+		autoFetch     = false;
+	}
 
 
-    /** directory where to find PDB files */
-    public void setPath(String p){
-        path = p ;
-    }
-    
-    /**
-     * Returns the path value.
-     * @return a String representing the path value
-     * @see #setPath
-     *
-     */
-    public String getPath() {
-        return path ;
-    }
-    
-    /** define supported file extensions 
-     * compressed extensions .Z,.gz do not need to be specified
-     * they are dealt with automatically.
-     
-     */
-    public void addExtension(String s){
-        //System.out.println("add Extension "+s);
-        extensions.add(s);
-    }
-    
-    
-    
-    
-    /** try to find the file in the filesystem and return a filestream in order to parse it 
-     * rules how to find file
-     * - first check: if file is in path specified by PDBpath
-     * - secnd check: if not found check in PDBpath/xy/ where xy is second and third char of PDBcode.
-     */
-    
-    private InputStream getInputStream(String pdbId) 
-    throws IOException
-    {
-        //System.out.println("checking file");
-        
-        // compression formats supported
-        // this has been moved to InputStreamProvider ...
-        
-        //String[] str = {".gz",".zip",".Z"};
-        //ArrayList  compressions = new ArrayList( Arrays.asList( str ) ); 
-        
-        InputStream inputStream =null;
-        
-        String pdbFile = null ;
-        File f = null ;
-        
-        // this are the possible PDB file names...
-        String fpath = path+"/"+pdbId;
-        String ppath = path +"/pdb"+pdbId;
-        
-        String[] paths = new String[]{fpath,ppath};
+	/** should the parser to fetch missing PDB files from the EBI FTP server automatically?
+	 *  default is false
+	 * @return flag
+	 */
+	public boolean isAutoFetch() {
+		return autoFetch;
+	}
 
-        for ( int p=0;p<paths.length;p++ ){
-            String testpath = paths[p];
-            //System.out.println(testpath);
-            for (int i=0 ; i<extensions.size();i++){
-                String ex = (String)extensions.get(i) ;
-                //System.out.println("PDBFileReader testing: "+testpath+ex);
-                f = new File(testpath+ex) ;
-            
-                if ( f.exists()) {
-                    //System.out.println("found!");
-                    pdbFile = testpath+ex ;
-                    
-                    InputStreamProvider isp = new InputStreamProvider();
-                    
-                    inputStream = isp.getInputStream(pdbFile);
-                    break;
-                }
-            
-                if ( pdbFile != null) break;        
-            }
-        }
-        
-        if ( pdbFile == null ) {
-            String message = "no structure with PDB code " + pdbId + " found!" ;
-            throw new IOException (message);
-        }
-        
-        return inputStream ;
-    }
-    
-    
-    
-    
-    
-    
-    /** load a structure from local file system and return a PDBStructure object 
-     
-     * @param pdbId  a String specifying the id value (PDB code)
-     * @return the Structure object
-     * @throws IOException ...
-     */
-    public Structure getStructureById(String pdbId) 
-    throws IOException
-    {
-        
-        
-        InputStream inStream = getInputStream(pdbId);
-                
-        PDBFileParser pdbpars = new PDBFileParser();
-        pdbpars.setParseSecStruc(parseSecStruc);
-        Structure struc = pdbpars.parsePDBFile(inStream) ;
-        return struc ;
-    }
-    
-    /** opens filename, parses it and returns
-     * aStructure object .
-     * @param filename  a String
-     * @return the Structure object
-     * @throws IOException ...
-     */
-    public Structure getStructure(String filename) 
-    throws IOException
-    {
-        File f = new File(filename);
-        return getStructure(f);
-                
-    }
-    
-    /** opens filename, parses it and returns a Structure object
-     * 
-     * @param filename a File object
-     * @return the Structure object
-     * @throws IOException ...
-     */
-    public Structure getStructure(File filename) throws IOException {
+	/** tell the parser to fetch missing PDB files from the EBI FTP server automatically.
+	 * 
+	 * default is false. If true, new PDB files will be automatically stored in the Path and gzip compressed.
+	 * 
+	 * @param autoFetch
+	 */
+	public void setAutoFetch(boolean autoFetch) {
+		this.autoFetch = autoFetch;
+	}
 
-        InputStreamProvider isp = new InputStreamProvider();
-        
-        InputStream inStream = isp.getInputStream(filename);
 
-        PDBFileParser pdbpars = new PDBFileParser();
-        pdbpars.setParseSecStruc(parseSecStruc);
-        Structure struc = pdbpars.parsePDBFile(inStream) ;
-        return struc ;
+	public boolean isParseSecStruc() {
+		return parseSecStruc;
+	}
 
-    }
-    
+
+	public void setParseSecStruc(boolean parseSecStruc) {
+		this.parseSecStruc = parseSecStruc;
+	}
+
+
+	/** directory where to find PDB files */
+	public void setPath(String p){
+		path = p ;
+	}
+
+	/**
+	 * Returns the path value.
+	 * @return a String representing the path value
+	 * @see #setPath
+	 *
+	 */
+	public String getPath() {
+		return path ;
+	}
+
+	/** define supported file extensions 
+	 * compressed extensions .Z,.gz do not need to be specified
+	 * they are dealt with automatically.
+
+	 */
+	public void addExtension(String s){
+		//System.out.println("add Extension "+s);
+		extensions.add(s);
+	}
+
+
+
+
+	/** try to find the file in the filesystem and return a filestream in order to parse it 
+	 * rules how to find file
+	 * - first check: if file is in path specified by PDBpath
+	 * - secnd check: if not found check in PDBpath/xy/ where xy is second and third char of PDBcode.
+	 * if autoFetch is set it will try to download missing PDB files automatically.
+	 */
+
+	private InputStream getInputStream(String pdbId) 
+	throws IOException
+	{
+		//System.out.println("checking file");
+
+		// compression formats supported
+		// this has been moved to InputStreamProvider ...
+
+		//String[] str = {".gz",".zip",".Z"};
+		//ArrayList  compressions = new ArrayList( Arrays.asList( str ) ); 
+
+		InputStream inputStream =null;
+
+		String pdbFile = null ;
+		File f = null ;
+
+		// this are the possible PDB file names...
+		String fpath = path+"/"+pdbId;
+		String ppath = path +"/pdb"+pdbId;
+
+		String[] paths = new String[]{fpath,ppath};
+
+		for ( int p=0;p<paths.length;p++ ){
+			String testpath = paths[p];
+			//System.out.println(testpath);
+			for (int i=0 ; i<extensions.size();i++){
+				String ex = (String)extensions.get(i) ;
+				//System.out.println("PDBFileReader testing: "+testpath+ex);
+				f = new File(testpath+ex) ;
+
+				if ( f.exists()) {
+					//System.out.println("found!");
+					pdbFile = testpath+ex ;
+
+					InputStreamProvider isp = new InputStreamProvider();
+
+					inputStream = isp.getInputStream(pdbFile);
+					break;
+				}
+
+				if ( pdbFile != null) break;        
+			}
+		}
+
+		if ( pdbFile == null ) {
+			if ( autoFetch) 
+				return downloadAndGetInputStream(pdbId);
+
+			String message = "no structure with PDB code " + pdbId + " found!" ;
+			throw new IOException (message);
+		}
+
+		return inputStream ;
+	}
+
+
+	public void gzipCompress(Structure s){
+
+	}
+
+
+	private File downloadPDB(String pdbId){
+	
+		File tempFile = new File(path+"/"+pdbId+".pdb.gz");		
+		File pdbHome = new File(path);
+		
+		if ( ! pdbHome.canWrite() ){
+			System.err.println("can not write to " + pdbHome);
+			return null;
+		}
+		
+		String ftp = String.format("ftp://ftp.ebi.ac.uk/pub/databases/msd/pdb_uncompressed/pdb%s.ent", pdbId.toLowerCase()); 
+
+		System.out.println("Fetching " + ftp); 
+		try {
+			URL url = new URL(ftp);
+			InputStream conn = url.openStream();			
+
+			// prepare destination
+			System.out.println("writing to " + tempFile);
+			
+		
+			FileOutputStream outPut = new FileOutputStream(tempFile);
+			GZIPOutputStream gzOutPut = new GZIPOutputStream(outPut);
+			PrintWriter pw = new PrintWriter(gzOutPut);
+			
+			BufferedReader fileBuffer = new BufferedReader(new InputStreamReader(conn));
+			String line;
+			while ((line = fileBuffer.readLine()) != null) {				
+				pw.println(line);
+			}
+			pw.flush();
+			pw.close();
+			outPut.close();
+			conn.close();
+		} catch (Exception e){
+			e.printStackTrace();
+			return null; 
+		}
+		return tempFile;
+	}
+
+	private InputStream downloadAndGetInputStream(String pdbId) 
+	throws IOException{
+		//PDBURLReader reader = new PDBURLReader();
+		//Structure s = reader.getStructureById(pdbId);
+		File tmp = downloadPDB(pdbId);
+		if ( tmp != null ) {
+			InputStreamProvider prov = new InputStreamProvider();
+			return prov.getInputStream(tmp);
+			
+
+		} else {
+			throw new IOException("could not find PDB " + pdbId + " in file system and also could not download");
+		}
+
+
+	}
+
+
+
+
+	/** load a structure from local file system and return a PDBStructure object 
+
+	 * @param pdbId  a String specifying the id value (PDB code)
+	 * @return the Structure object
+	 * @throws IOException ...
+	 */
+	public Structure getStructureById(String pdbId) 
+	throws IOException
+	{
+
+
+		InputStream inStream = getInputStream(pdbId);
+
+		PDBFileParser pdbpars = new PDBFileParser();
+		pdbpars.setParseSecStruc(parseSecStruc);
+		Structure struc = pdbpars.parsePDBFile(inStream) ;
+		return struc ;
+	}
+
+	/** opens filename, parses it and returns
+	 * aStructure object .
+	 * @param filename  a String
+	 * @return the Structure object
+	 * @throws IOException ...
+	 */
+	public Structure getStructure(String filename) 
+	throws IOException
+	{
+		File f = new File(filename);
+		return getStructure(f);
+
+	}
+
+	/** opens filename, parses it and returns a Structure object
+	 * 
+	 * @param filename a File object
+	 * @return the Structure object
+	 * @throws IOException ...
+	 */
+	public Structure getStructure(File filename) throws IOException {
+
+		InputStreamProvider isp = new InputStreamProvider();
+
+		InputStream inStream = isp.getInputStream(filename);
+
+		PDBFileParser pdbpars = new PDBFileParser();
+		pdbpars.setParseSecStruc(parseSecStruc);
+		Structure struc = pdbpars.parsePDBFile(inStream) ;
+		return struc ;
+
+	}
+
 }
