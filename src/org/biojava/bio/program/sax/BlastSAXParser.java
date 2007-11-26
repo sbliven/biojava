2282b9a6b0a5ae592540806299a7a0d5dd763bbb
holland <holland@7c6358e6-4a41-0410-a743-a5b2a554c398>
2007-11-26 14:47:14 +0000

    Reverted eariler commit.
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@4327 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/program/sax/BlastSAXParser.java
+++ src/org/biojava/bio/program/sax/BlastSAXParser.java
@@ -212,7 +212,7 @@ final class BlastSAXParser extends AbstractNativeAppSAXParser {
                 while(true){
                     try {
                         poLine = oContents.readLine();
-                        if (poLine.startsWith("Searching") || poLine.startsWith("Sequences")) {
+                        if (poLine.startsWith("Searching")) {
 							break;
 						} else if (poLine.startsWith("Results of")) {
 							// in PSI-blast is this line...
