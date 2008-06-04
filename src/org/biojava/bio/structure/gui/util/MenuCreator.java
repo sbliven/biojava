300ad0c9ec73627bd433c840e9bd9901717f3d9c
andreas <andreas@7c6358e6-4a41-0410-a743-a5b2a554c398>
2008-06-04 21:19:03 +0000

    adding an open menu item
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@4779 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/structure/gui/util/MenuCreator.java
+++ src/org/biojava/bio/structure/gui/util/MenuCreator.java
@@ -27,17 +27,23 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JEditorPane;
+import javax.swing.JFileChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 
+import org.biojava.bio.structure.Structure;
 import org.biojava.bio.structure.gui.AlignmentGui;
+import org.biojava.bio.structure.gui.BiojavaJmol;
+import org.biojava.bio.structure.io.PDBFileReader;
+
 
 /**
  *  Create the menu for BiojavaJmol
@@ -53,66 +59,99 @@ public class MenuCreator {
 	public static JMenuBar initMenu(){
 
 		// show a menu
-		
+
 		JMenuBar menu = new JMenuBar();
-		
+
 		JMenu file= new JMenu("File");
-		file.getAccessibleContext().setAccessibleDescription("exit the application");
+		file.getAccessibleContext().setAccessibleDescription("File Menu");
+
+		JMenuItem openI = new JMenuItem("Open");
+		openI.setMnemonic(KeyEvent.VK_O);
+		openI.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				String cmd = e.getActionCommand();			        
+				if ( cmd.equals("Open")){
+					final JFileChooser fc = new JFileChooser();
+
+//					In response to a button click:
+					int returnVal = fc.showOpenDialog(null);
+					if ( returnVal == JFileChooser.APPROVE_OPTION) {
+						File file = fc.getSelectedFile();
+						
+						PDBFileReader reader = new PDBFileReader();
+						try {
+							Structure s = reader.getStructure(file);
+							BiojavaJmol jmol = new BiojavaJmol();
+							jmol.setStructure(s);
+							jmol.evalString("select * ; color chain;");
+							jmol.evalString("select *; spacefill off; wireframe off; backbone 0.4;  ");
+
+						} catch (Exception ex){
+							ex.printStackTrace();
+						}
+						
+
+					}
+				}				
+			}		
+		});
+		file.add(openI);
+
 		JMenuItem exitI = new JMenuItem("Exit");
 		exitI.setMnemonic(KeyEvent.VK_X);
 		exitI.addActionListener(new ActionListener(){
 
 			public void actionPerformed(ActionEvent e) {
-				 String cmd = e.getActionCommand();
-			        
-			        if ( cmd.equals("Exit")){
-			        	System.exit(0);
-			        }				
+				String cmd = e.getActionCommand();
+
+				if ( cmd.equals("Exit")){
+					System.exit(0);
+				}				
 			}			
 		});
-		
+
 		file.add(exitI);
 		menu.add(file);
-			
+
 		JMenu align = new JMenu("Align");
 		JMenuItem pairI = new JMenuItem("2 protein structures");
 		pairI.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				 String cmd = e.getActionCommand();
-			        
-			        if ( cmd.equals("2 protein structures")){
-			        	MenuCreator.showPairDialog();
-			        }				
+				String cmd = e.getActionCommand();
+
+				if ( cmd.equals("2 protein structures")){
+					MenuCreator.showPairDialog();
+				}				
 			}
 		});
-		
+
 		align.add(pairI);
-		
+
 		menu.add(align);
-		
+
 		JMenu about = new JMenu("About");
 		JMenuItem aboutI = new JMenuItem("PDBview");
 		aboutI.addActionListener(new ActionListener(){
 
 			public void actionPerformed(ActionEvent e) {
-				 String cmd = e.getActionCommand();
-			        
-			        if ( cmd.equals("PDBview")){
-			        	MenuCreator.showAboutDialog();
-			        }				
+				String cmd = e.getActionCommand();
+
+				if ( cmd.equals("PDBview")){
+					MenuCreator.showAboutDialog();
+				}				
 			}			
 		});
-		
+
 		about.add(aboutI);
-		
+
 		menu.add(Box.createGlue());
 		menu.add(about);
-		
+
 		return menu;
-		
+
 	}
-	
-	
+
+
 	/** provide a display for the pairwise structure alignment
 	 * 
 	 */
@@ -120,54 +159,54 @@ public class MenuCreator {
 		AlignmentGui gui = new AlignmentGui();
 		gui.setVisible(true);
 	}
-	
+
 	/** show some info about this gui
 	 * 
 	 */
 	private static void showAboutDialog(){
-		
+
 		JDialog dialog = new JDialog();
 
 		dialog.setSize(new Dimension(300,300));
 
 		String msg = "This viewer is based on <b>BioJava</b> and <b>Jmol</>. <br>Author: Andreas Prlic <br> ";
 		msg += "Structure Alignment algorithm based on a variation of the PSC++ algorithm by Peter Lackner.";
-		
-		
+
+
 		JEditorPane txt = new JEditorPane("text/html", msg);
 		txt.setEditable(false);
 
-		
-		 JScrollPane scroll = new JScrollPane(txt);
 
-		 Box vBox = Box.createVerticalBox();
-		 vBox.add(scroll);
+		JScrollPane scroll = new JScrollPane(txt);
 
-		 JButton close = new JButton("Close");
+		Box vBox = Box.createVerticalBox();
+		vBox.add(scroll);
 
-		 close.addActionListener(new ActionListener(){
-			 public void actionPerformed(ActionEvent event) {
-				 Object source = event.getSource();
-				 
-				 JButton but = (JButton)source;
-				 Container parent = but.getParent().getParent().getParent().getParent().getParent().getParent() ;
-				 
-				 JDialog dia = (JDialog) parent;
-				 dia.dispose();
-			 }
-		 });
+		JButton close = new JButton("Close");
 
-		 Box hBoxb = Box.createHorizontalBox();
-		 hBoxb.add(Box.createGlue());
-		 hBoxb.add(close,BorderLayout.EAST);
+		close.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent event) {
+				Object source = event.getSource();
 
-		 vBox.add(hBoxb);
+				JButton but = (JButton)source;
+				Container parent = but.getParent().getParent().getParent().getParent().getParent().getParent() ;
 
-		 dialog.getContentPane().add(vBox);
-		 dialog.setVisible(true);
-		
-		
+				JDialog dia = (JDialog) parent;
+				dia.dispose();
+			}
+		});
+
+		Box hBoxb = Box.createHorizontalBox();
+		hBoxb.add(Box.createGlue());
+		hBoxb.add(close,BorderLayout.EAST);
+
+		vBox.add(hBoxb);
+
+		dialog.getContentPane().add(vBox);
+		dialog.setVisible(true);
+
+
 	}
-	
-	
+
+
 }
