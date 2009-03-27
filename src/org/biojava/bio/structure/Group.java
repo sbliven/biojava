842db59a4e56588857e98d099ac082a09ce118e3
andreas <andreas@7c6358e6-4a41-0410-a743-a5b2a554c398>
2009-03-27 18:54:11 +0000

    improving javadoc
    git-svn-id: file:///home/svn-repositories/biojava/biojava-live/trunk@6884 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/org/biojava/bio/structure/Group.java
+++ src/org/biojava/bio/structure/Group.java
@@ -30,18 +30,18 @@ import org.biojava.bio.structure.io.PDBParseException;
 
 
 /**
- *  
+ *
  * This is the data structure for a single Group of atoms.  A protein
  * sequence ({@link Chain} in PDB file) is represented as a list of Groups.
  * There are 3 types of Groups:
- * 
+ *
  * <ul>
  * <li>{@link AminoAcid}</li>
  * <li>{@link HetatomImpl Hetatom}</li>
  * <li>{@link NucleotideImpl Nucleotide}</li>
  * </ul>
- *  
- *   
+ *
+ *
  * @see HetatomImpl
  * @see AminoAcidImpl
  * @see NucleotideImpl
@@ -51,128 +51,141 @@ import org.biojava.bio.structure.io.PDBParseException;
  * @version %I% %G%
  */
 public interface Group {
-    
-    
-    /* returns and identical copy of this Group .
+
+
+	/* returns and identical copy of this Group .
      public Object clone() ;
-     */
-    
-    /**
-     * return the PDBcode (residue number) of this group.
-     * @see #setPDBCode
-     * @return a String representing the PDBCode value
-     */
-    public String getPDBCode(); 
-    
-    /**
-     * Specifies the PDBCode value.
-     *
-     * @param pdbcode  a String specifying the PDBCode value
-     * @see #getPDBCode     
-     */    
-    public void setPDBCode(String pdbcode);
-    
-    
-    /** getnumber of atoms.
-     *  @return number of atoms of this Group
-     */
-    public int size();
-    
-    /**
-     *  returns true or false, depending if this group has 3D coordinates or not.
-     *
-     * @return true if Group has 3D coordinates
-     */
-    public boolean has3D ();
-    
-    /** flag if group has 3D data .
-     *
-     * @param flag  true to set flag that this Group has 3D coordinates
-     */
-    public void setPDBFlag(boolean flag);
-    
-    /** 
-     * get Type of group, e.g. amino, hetatom, nucleotide.
-     * 
-     *
-     * @return a String representing the type value     
-     */
-    public String getType();
-    
-    /** add an atom to this group.
-     *
-     * @param atom  an Atom object
-     */
-    public void addAtom(Atom atom);
-    
-    /** Get list of atoms.
-     *
-     * @return an List object representing the atoms
-     * @see #setAtoms(List) 
-     */
-    public List<Atom> getAtoms() ;
-    
-    
-    /** Set the atoms of this group.
-     * @see org.biojava.bio.structure.Atom
-     * @param atoms a list of atoms
-     */
-    public void setAtoms(List<Atom> atoms);
-    
-    /** Remove all atoms from this group.
-     * 
-     */
-    public void clearAtoms();
-    
-    /** Get an atom.  Throws StructureException if atom not found.
-     *
-     * @param name  a String
-     * @return an Atom object
-     * @throws StructureException if atom not found.
-     */
-    public Atom getAtom(String name) throws StructureException;
-    
-    /** Get at atom by position.
-     *
-     * @param position  an int
-     * @return an Atom object
-     * @throws StructureException if not atom at this position     
-     */
-    public Atom getAtom(int position) throws StructureException;
-    
-    /** Teturns flag whether a particular atom is existing within this group .
-     *
-     * @param name  a String ...
-     * @return true if Atom with name is existing within this group
-     */
-    public boolean hasAtom(String name);
-    
-    /** set the PDB 3 character name for this group.
-     * 
-     * @return a String representing the PDBName value
-     * @see #setPDBName
-     */
-    public String getPDBName();
-    
-    /** get the PDB 3 character name for this group.
-     *
-     * @param s  a String specifying the PDBName value
-     * @throws PDBParseException ...     
-     * @see #getPDBName
-     */
-    public void setPDBName(String s) throws PDBParseException;
-    
-    
-    /** calculate if a groups has all atoms required for an amino acid.
+	 */
+
+	/**
+	 * Return the PDBcode (residue number + insertion code ) of this group.
+	 *
+	 * The residue number is treated as a String for the following reasons:
+	 *
+	 * Every amino acid in a PDB file is
+	 * identified uniquely by 3 things: The chain ID, the residue number and
+	 * the insertion code. To make sure one does not forget about the
+	 * insertion code, in BJ it is appended to the residue number.
+	 *
+	 * To add to this, residue numbers can be negative, non-consecutive and
+	 * also non-sequential. As such it is often easiest, to treat them as
+	 * public identifiers and within your own code work with  the internal
+	 * atom or group positions...
+	 *
+	 * @see #setPDBCode
+	 * @return a String representing the PDBCode value
+	 */
+	public String getPDBCode();
+
+	/**
+	 * Specifies the PDBCode (residue number + insertion code) value.
+	 *
+	 * @param pdbcode  a String specifying the PDBCode value
+	 * @see #getPDBCode
+	 */
+	public void setPDBCode(String pdbcode);
+
+
+	/** getnumber of atoms.
+	 *  @return number of atoms of this Group
+	 */
+	public int size();
+
+	/**
+	 *  returns true or false, depending if this group has 3D coordinates or not.
+	 *
+	 * @return true if Group has 3D coordinates
+	 */
+	public boolean has3D ();
+
+	/** flag if group has 3D data .
+	 *
+	 * @param flag  true to set flag that this Group has 3D coordinates
+	 */
+	public void setPDBFlag(boolean flag);
+
+	/**
+	 * get Type of group, e.g. amino, hetatom, nucleotide.
+	 *
+	 *
+	 * @return a String representing the type value
+	 */
+	public String getType();
+
+	/** add an atom to this group.
+	 *
+	 * @param atom  an Atom object
+	 */
+	public void addAtom(Atom atom);
+
+	/** Get list of atoms.
+	 *
+	 * @return an List object representing the atoms
+	 * @see #setAtoms(List)
+	 */
+	public List<Atom> getAtoms() ;
+
+
+	/** Set the atoms of this group.
+	 * @see org.biojava.bio.structure.Atom
+	 * @param atoms a list of atoms
+	 */
+	public void setAtoms(List<Atom> atoms);
+
+	/** Remove all atoms from this group.
+	 *
+	 */
+	public void clearAtoms();
+
+	/** Get an atom.  Throws StructureException if atom not found.
+	 *
+	 * @param name  a String
+	 * @return an Atom object
+	 * @throws StructureException if atom not found.
+	 */
+	public Atom getAtom(String name) throws StructureException;
+
+	/** Get at atom by position.
+	 *
+	 * @param position  an int
+	 * @return an Atom object
+	 * @throws StructureException if not atom at this position
+	 */
+	public Atom getAtom(int position) throws StructureException;
+
+	/** Teturns flag whether a particular atom is existing within this group .
+	 *
+	 * @param name  a String ...
+	 * @return true if Atom with name is existing within this group
+	 */
+	public boolean hasAtom(String name);
+
+	/** set the PDB 3 character name for this group.
+	 *
+	 * @return a String representing the PDBName value
+	 * @see #setPDBName
+	 */
+	public String getPDBName();
+
+	/** get the PDB 3 character name for this group.
+	 *
+	 * @param s  a String specifying the PDBName value
+	 * @throws PDBParseException ...
+	 * @see #getPDBName
+	 */
+	public void setPDBName(String s) throws PDBParseException;
+
+
+	/** calculate if a groups has all atoms required for an amino acid.
      this allows to include chemically modified amino acids that
      are labeled hetatoms into some computations ... the usual way
      to identify if a group is an amino acid is getType() !
-     
+
      <p>
      amino atoms are : N, CA, C, O, CB
      GLY does not have CB (unless we would calculate some artificially
      </p>
-     
+
      Example: 1DW9 chain A first group is a Selenomethionine, provided as HETATM, but here returns true.
      <pre>
      HETATM    1  N   MSE A   1      11.720  20.973   1.584  0.00  0.00           N
@@ -181,77 +194,77 @@ public interface Group {
      HETATM    4  O   MSE A   1      10.198  19.156   2.985  0.00  0.00           O
      HETATM    5  CB  MSE A   1      10.407  19.441   0.088  0.00  0.00           C
      </pre>
-     *
-     * @return true if all Atoms required for an AminoAcid are available (N, CA, C, O, CB)	
+	 *
+	 * @return true if all Atoms required for an AminoAcid are available (N, CA, C, O, CB)
      @see #getType
-     */
-    public boolean hasAminoAtoms() ;
-    
-    
-    
-    /** properties of this amino acid. currerntly available properties.
-     * are:
-     * phi
-     * psi
-     * 
-     *
-     * @param properties  a Map object specifying the properties value
-     * @see #getProperties
-     
-     */
-    
-    public void setProperties(Map<String,Object> properties) ;
-    
-    /** return properties. 
-     * @see #setProperties
-     *
-     * @return a HashMap object representing the properties value
-     */
-    public Map<String,Object> getProperties() ;
-    
-    /** set a single property .
-     *
-     * @param key    a String
-     * @param value  an Object
-     * @see #getProperty
-     
-     */
-    public void setProperty(String key, Object value) ;
-    
-    /** get a single property .
-     *
-     * @param key  a String
-     * @return an Object
-     * @see #setProperty
-     */
-    public Object getProperty(String key) ;
-    
-    /** get an Atom Iterator.
-     *
-     * @return an Iterator object
-     */
-    public Iterator<Atom> iterator() ;
-    
-    
-    /** returns and identical copy of this Group object .
-     * @return  and identical copy of this Group object 
-     */
-    public Object clone();
-    
-    
-    /** Set the back-reference (to its parent Chain).
-     * @param parent the parent Chain
-     * @see #getParent()
-     * 
-     */
-    public void setParent(Chain parent) ; 
-    
-    /** Returns the parent Chain of the Group.
-     * 
-     * @return Chain the Chain object that contains the Group
-     * @see #setParent(Chain)
-     */
-    
-    public Chain getParent() ;
-    
+	 */
+	public boolean hasAminoAtoms() ;
+
+
+
+	/** properties of this amino acid. currerntly available properties.
+	 * are:
+	 * phi
+	 * psi
+	 *
+	 *
+	 * @param properties  a Map object specifying the properties value
+	 * @see #getProperties
+
+	 */
+
+	public void setProperties(Map<String,Object> properties) ;
+
+	/** return properties.
+	 * @see #setProperties
+	 *
+	 * @return a HashMap object representing the properties value
+	 */
+	public Map<String,Object> getProperties() ;
+
+	/** set a single property .
+	 *
+	 * @param key    a String
+	 * @param value  an Object
+	 * @see #getProperty
+
+	 */
+	public void setProperty(String key, Object value) ;
+
+	/** get a single property .
+	 *
+	 * @param key  a String
+	 * @return an Object
+	 * @see #setProperty
+	 */
+	public Object getProperty(String key) ;
+
+	/** get an Atom Iterator.
+	 *
+	 * @return an Iterator object
+	 */
+	public Iterator<Atom> iterator() ;
+
+
+	/** returns and identical copy of this Group object .
+	 * @return  and identical copy of this Group object
+	 */
+	public Object clone();
+
+
+	/** Set the back-reference (to its parent Chain).
+	 * @param parent the parent Chain
+	 * @see #getParent()
+	 *
+	 */
+	public void setParent(Chain parent) ;
+
+	/** Returns the parent Chain of the Group.
+	 *
+	 * @return Chain the Chain object that contains the Group
+	 * @see #setParent(Chain)
+	 */
+
+	public Chain getParent() ;
+
 }
